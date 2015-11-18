5e8a2ed26f3c89aa736744987c2d9fbe2064989f
Olaoluwa Osuntokun <laolu32@gmail.com>
2015-11-18 14:59:07 -0800

    add skeleton for initial wallet tests

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -1,1 +1,27 @@
 package wallet
+
+import "testing"
+
+func createTestWallet() {
+}
+
+func TestBasicWalletReservationWorkFlow(t *testing.T) {
+	// all stages
+	// fields on struct properly set
+	// funding transaction on disk
+}
+
+func TestFundingTransactiontxFees(t *testing.T) {
+}
+
+func TestFundingTransactionLockedOutputs(t *testing.T) {
+}
+
+func TestFundingTransactionCancellationFreeOutputs(t *testing.T) {
+}
+
+func TestFundingReservationInsufficientFunds(t *testing.T) {
+}
+
+func TestFundingReservationInvalidCounterpartySigs(t *testing.T) {
+}

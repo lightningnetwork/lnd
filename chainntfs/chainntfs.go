b2522ef75ad35f6b15eae490a6f2b36cdff44207
Olaoluwa Osuntokun <laolu32@gmail.com>
2015-12-16 14:38:21 -0600

    Create skeleton for chain notifications
    * Goal is to backend agnostic source to chain notifications
    * Open channels will communicate with this source to register intent
    for critical notifications
    * Pending notifications will need to be committed to disk.
    * Will also need a journal to ensure no notification is missed
    * Later goals include extending this to communicate with 3rd party
    outsource services

---
+++ chainntfs/chainntfs.go
@@ -0,0 +1,12 @@
+package chainntnfs
+
+// TODO(roasbeef): finish
+//  * multiple backends for interface
+//   * btcd - websockets
+//   * core - rpc polling or ZeroMQ
+//   * direct p2p
+//   * random bitcoin API?
+//   * electrum?
+//   * other stuff maybe...
+type ChainNotifier interface {
+}

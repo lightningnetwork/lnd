51dab184469414d4c62550572c4c4ea995ba8acf
Joseph Poon <joseph@lightning.network>
2015-12-16 10:57:22 -0800

    Committed the wrong _test file earlier...

--- wallet/shachain/hash_chain_test.go
+++ wallet/shachain/hash_chain_test.go
@@ -1,3 +1,13 @@
 package wallet
 
-import "testing"
+import (
+	//"fmt"
+	"github.com/btcsuite/btcd/wire"
+	"testing"
+)
+
+func TestRunShaChain(t *testing.T) {
+	var seed wire.ShaHash
+	var hash wire.ShaHash
+	NewShaChainFromSeed(seed, 0, hash)
+}

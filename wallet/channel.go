354bc74d3c596767a9776f9235bb3cb2b240b583
Olaoluwa Osuntokun <laolu32@gmail.com>
2015-12-16 14:40:11 -0600

    Flesh out LightningChannel struct a bit more
    * p2p logic will only know about this object
    * Purposes include storing old commitment txns, receiving them,
    triggering p2p actions base off of channelEvents, getting/verifying
    revocation hashes, etc

--- wallet/channel.go
+++ wallet/channel.go
@@ -1,4 +1,33 @@
 package wallet
 
+import (
+	"sync"
+
+	"li.lan/labs/plasma/chainntfs"
+	"li.lan/labs/plasma/revocation"
+
+	"github.com/btcsuite/btcwallet/walletdb"
+)
+
 type LightningChannel struct {
+	shachan *revocation.ShaChain
+	wallet  *LightningWallet
+
+	channelEvents *chainntnfs.ChainNotifier
+
+	sync.RWMutex
+
+	channelNamespace walletdb.Namespace
+
+	// TODO(roasbeef): create and embed 'Service' interface w/ below?
+	started int32
+
+	shutdown int32
+
+	quit chan struct{}
+	wg   sync.WaitGroup
+}
+
+func newLightningChannel() *LightningChannel {
+	return nil
 }

3f0173f19a5d047869fc4d46354c47f2832eb47a
Olaoluwa Osuntokun <laolu32@gmail.com>
2016-06-20 20:47:43 -0700

    chainnfts: export logger for use by concrete implementations

--- chainntfs/log.go
+++ chainntfs/log.go
@@ -10,7 +10,7 @@ import (
 // log is a logger that is initialized with no output filters.  This
 // means the package will not perform any logging by default until the caller
 // requests it.
-var log btclog.Logger
+var Log btclog.Logger
 
 // The default amount of logging is none.
 func init() {
@@ -20,14 +20,14 @@ func init() {
 // DisableLog disables all library log output.  Logging output is disabled
 // by default until either UseLogger or SetLogWriter are called.
 func DisableLog() {
-	log = btclog.Disabled
+	Log = btclog.Disabled
 }
 
 // UseLogger uses a specified Logger to output package logging info.
 // This should be used in preference to SetLogWriter if the caller is also
 // using btclog.
 func UseLogger(logger btclog.Logger) {
-	log = logger
+	Log = logger
 }
 
 // SetLogWriter uses a specified io.Writer to output package logging info.

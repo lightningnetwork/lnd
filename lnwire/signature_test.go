791683b39315d6edb0c86c7cceb0febbd5bf7e30
Alex <alex@akselrod.org>
2016-12-07 15:56:55 -0700

    Rename signature test to fit the pattern of the other tests.

--- lnwire/signature_test.go
+++ lnwire/signature_test.go
@@ -8,10 +8,10 @@ import (
 	"github.com/roasbeef/btcd/btcec"
 )
 
-func TestSerializeDeserializeSignature(t *testing.T) {
+func TestSignatureSerializeDeserialize(t *testing.T) {
 	// Local-scoped closure to serialize and deserialize a Signature and
 	// check for errors as well as check if the results are correct.
-	serializeDeserializeSignature := func(e btcec.Signature) error {
+	signatureSerializeDeserialize := func(e btcec.Signature) error {
 		var b [64]byte
 		err := serializeSigToWire(&b, &e)
 		if err != nil {
@@ -39,14 +39,14 @@ func TestSerializeDeserializeSignature(t *testing.T) {
 	sig.R = big.NewInt(1) // Allocate a big.Int before we call .Sub.
 	sig.R.Sub(btcec.S256().N, sig.R)
 	sig.S = big.NewInt(128)
-	err := serializeDeserializeSignature(sig)
+	err := signatureSerializeDeserialize(sig)
 	if err != nil {
 		t.Fatalf("R = N-1, S = 128: %s", err.Error())
 	}
 
 	// Check R = N-1, S = 127.
 	sig.S = big.NewInt(127)
-	err = serializeDeserializeSignature(sig)
+	err = signatureSerializeDeserialize(sig)
 	if err != nil {
 		t.Fatalf("R = N-1, S = 127: %s", err.Error())
 	}
@@ -54,14 +54,14 @@ func TestSerializeDeserializeSignature(t *testing.T) {
 	// Check R = N-1, S = N>>1.
 	sig.S.Set(btcec.S256().N)
 	sig.S.Rsh(sig.S, 1)
-	err = serializeDeserializeSignature(sig)
+	err = signatureSerializeDeserialize(sig)
 	if err != nil {
 		t.Fatalf("R = N-1, S = N>>1: %s", err.Error())
 	}
 
 	// Check R = N-1, S = N.
 	sig.S.Set(btcec.S256().N)
-	err = serializeDeserializeSignature(sig)
+	err = signatureSerializeDeserialize(sig)
 	if err.Error() != "signature S isn't 1 or more" {
 		t.Fatalf("R = N-1, S = N should become R = N-1, S = 0: %s",
 			err.Error())
@@ -69,7 +69,7 @@ func TestSerializeDeserializeSignature(t *testing.T) {
 
 	// Check R = N-1, S = N-1.
 	sig.S.Sub(sig.S, big.NewInt(1))
-	err = serializeDeserializeSignature(sig)
+	err = signatureSerializeDeserialize(sig)
 	if err.Error() != "Pre/post-serialize Ss don't match: 115792089237316"+
 		"195423570985008687907852837564279074904382605163141518161494"+
 		"336, 1" {
@@ -80,7 +80,7 @@ func TestSerializeDeserializeSignature(t *testing.T) {
 	// Check R = 2N, S = 128
 	sig.R.Mul(btcec.S256().N, big.NewInt(2))
 	sig.S.Set(big.NewInt(127))
-	err = serializeDeserializeSignature(sig)
+	err = signatureSerializeDeserialize(sig)
 	if err.Error() != "R is over 32 bytes long without padding" {
 		t.Fatalf("R = 2N, S = 128, R should be over 32 bytes: %s",
 			err.Error())

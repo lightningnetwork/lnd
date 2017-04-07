62ac716a29e9b5118f705f40c78d42966eb595b9
Olaoluwa Osuntokun <laolu32@gmail.com>
2017-04-07 17:54:56 +0200

    lnd: bump version to 0.2-alpha

--- version.go
+++ version.go
@@ -15,8 +15,8 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 // versioning 2.0.0 spec (http://semver.org/).
 const (
 	appMajor uint = 0
-	appMinor uint = 1
-	appPatch uint = 1
+	appMinor uint = 2
+	appPatch uint = 0
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.

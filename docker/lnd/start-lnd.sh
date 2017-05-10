lightningnetwork/lnd

commit d5af509435603686d39cc2b35d40a28621ca0882

docker: update docker instructions to be aware of multi-chain config

docker/lnd/start-lnd.sh
@@ -47,10 +47,11 @@ BITCOIN_NETWORK=$(set_default "$BITCOIN_NETWORK" "simnet")
 lnd \
     --datadir="/data" \
     --logdir="/data" \
-    "--$BITCOIN_NETWORK" \
-    --btcdhost="btcd" \
     --rpccert="/rpc/rpc.cert" \
-    --rpcuser="$RPCUSER" \
-    --rpcpass="$RPCPASS" \
+    --bitcoin.active \
+    "--bitcoin.$BITCOIN_NETWORK" \
+    --bitcoin.rpchost="btcd" \
+    --bitcoin.rpcuser="$RPCUSER" \
+    --bitcoin.rpcpass="$RPCPASS" \
     --debuglevel="$DEBUG" \
     "$@"

commit 68155a4e976362dd6e33357d68e9447c8d2e2168

docker: fix multi-chain config in lnd
Bug introduced in d5af509435603686d39cc2b35d40a28621ca0882.

docker/lnd/start-lnd.sh
@@ -47,7 +47,7 @@ BITCOIN_NETWORK=$(set_default "$BITCOIN_NETWORK" "simnet")
 lnd \
     --datadir="/data" \
     --logdir="/data" \
-    --rpccert="/rpc/rpc.cert" \
+    --bitcoin.rpccert="/rpc/rpc.cert" \
     --bitcoin.active \
     "--bitcoin.$BITCOIN_NETWORK" \
     --bitcoin.rpchost="btcd" \
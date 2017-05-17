49e27c77ddfd168b18b35990f9479795fecccdb6
Olaoluwa Osuntokun <laolu32@gmail.com>
2017-05-16 18:46:50 -0700

    chainntnfs/btcdnotify: modify logging level from error to warn

--- chainntnfs/btcdnotify/btcd.go
+++ chainntnfs/btcdnotify/btcd.go
@@ -416,7 +416,7 @@ func (b *BtcdNotifier) attemptHistoricalDispatch(msg *confirmationsNotification,
 	tx, err := b.chainConn.GetRawTransactionVerbose(msg.txid)
 	if err != nil || tx == nil || tx.BlockHash == "" {
 		if err != nil {
-			chainntnfs.Log.Errorf("unable to query for txid(%v): %v",
+			chainntnfs.Log.Warnf("unable to query for txid(%v): %v",
 				msg.txid, err)
 		}
 		return false

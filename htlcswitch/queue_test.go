22d90d6b35bd65bdcfd9bd7e9dd328653c551ba1
Andrey Samokhvalov <andrew.shvv@gmail.com>
2017-05-24 17:34:35 +0300

    htlcswitch: add packet queue
    In this commit pending packet queue have been added. This queue
    consumes the htlc packet, store it inside the golang list and send it
    to the pending channel upon release notification.

---
+++ htlcswitch/queue_test.go
@@ -0,0 +1,43 @@
+package htlcswitch
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/roasbeef/btcutil"
+)
+
+// TestWaitingQueueThreadSafety test the thread safety properties of the
+// waiting queue, by executing methods in seprate goroutines which operates
+// with the same data.
+func TestWaitingQueueThreadSafety(t *testing.T) {
+	q := newWaitingQueue()
+
+	a := make([]btcutil.Amount, 1000)
+	for i := 0; i < len(a); i++ {
+		a[i] = btcutil.Amount(i)
+		q.consume(&htlcPacket{
+			amount: btcutil.Amount(i),
+			htlc:   &lnwire.UpdateAddHTLC{},
+		})
+	}
+
+	var b []btcutil.Amount
+	for i := 0; i < len(a); i++ {
+		q.release()
+
+		select {
+		case packet := <-q.pending:
+			b = append(b, packet.amount)
+
+		case <-time.After(2 * time.Second):
+			t.Fatal("timeout")
+		}
+	}
+
+	if !reflect.DeepEqual(b, a) {
+		t.Fatal("wrong order of the objects")
+	}
+}

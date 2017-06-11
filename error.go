ddadcb7f8a59fbc497139ca0b93ae7bf52958933
Olaoluwa Osuntokun <laolu32@gmail.com>
2017-06-10 17:32:32 -0700

    sphinx: add comment for ErrReplayedPacket

--- error.go
+++ error.go
@@ -3,5 +3,8 @@ package sphinx
 import "fmt"
 
 var (
+	// ErrReplayedPacket is an error returned when a packet is rejected
+	// during processing due to being an attempted replay or probing
+	// attempt.
 	ErrReplayedPacket = fmt.Errorf("sphinx packet replay attempted")
 )

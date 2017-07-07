906771ba16c6e62bd27756d39cef86e82a16abf8
Olaoluwa Osuntokun <laolu32@gmail.com>
2017-07-06 18:30:06 -0700

    features: add temporary feature for proper HTLC dust accounting

--- features.go
+++ features.go
@@ -29,4 +29,8 @@ var localFeatures = lnwire.NewFeatureVector([]lnwire.Feature{
 		Name: "sphinx-payload",
 		Flag: lnwire.RequiredFlag,
 	},
+	{
+		Name: "htlc-dust-accounting",
+		Flag: lnwire.RequiredFlag,
+	},
 })

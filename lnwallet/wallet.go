0a5615c44c65c63755091b51f2f897e467137861
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2017-09-22 15:37:08 +0800

    Fix typo

--- lnwallet/wallet.go
+++ lnwallet/wallet.go
@@ -229,7 +229,7 @@ type addSingleFunderSigsMsg struct {
 // embeddable within future projects interacting with the Lightning Network.
 //
 // NOTE: At the moment the wallet requires a btcd full node, as it's dependent
-// on btcd's websockets notifications as even triggers during the lifetime of a
+// on btcd's websockets notifications as event triggers during the lifetime of a
 // channel. However, once the chainntnfs package is complete, the wallet will
 // be compatible with multiple RPC/notification services such as Electrum,
 // Bitcoin Core + ZeroMQ, etc. Eventually, the wallet won't require a full-node

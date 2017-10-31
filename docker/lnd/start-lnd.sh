5ced45af01b0fe4d20f08a32b3cef91d18b35a89
Olaoluwa Osuntokun <laolu32@gmail.com>
2017-10-30 18:40:44 -0700

    docker/lnd: start container w/o prompting user for wallet passphrase
    In this commit we restore the prior workflow of the docker-compose set
    up with have to allow developers to quickly erect an lnd+btcd instance.
    In a recent change, we now prompt the user for a passphrase that will
    be used to encrypt the wallet database. This affected the prior
    workflow, as it assumed set up was non-interactive.
    
    In this commit we restore the prior behavior as we now start the lnd
    nodes with --noencryptwallet.

--- docker/lnd/start-lnd.sh
+++ docker/lnd/start-lnd.sh
@@ -46,6 +46,7 @@ NETWORK=$(set_default "$NETWORK" "simnet")
 CHAIN=$(set_default "$CHAIN" "bitcoin")
 
 lnd \
+    --noencryptwallet
     --logdir="/data" \
     "--$CHAIN.rpccert"="/rpc/rpc.cert" \
     "--$CHAIN.active" \

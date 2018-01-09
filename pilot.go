4d37e6a0882ca77e5884f501a7cd0d3bc5720cd4
Johan T. Halseth <johanth@gmail.com>
2018-01-09 10:01:17 +0100

    pilot: add goroutine to waitgroup

--- pilot.go
+++ pilot.go
@@ -198,6 +198,7 @@ func initAutoPilot(svr *server, cfg *autoPilotConfig) (*autopilot.Agent, error) 
 		}
 
 	}()
+	svr.wg.Add(1)
 	go func() {
 		defer txnSubscription.Cancel()
 		defer svr.wg.Done()

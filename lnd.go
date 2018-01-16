21c29c33d7c69b23625a46e0623ac3392872ec1f
Alex <alex@akselrod.org>
2018-01-16 09:18:41 -0700

    multi: upgrade macaroons to v2, replace per-method auth with interceptors
    This commit reworks the macaroon authentication framework to use the
    v2 macaroon format and bakery API. It also replaces the code in each
    RPC method which calls the macaroon verifier with interceptors which
    call the macaroon verifier instead. In addition, the operation
    permissions are reworked to fit the new format of "allow" commands
    (specifically, entity/operation permissions instead of method
    permissions).

--- lnd.go
+++ lnd.go
@@ -24,7 +24,7 @@ import (
 	"sync"
 	"time"
 
-	"gopkg.in/macaroon-bakery.v1/bakery"
+	"gopkg.in/macaroon-bakery.v2/bakery"
 
 	"golang.org/x/net/context"
 
@@ -143,10 +143,15 @@ func lndMain() error {
 	defer chanDB.Close()
 
 	// Only process macaroons if --no-macaroons isn't set.
-	var macaroonService *bakery.Service
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	var macaroonService *bakery.Bakery
 	if !cfg.NoMacaroons {
 		// Create the macaroon authentication/authorization service.
-		macaroonService, err = macaroons.NewService(macaroonDatabaseDir)
+		macaroonService, err = macaroons.NewService(macaroonDatabaseDir,
+			macaroons.IPLockChecker)
 		if err != nil {
 			srvrLog.Errorf("unable to create macaroon service: %v", err)
 			return err
@@ -154,8 +159,8 @@ func lndMain() error {
 
 		// Create macaroon files for lncli to use if they don't exist.
 		if !fileExists(cfg.AdminMacPath) && !fileExists(cfg.ReadMacPath) {
-			err = genMacaroons(macaroonService, cfg.AdminMacPath,
-				cfg.ReadMacPath)
+			err = genMacaroons(ctx, macaroonService,
+				cfg.AdminMacPath, cfg.ReadMacPath)
 			if err != nil {
 				ltndLog.Errorf("unable to create macaroon "+
 					"files: %v", err)
@@ -368,9 +373,19 @@ func lndMain() error {
 	}
 	server.fundingMgr = fundingMgr
 
+	// Check macaroon authentication if macaroons aren't disabled.
+	if macaroonService != nil {
+		serverOpts = append(serverOpts,
+			grpc.UnaryInterceptor(macaroons.UnaryServerInterceptor(
+				macaroonService, permissions)),
+			grpc.StreamInterceptor(macaroons.StreamServerInterceptor(
+				macaroonService, permissions)),
+		)
+	}
+
 	// Initialize, and register our implementation of the gRPC interface
 	// exported by the rpcServer.
-	rpcServer := newRPCServer(server, macaroonService)
+	rpcServer := newRPCServer(server)
 	if err := rpcServer.Start(); err != nil {
 		return err
 	}
@@ -393,10 +408,6 @@ func lndMain() error {
 	}
 
 	// Finally, start the REST proxy for our gRPC server above.
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
 	mux := proxy.NewServeMux()
 	err = lnrpc.RegisterLightningHandlerFromEndpoint(ctx, mux,
 		cfg.RPCListeners[0], proxyOpts)
@@ -648,32 +659,35 @@ func genCertPair(certFile, keyFile string) error {
 
 // genMacaroons generates a pair of macaroon files; one admin-level and one
 // read-only. These can also be used to generate more granular macaroons.
-func genMacaroons(svc *bakery.Service, admFile, roFile string) error {
-	// Generate the admin macaroon and write it to a file.
-	admMacaroon, err := svc.NewMacaroon("", nil, nil)
-	if err != nil {
-		return err
-	}
-	admBytes, err := admMacaroon.MarshalBinary()
-	if err != nil {
-		return err
-	}
-	if err = ioutil.WriteFile(admFile, admBytes, 0600); err != nil {
-		return err
-	}
+func genMacaroons(ctx context.Context, svc *bakery.Bakery, admFile,
+	roFile string) error {
 
 	// Generate the read-only macaroon and write it to a file.
-	roMacaroon, err := macaroons.AddConstraints(admMacaroon,
-		macaroons.AllowConstraint(roPermissions...))
+	roMacaroon, err := svc.Oven.NewMacaroon(ctx, bakery.LatestVersion, nil,
+		readPermissions...)
 	if err != nil {
 		return err
 	}
-	roBytes, err := roMacaroon.MarshalBinary()
+	roBytes, err := roMacaroon.M().MarshalBinary()
 	if err != nil {
 		return err
 	}
 	if err = ioutil.WriteFile(roFile, roBytes, 0644); err != nil {
 		os.Remove(admFile)
+		return err
+	}
+
+	// Generate the admin macaroon and write it to a file.
+	admMacaroon, err := svc.Oven.NewMacaroon(ctx, bakery.LatestVersion,
+		nil, append(readPermissions, writePermissions...)...)
+	if err != nil {
+		return err
+	}
+	admBytes, err := admMacaroon.M().MarshalBinary()
+	if err != nil {
+		return err
+	}
+	if err = ioutil.WriteFile(admFile, admBytes, 0600); err != nil {
 		return err
 	}
 
@@ -685,7 +699,7 @@ func genMacaroons(svc *bakery.Service, admFile, roFile string) error {
 // the user to this RPC server.
 func waitForWalletPassword(grpcEndpoints, restEndpoints []string,
 	serverOpts []grpc.ServerOption, proxyOpts []grpc.DialOption,
-	tlsConf *tls.Config, macaroonService *bakery.Service) ([]byte, []byte, error) {
+	tlsConf *tls.Config, macaroonService *bakery.Bakery) ([]byte, []byte, error) {
 
 	// Set up a new PasswordService, which will listen
 	// for passwords provided over RPC.
--- macaroons/service.go
+++ macaroons/service.go
@@ -1,9 +1,15 @@
 package macaroons
 
 import (
+	"fmt"
 	"path"
 
-	"gopkg.in/macaroon-bakery.v1/bakery"
+	"google.golang.org/grpc"
+
+	"gopkg.in/macaroon-bakery.v2/bakery"
+	"gopkg.in/macaroon-bakery.v2/bakery/checkers"
+
+	"golang.org/x/net/context"
 
 	"github.com/boltdb/bolt"
 )
@@ -15,8 +21,13 @@ var (
 )
 
 // NewService returns a service backed by the macaroon Bolt DB stored in the
-// passed directory.
-func NewService(dir string) (*bakery.Service, error) {
+// passed directory. The `checks` argument can be any of the `Checker` type
+// functions defined in this package, or a custom checker if desired. This
+// constructor prevents double-registration of checkers to prevent panics, so
+// listing the same checker more than once is not harmful. Default checkers,
+// such as those for `allow`, `time-before`, `declared`, and `error` caveats
+// are registered automatically and don't need to be added.
+func NewService(dir string, checks ...Checker) (*bakery.Bakery, error) {
 	// Open the database that we'll use to store the primary macaroon key,
 	// and all generated macaroons+caveats.
 	macaroonDB, err := bolt.Open(path.Join(dir, dbFilename), 0600,
@@ -29,19 +40,90 @@ func NewService(dir string) (*bakery.Service, error) {
 	if err != nil {
 		return nil, err
 	}
-	macaroonStore, err := NewStorage(macaroonDB)
-	if err != nil {
-		return nil, err
-	}
 
-	macaroonParams := bakery.NewServiceParams{
+	macaroonParams := bakery.BakeryParams{
 		Location:     "lnd",
-		Store:        macaroonStore,
 		RootKeyStore: rootKeyStore,
 		// No third-party caveat support for now.
 		// TODO(aakselrod): Add third-party caveat support.
 		Locator: nil,
 		Key:     nil,
 	}
-	return bakery.NewService(macaroonParams)
+
+	svc := bakery.New(macaroonParams)
+
+	// Register all custom caveat checkers with the bakery's checker.
+	// TODO(aakselrod): Add more checks as required.
+	checker := svc.Checker.FirstPartyCaveatChecker.(*checkers.Checker)
+	for _, check := range checks {
+		cond, fun := check()
+		if !isRegistered(checker, cond) {
+			checker.Register(cond, "std", fun)
+		}
+	}
+
+	return svc, nil
+}
+
+// isRegistered checks to see if the required checker has already been
+// registered in order to avoid a panic caused by double registration.
+func isRegistered(c *checkers.Checker, name string) bool {
+	if c == nil {
+		return false
+	}
+
+	for _, info := range c.Info() {
+		if info.Name == name && info.Prefix == "std" {
+			return true
+		}
+	}
+
+	return false
+}
+
+// UnaryServerInterceptor is a GRPC interceptor that checks whether the
+// request is authorized by the included macaroons.
+func UnaryServerInterceptor(svc *bakery.Bakery,
+	permissionMap map[string][]bakery.Op) grpc.UnaryServerInterceptor {
+
+	return func(ctx context.Context, req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler) (interface{}, error) {
+
+		if _, ok := permissionMap[info.FullMethod]; !ok {
+			return nil, fmt.Errorf("%s: unknown permissions "+
+				"required for method", info.FullMethod)
+		}
+
+		err := ValidateMacaroon(ctx, permissionMap[info.FullMethod],
+			svc)
+		if err != nil {
+			return nil, err
+		}
+
+		return handler(ctx, req)
+	}
+}
+
+// StreamServerInterceptor is a GRPC interceptor that checks whether the
+// request is authorized by the included macaroons.
+func StreamServerInterceptor(svc *bakery.Bakery,
+	permissionMap map[string][]bakery.Op) grpc.StreamServerInterceptor {
+
+	return func(srv interface{}, ss grpc.ServerStream,
+		info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+
+		if _, ok := permissionMap[info.FullMethod]; !ok {
+			return fmt.Errorf("%s: unknown permissions required "+
+				"for method", info.FullMethod)
+		}
+
+		err := ValidateMacaroon(ss.Context(),
+			permissionMap[info.FullMethod], svc)
+		if err != nil {
+			return err
+		}
+
+		return handler(srv, ss)
+	}
 }

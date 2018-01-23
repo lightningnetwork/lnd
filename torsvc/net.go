048c34c6c015a7eda0d5081f2d1662eb3dbc0a75
MeshCollider <dobsonsa68@gmail.com>
2018-01-23 16:59:15 +1300

    torsvc: Replace MultiNet with polymorphic Net interface

--- torsvc/net.go
+++ torsvc/net.go
@@ -5,54 +5,66 @@ import (
 	"net"
 )
 
-// MultiNet is an implementation of the Net interface that abstracts away the
-// "net" and "torsvc" functions from the user. This can be used to "switch"
-// Tor functionality on/off based on the MultiNet.Tor boolean. MultiNet allows
-// for callers to call net functions when necessary and torsvc functions when
-// necessary
-type MultiNet struct {
-	Tor      bool
-	TorDNS   string
-	TorSocks string
-}
+// RegularNet is an implementation of the Net interface that defines behaviour
+// for Regular network connections
+type RegularNet struct{}
 
-// A compile time check to ensure MultiNet implements the Net interface.
-var _ Net = (*MultiNet)(nil)
-
-// Dial uses either the "net" or "torsvc" dial function.
-func (m *MultiNet) Dial(network, address string) (net.Conn, error) {
-	if m.Tor {
-		if network != "tcp" {
-			return nil, fmt.Errorf("Cannot dial non-tcp network via Tor")
-		}
-		return TorDial(address, m.TorSocks)
-	}
+// Dial on the regular network uses net.Dial
+func (r *RegularNet) Dial(network, address string) (net.Conn, error) {
 	return net.Dial(network, address)
 }
 
-// LookupHost uses either the "net" or "torsvc LookupHost function.
-func (m *MultiNet) LookupHost(host string) ([]string, error) {
-	if m.Tor {
-		return TorLookupHost(host, m.TorSocks)
-	}
+// LookupHost for regular network uses the net.LookupHost function
+func (r *RegularNet) LookupHost(host string) ([]string, error) {
 	return net.LookupHost(host)
 }
 
-// LookupSRV uses either the "net" or "torsvc" LookupSRV function.
-func (m *MultiNet) LookupSRV(service, proto, name string) (string, []*net.SRV, error) {
-	if m.Tor {
-		return TorLookupSRV(service, proto, name, m.TorSocks, m.TorDNS)
-	}
+// LookupSRV for regular network uses net.LookupSRV function
+func (r *RegularNet) LookupSRV(service, proto, name string) (string, []*net.SRV, error) {
 	return net.LookupSRV(service, proto, name)
 }
 
-// ResolveTCPAddr uses either the "net" or "torsvc" ResolveTCP function.
-func (m *MultiNet) ResolveTCPAddr(network, address string) (*net.TCPAddr, error) {
-	if m.Tor {
-		if network != "tcp" {
-			return nil, fmt.Errorf("Cannot dial non-tcp network via Tor")
-		}
-		return TorResolveTCP(address, m.TorSocks)
-	}
+// ResolveTCPAddr for regular network uses net.ResolveTCPAddr function
+func (r *RegularNet) ResolveTCPAddr(network, address string) (*net.TCPAddr, error) {
 	return net.ResolveTCPAddr(network, address)
+}
+
+// TorProxyNet is an implementation of the Net interface that defines behaviour
+// for Tor network connections
+type TorProxyNet struct {
+	// TorDNS is the IP:PORT of the DNS server for Tor to use for SRV queries
+	TorDNS string
+
+	// TorSocks is the port which Tor's exposed SOCKS5 proxy is listening on.
+	// This is used for an outbound-only mode, so the node will not listen for
+	// incoming connections
+	TorSocks string
+}
+
+// Dial on the Tor network uses the torsvc TorDial() function, and requires
+// that network specified be tcp because only that is supported
+func (t *TorProxyNet) Dial(network, address string) (net.Conn, error) {
+	if network != "tcp" {
+		return nil, fmt.Errorf("Cannot dial non-tcp network via Tor")
+	}
+	return TorDial(address, t.TorSocks)
+}
+
+// LookupHost on Tor network uses the torsvc TorLookupHost function.
+func (t *TorProxyNet) LookupHost(host string) ([]string, error) {
+	return TorLookupHost(host, t.TorSocks)
+}
+
+// LookupSRV on Tor network uses the torsvc TorLookupHost function.
+func (t *TorProxyNet) LookupSRV(service, proto, name string) (string, []*net.SRV, error) {
+	return TorLookupSRV(service, proto, name, t.TorSocks, t.TorDNS)
+}
+
+// ResolveTCPAddr on Tor network uses the towsvc TorResolveTCP function, and
+// requires network to be "tcp" because only "tcp" is supported
+func (t *TorProxyNet) ResolveTCPAddr(network, address string) (*net.TCPAddr, error) {
+	if network != "tcp" {
+		return nil, fmt.Errorf("Cannot dial non-tcp network via Tor")
+	}
+	return TorResolveTCP(address, t.TorSocks)
 }

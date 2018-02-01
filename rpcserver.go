7144707734171023392d514ccc61941f7e30adb6
Olaoluwa Osuntokun <laolu32@gmail.com>
2018-02-01 11:54:58 -0800

    rpc: store display of alias in getinfo

--- rpcserver.go
+++ rpcserver.go
@@ -1198,6 +1198,7 @@ func (r *rpcServer) GetInfo(ctx context.Context,
 		Testnet:            activeNetParams.Params == &chaincfg.TestNet3Params,
 		Chains:             activeChains,
 		Uris:               uris,
+		Alias:              nodeAnn.Alias.String(),
 	}, nil
 }
 

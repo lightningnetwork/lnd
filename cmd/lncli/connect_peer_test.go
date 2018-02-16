8f61501752eb409a2a362639f20716ef01e293a4
Sean Erle Johnson <merehap@gmail.com>
2018-02-15 20:53:02 -0800

    lncli: move connectPeer and disconnectPeer tests to use TestCommand

--- cmd/lncli/connect_peer_test.go
+++ cmd/lncli/connect_peer_test.go
@@ -1,36 +1,18 @@
 package main
 
 import (
-	"flag"
 	"io"
 	"testing"
 
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/lightningnetwork/lnd/lnrpc/testing"
 	"github.com/stretchr/testify/require"
-	"github.com/urfave/cli"
 )
-
-func testConnectPeer(
-	t *testing.T, client lnrpc.LightningClient, address string) (string, error) {
-
-	set := flag.NewFlagSet("test", 0)
-	context := cli.NewContext(nil, set, nil)
-	set.Parse([]string{address})
-
-	writer := StringWriter{}
-	err := connectPeer(context, client, &writer)
-	if err != nil {
-		return "", err
-	}
-
-	return writer.Join(), nil
-}
 
 // connectPeer returns the correct output if no errors occurred.
 func TestConnectPeer(t *testing.T) {
 	client := lnrpctesting.NewStubLightningClient()
-	resp, err := testConnectPeer(t, &client, GoodAddress)
+	resp, err := testConnectPeer(t, &client, []string{GoodAddress})
 	require.NoError(t, err)
 	require.Equal(t, "{\n    \"peer_id\": 0\n}\n", resp,
 		"Incorrect JSON response from connectPeer.")
@@ -39,7 +21,7 @@ func TestConnectPeer(t *testing.T) {
 // connectPeer doesn't require a port in order to successfully connect.
 func TestConnectPeer_NoPort(t *testing.T) {
 	client := lnrpctesting.NewStubLightningClient()
-	resp, err := testConnectPeer(t, &client, GoodAddressWithoutPort)
+	resp, err := testConnectPeer(t, &client, []string{GoodAddressWithoutPort})
 	require.NoError(t, err)
 	require.Equal(t, "{\n    \"peer_id\": 0\n}\n", resp,
 		"Incorrect JSON response from connectPeer.")
@@ -48,7 +30,7 @@ func TestConnectPeer_NoPort(t *testing.T) {
 // connectPeer returns the correct error if an invalid address was specified.
 func TestConnectPeer_BadAddressFormat(t *testing.T) {
 	client := lnrpctesting.NewStubLightningClient()
-	_, err := testConnectPeer(t, &client, BadAddress)
+	_, err := testConnectPeer(t, &client, []string{BadAddress})
 	require.Error(t, err)
 	require.Equal(t, ErrBadAddressFormat, err, "Incorrect error returned")
 }
@@ -56,7 +38,7 @@ func TestConnectPeer_BadAddressFormat(t *testing.T) {
 // connectPeer bubbles up the error if the LightningClient fails to connect.
 func TestConnectPeer_FailedConnecting(t *testing.T) {
 	client := lnrpctesting.NewFailingStubLightningClient(io.ErrClosedPipe)
-	_, err := testConnectPeer(t, &client, GoodAddress)
+	_, err := testConnectPeer(t, &client, []string{GoodAddress})
 	require.Error(t, err)
 	require.Equal(t, io.ErrClosedPipe, err, "Incorrect error returned.")
 }
@@ -64,7 +46,14 @@ func TestConnectPeer_FailedConnecting(t *testing.T) {
 // connectPeer returns a friendly error message upon EOF errors.
 func TestConnectPeer_FailedConnectingWithEOF(t *testing.T) {
 	client := lnrpctesting.NewFailingStubLightningClient(io.EOF)
-	_, err := testConnectPeer(t, &client, GoodAddress)
+	_, err := testConnectPeer(t, &client, []string{GoodAddress})
 	require.Error(t, err)
 	require.Equal(t, io.EOF, err, "Incorrect error returned.")
+}
+
+func testConnectPeer(
+	t *testing.T, client lnrpc.LightningClient, args []string) (string, error) {
+
+	return TestCommand(
+		client, connectCommand, connectPeer, "connect", args)
 }
--- cmd/lncli/disconnect_peer_test.go
+++ cmd/lncli/disconnect_peer_test.go
@@ -11,26 +11,10 @@ import (
 	"github.com/urfave/cli"
 )
 
-func testDisconnectPeer(
-	t *testing.T, client lnrpc.LightningClient, args []string) (string, error) {
-
-	set := flag.NewFlagSet("test", 0)
-	context := cli.NewContext(nil, set, nil)
-	set.Parse(args)
-
-	writer := StringWriter{}
-	err := disconnectPeer(context, client, &writer)
-	if err != nil {
-		return "", err
-	}
-
-	return writer.Join(), nil
-}
-
 // disconnectPeer returns the correct output if no errors occur.
 func TestDisconnectPeer(t *testing.T) {
 	client := lnrpctesting.NewStubLightningClient()
-	resp, err := testDisconnectPeer(t, &client, []string{PubKey})
+	resp, err := testDisconnectPeer(&client, []string{PubKey})
 	require.NoError(t, err)
 	require.Equal(t, "{\n\n}\n", resp,
 		"Incorrect JSON response from disconnectPeer.")
@@ -56,7 +40,7 @@ func TestDisconnectPeer_NodeKeyFlag(t *testing.T) {
 // disconnectPeer returns the correct error if no pubkey was specified
 func TestDisconnectPeer_NoPubkey(t *testing.T) {
 	client := lnrpctesting.NewStubLightningClient()
-	_, err := testDisconnectPeer(t, &client, []string{})
+	_, err := testDisconnectPeer(&client, []string{})
 	require.Error(t, err)
 	require.Equal(t, ErrMissingPubKey, err, "Incorrect error returned.")
 }
@@ -64,7 +48,7 @@ func TestDisconnectPeer_NoPubkey(t *testing.T) {
 // disconnectPeer bubbles up the error if the LightningClient fails to connect.
 func TestDisconnectPeer_FailedDisconnecting(t *testing.T) {
 	client := lnrpctesting.NewFailingStubLightningClient(io.ErrClosedPipe)
-	_, err := testDisconnectPeer(t, &client, []string{PubKey})
+	_, err := testDisconnectPeer(&client, []string{PubKey})
 	require.Error(t, err)
 	require.Equal(t, io.ErrClosedPipe, err, "Incorrect error returned.")
 }
@@ -72,7 +56,25 @@ func TestDisconnectPeer_FailedDisconnecting(t *testing.T) {
 // disconnectPeer returns a friendly error message upon EOF errors.
 func TestDisconnectPeer_FailedDisconnectingWithEOF(t *testing.T) {
 	client := lnrpctesting.NewFailingStubLightningClient(io.EOF)
-	_, err := testDisconnectPeer(t, &client, []string{PubKey})
+	_, err := testDisconnectPeer(&client, []string{PubKey})
 	require.Error(t, err)
 	require.Equal(t, io.EOF, err, "Incorrect error returned.")
+}
+
+func testDisconnectPeer(client lnrpc.LightningClient, args []string) (string, error) {
+
+	set := flag.NewFlagSet("test", 0)
+	context := cli.NewContext(nil, set, nil)
+	set.Parse(args)
+
+	writer := StringWriter{}
+	err := disconnectPeer(context, client, &writer)
+	if err != nil {
+		return "", err
+	}
+
+	return writer.Join(), nil
+
+	return TestCommand(
+		client, disconnectCommand, disconnectPeer, "disconnect", args)
 }

c1f0c4ffdae4fd2def3dea24716d5d9fd935a7c6
Conner Fromknecht <conner@mit.edu>
2018-03-12 12:39:35 -0700

    peer: DecodeOnionObfuscator -> ExractErrorEncrypter

--- peer.go
+++ peer.go
@@ -399,7 +399,7 @@ func (p *peer) loadActiveChannels(chans []*channeldb.OpenChannel) error {
 		linkCfg := htlcswitch.ChannelLinkConfig{
 			Peer:                  p,
 			DecodeHopIterators:    p.server.sphinx.DecodeHopIterators,
-			DecodeOnionObfuscator: p.server.sphinx.ExtractErrorEncrypter,
+			ExtractErrorEncrypter: p.server.sphinx.ExtractErrorEncrypter,
 			GetLastChannelUpdate: createGetLastUpdate(p.server.chanRouter,
 				p.PubKey(), lnChan.ShortChanID()),
 			DebugHTLC:      cfg.DebugHTLC,
@@ -1366,7 +1366,7 @@ out:
 			linkConfig := htlcswitch.ChannelLinkConfig{
 				Peer:                  p,
 				DecodeHopIterators:    p.server.sphinx.DecodeHopIterators,
-				DecodeOnionObfuscator: p.server.sphinx.ExtractErrorEncrypter,
+				ExtractErrorEncrypter: p.server.sphinx.ExtractErrorEncrypter,
 				GetLastChannelUpdate: createGetLastUpdate(p.server.chanRouter,
 					p.PubKey(), newChanReq.channel.ShortChanID()),
 				DebugHTLC:      cfg.DebugHTLC,

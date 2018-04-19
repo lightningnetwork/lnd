409ab08c36e13adb5f2462ce75d2483d357b5f6a
Johan T. Halseth <johanth@gmail.com>
2018-04-19 09:44:34 +0200

    rpcserver: remove send to settledContracts

--- rpcserver.go
+++ rpcserver.go
@@ -1043,12 +1043,6 @@ func (r *rpcServer) CloseChannel(in *lnrpc.CloseChannelRequest,
 			r.server.htlcSwitch.RemoveLink(chanID)
 		}
 
-		select {
-		case r.server.breachArbiter.settledContracts <- *chanPoint:
-		case <-r.quit:
-			return fmt.Errorf("server shutting down")
-		}
-
 		// With the necessary indexes cleaned up, we'll now force close
 		// the channel.
 		chainArbitrator := r.server.chainArb

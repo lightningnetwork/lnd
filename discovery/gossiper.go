1a15924d65f27f1fce41cc3efe1710a14267070d
Olaoluwa Osuntokun <laolu32@gmail.com>
2018-06-11 22:58:24 -0700

    discovery: fix log for adding new gossip syncers
    In this commit, we fix the logging when adding new gossip syncers. The
    old log would log the byte array, rather than the byte slice. We fix
    this by slicing before logging.

--- discovery/gossiper.go
+++ discovery/gossiper.go
@@ -1233,7 +1233,7 @@ func (d *AuthenticatedGossiper) InitSyncState(syncPeer lnpeer.Peer, recvUpdates 
 	}
 
 	log.Infof("Creating new gossipSyncer for peer=%x",
-		nodeID)
+		nodeID[:])
 
 	syncer := newGossiperSyncer(gossipSyncerCfg{
 		chainHash:       d.cfg.ChainHash,

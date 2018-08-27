73af09a06ae9cd5ba92a376e8253ae5450fe09cc
Olaoluwa Osuntokun <laolu32@gmail.com>
2018-08-27 14:23:50 -0700

    lnd+cmd/lncli: bump version to 0.5-beta

--- version.go
+++ version.go
@@ -18,8 +18,8 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 // versioning 2.0.0 spec (http://semver.org/).
 const (
 	appMajor uint = 0
-	appMinor uint = 4
-	appPatch uint = 2
+	appMinor uint = 5
+	appPatch uint = 0
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.

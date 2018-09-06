d742529bb22cc477ba3512f9ff217517e334d726
Johan T. Halseth <johanth@gmail.com>
2018-09-06 08:39:09 +0200

    rpcserver: set AmtPaidMsat when creating RPC invoice

--- rpcserver.go
+++ rpcserver.go
@@ -2766,7 +2766,7 @@ func createRPCInvoice(invoice *channeldb.Invoice) (*lnrpc.Invoice, error) {
 
 	preimage := invoice.Terms.PaymentPreimage
 	satAmt := invoice.Terms.Value.ToSatoshis()
-	amtPaid := invoice.AmtPaid.ToSatoshis()
+	satAmtPaid := invoice.AmtPaid.ToSatoshis()
 
 	return &lnrpc.Invoice{
 		Memo:            string(invoice.Memo[:]),
@@ -2785,7 +2785,9 @@ func createRPCInvoice(invoice *channeldb.Invoice) (*lnrpc.Invoice, error) {
 		RouteHints:      routeHints,
 		AddIndex:        invoice.AddIndex,
 		SettleIndex:     invoice.SettleIndex,
-		AmtPaid:         int64(amtPaid),
+		AmtPaidSat:      int64(satAmtPaid),
+		AmtPaidMsat:     int64(invoice.AmtPaid),
+		AmtPaid:         int64(invoice.AmtPaid),
 	}, nil
 }
 

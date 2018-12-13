80ac8c1df08c09ea7a433d4df0738af23cee6a81
Johan T. Halseth <johanth@gmail.com>
2018-12-13 12:26:30 +0100

    lncli: add lncli autopilot commands
    This commit adds commands 'status', 'enable' and 'disable' for
    autopilot.
    
    The commands will only be enabled for autopilotrpc builds of lncli.

---
+++ cmd/lncli/autopilotrpc_default.go
@@ -0,0 +1,10 @@
+// +build !autopilotrpc
+
+package main
+
+import "github.com/urfave/cli"
+
+// autopilotCommands will return nil for non-autopilotrpc builds.
+func autopilotCommands() []cli.Command {
+	return nil
+}

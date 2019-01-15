ce68e7028dfdac8cef10f729f3dca4dd7aa701da
Joost Jager <joost.jager@gmail.com>
2019-01-15 10:07:15 +0100

    lnrpc/invoicesrpc: remove lnrpc type from add invoice

--- invoices/invoiceregistry.go
+++ invoices/invoiceregistry.go
@@ -376,7 +376,8 @@ func (i *InvoiceRegistry) AddDebugInvoice(amt btcutil.Amount,
 // daemon add/forward HTLCs are able to obtain the proper preimage required for
 // redemption in the case that we're the final destination. We also return the
 // addIndex of the newly created invoice which monotonically increases for each
-// new invoice added.
+// new invoice added. A side effect of this function is that it also sets
+// AddIndex on the invoice argument.
 func (i *InvoiceRegistry) AddInvoice(invoice *channeldb.Invoice) (uint64, error) {
 
 	i.Lock()

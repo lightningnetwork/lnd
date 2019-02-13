caa4a6d9650c97c6d5645cac7354216eaee721a2
MrManPew <35897350+MrManPew@users.noreply.github.com>
2019-02-13 23:21:07 +0200

    check if config exist only if it was changed

--- config.go
+++ config.go
@@ -360,10 +360,12 @@ func loadConfig() (*config, error) {
 		}
 	}
 	
-	// Check if config file exist
-        if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
-                return nil, fmt.Errorf("config file does not exist in %s", configFilePath)
-        }
+        // If user supplied alternative config, check if it exist
+	if configFilePath != defaultConfigFile {
+		if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
+			return nil, fmt.Errorf("command line config file does not exist in %s", configFilePath)
+		}
+	}
 
 	// Next, load any additional configuration options from the file.
 	var configFileError error

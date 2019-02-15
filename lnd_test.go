4686db4196bf3f59c9a2c7318a67169489fb858f
Conner Fromknecht <conner@lightning.engineering>
2019-02-14 17:13:11 -0800

    config+server+lnd_test: expose chan status manager config options
    Exposes the three parameters that dictate
    the behavior of the channel status manager:
     * --chan-enable-timeout
     * --chan-disable-timeout
     * --chan-status-sample-interval

--- lnd_test.go
+++ lnd_test.go
@@ -12755,7 +12755,7 @@ func testSendUpdateDisableChannel(net *lntest.NetworkHarness, t *harnessTest) {
 	// We create a new node Eve that has an inactive channel timeout of
 	// just 2 seconds (down from the default 20m). It will be used to test
 	// channel updates for channels going inactive.
-	eve, err := net.NewNode("Eve", []string{"--inactivechantimeout=2s"})
+	eve, err := net.NewNode("Eve", []string{"--chan-disable-timeout=2s"})
 	if err != nil {
 		t.Fatalf("unable to create eve's node: %v", err)
 	}
--- server.go
+++ server.go
@@ -3105,7 +3105,7 @@ func (s *server) watchChannelStatus() {
 	status := make(map[wire.OutPoint]activeStatus)
 
 	// We'll check in on the channel statuses every 1/4 of the timeout.
-	unchangedTimeout := cfg.InactiveChanTimeout
+	unchangedTimeout := cfg.ChanDisableTimeout
 	tickerTimeout := unchangedTimeout / 4
 
 	if unchangedTimeout == 0 || tickerTimeout == 0 {

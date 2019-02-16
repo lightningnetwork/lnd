6f96d04b72e787a431c4b2c73c144020e2742f27
Conner Fromknecht <conner@lightning.engineering>
2019-02-15 19:31:24 -0800

    multi: add buffer.Write and pool.WriteBuffer, make GCQueue generic

---
+++ buffer/write.go
@@ -0,0 +1,19 @@
+package buffer
+
+import (
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+// WriteSize represents the size of the maximum plaintext message than can be
+// sent using brontide. The buffer does not include extra space for the MAC, as
+// that is applied by the Noise protocol after encrypting the plaintext.
+const WriteSize = lnwire.MaxMessagePayload
+
+// Write is static byte array occupying to maximum-allowed plaintext-message
+// size.
+type Write [WriteSize]byte
+
+// Recycle zeroes the Write, making it fresh for another use.
+func (b *Write) Recycle() {
+	RecycleSlice(b[:])
+}

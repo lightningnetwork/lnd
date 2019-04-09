4944eb3e54b32b6b1e34913947b00d10a64897d7
Valentine Wallace <vwallace@protonmail.com>
2019-04-08 19:31:36 -0700

    lnwallet: add implementation of WebApiFeeSource for external APIs
    This enables users to specify an external API for fee estimation.
    The API is expected to return fees in the JSON format:
    `{
    	fee_by_block_target: {
    	   a: x,
    	   b: y,
    	   ...
    	   c: z
            }
     }`
     where a, b, c are block targets and x, y, z are fees in sat/kb.
     Note that a, b, c need not be contiguous.

--- lnwallet/fee_estimator_test.go
+++ lnwallet/fee_estimator_test.go
@@ -1,6 +1,9 @@
 package lnwallet_test
 
 import (
+	"bytes"
+	"encoding/json"
+	"reflect"
 	"testing"
 
 	"github.com/btcsuite/btcutil"
@@ -67,8 +70,8 @@ func TestFeeRateTypes(t *testing.T) {
 	}
 }
 
-// TestStaticFeeEstimator checks that the StaticFeeEstimator
-// returns the expected fee rate.
+// TestStaticFeeEstimator checks that the StaticFeeEstimator returns the
+// expected fee rate.
 func TestStaticFeeEstimator(t *testing.T) {
 	t.Parallel()
 
@@ -87,5 +90,57 @@ func TestStaticFeeEstimator(t *testing.T) {
 
 	if feeRate != feePerKw {
 		t.Fatalf("expected fee rate %v, got %v", feePerKw, feeRate)
+	}
+}
+
+// TestSparseConfFeeSource checks that SparseConfFeeSource generates URLs and
+// parses API responses as expected.
+func TestSparseConfFeeSource(t *testing.T) {
+	t.Parallel()
+
+	// Test that GenQueryURL returns the URL as is.
+	url := "test"
+	feeSource := lnwallet.SparseConfFeeSource{URL: url}
+	queryURL := feeSource.GenQueryURL()
+	if queryURL != url {
+		t.Fatalf("expected query URL of %v, got %v", url, queryURL)
+	}
+
+	// Test parsing a properly formatted JSON API response.
+	// First, create the response as a bytes.Reader.
+	testFees := map[uint32]uint32{
+		1: 12345,
+		2: 42,
+		3: 54321,
+	}
+	testJSON := map[string]map[uint32]uint32{"fee_by_block_target": testFees}
+	jsonResp, err := json.Marshal(testJSON)
+	if err != nil {
+		t.Fatalf("unable to marshal JSON API response: %v", err)
+	}
+	reader := bytes.NewReader(jsonResp)
+
+	// Finally, ensure the expected map is returned without error.
+	fees, err := feeSource.ParseResponse(reader)
+	if err != nil {
+		t.Fatalf("unable to parse API response: %v", err)
+	}
+	if !reflect.DeepEqual(fees, testFees) {
+		t.Fatalf("expected %v, got %v", testFees, fees)
+	}
+
+	// Test parsing an improperly formatted JSON API response.
+	badFees := map[string]uint32{"hi": 12345, "hello": 42, "satoshi": 54321}
+	badJSON := map[string]map[string]uint32{"fee_by_block_target": badFees}
+	jsonResp, err = json.Marshal(badJSON)
+	if err != nil {
+		t.Fatalf("unable to marshal JSON API response: %v", err)
+	}
+	reader = bytes.NewReader(jsonResp)
+
+	// Finally, ensure the improperly formatted fees error.
+	_, err = feeSource.ParseResponse(reader)
+	if err == nil {
+		t.Fatalf("expected ParseResponse to fail")
 	}
 }

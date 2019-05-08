88400f5b0962669a6f97e0b96be4776674b3c0a0
Juan Pablo Civile <elementohb@gmail.com>
2019-05-08 17:07:26 -0300

    lnd: close the wallet unlock grpc server
    The server was kept alive long after it stopped being used. This caused
    problems for services using long-lived GRPC connections which might be
    created before wallet unlocked. They got stuck connected to the wallet
    unlock service needing a restart.

--- lnd.go
+++ lnd.go
@@ -879,6 +879,7 @@ func waitForWalletPassword(grpcEndpoints, restEndpoints []net.Addr,
 	// Set up a new PasswordService, which will listen for passwords
 	// provided over RPC.
 	grpcServer := grpc.NewServer(serverOpts...)
+	defer grpcServer.GracefulStop()
 
 	chainConfig := cfg.Bitcoin
 	if registeredChains.PrimaryChain() == litecoinChain {

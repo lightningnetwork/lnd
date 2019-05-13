4d26e90ef3546c15dc18e1ae5558d9afbcfe350f
Conner Fromknecht <conner@lightning.engineering>
2019-05-13 12:55:15 -0500

    routing/router: nil path pubkeys before spewing

--- routing/router.go
+++ routing/router.go
@@ -1405,7 +1405,11 @@ func generateSphinxPacket(rt *route.Route, paymentHash []byte) ([]byte,
 
 	log.Tracef("Constructed per-hop payloads for payment_hash=%x: %v",
 		paymentHash[:], newLogClosure(func() string {
-			return spew.Sdump(sphinxPath[:sphinxPath.TrueRouteLength()])
+			path := sphinxPath[:sphinxPath.TrueRouteLength()]
+			for i := range path {
+				path[i].NodePub.Curve = nil
+			}
+			return spew.Sdump(path)
 		}),
 	)
 

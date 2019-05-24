3df0821aa3de806755b5e3448fe6b8b04455d428
Johan T. Halseth <johanth@gmail.com>
2019-05-24 14:17:48 +0200

    make: add backend tag for itests
    Defaults to running itests with btcd.

--- make/testing_flags.mk
+++ make/testing_flags.mk
@@ -55,4 +55,12 @@ endif
 
 # Construct the integration test command with the added build flags.
 ITEST_TAGS := $(DEV_TAGS) rpctest chainrpc walletrpc signrpc invoicesrpc autopilotrpc routerrpc
+
+# Default to btcd backend if not set.
+ifneq ($(backend),)
+ITEST_TAGS += ${backend}
+else
+ITEST_TAGS += btcd
+endif
+
 ITEST := rm output*.log; date; $(GOTEST) -tags="$(ITEST_TAGS)" $(TEST_FLAGS) -logoutput

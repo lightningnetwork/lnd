0adc45ab5a4f675438cda15b4c768325c6ae6d43
Conner Fromknecht <conner@lightning.engineering>
2019-06-17 11:09:27 -0700

    rpcserver: wrap closure args to 80 chars

--- rpcserver.go
+++ rpcserver.go
@@ -3815,7 +3815,8 @@ func (r *rpcServer) GetNodeInfo(ctx context.Context,
 		channels      []*lnrpc.ChannelEdge
 	)
 
-	if err := node.ForEachChannel(nil, func(_ *bbolt.Tx, edge *channeldb.ChannelEdgeInfo,
+	if err := node.ForEachChannel(nil, func(_ *bbolt.Tx,
+		edge *channeldb.ChannelEdgeInfo,
 		c1, c2 *channeldb.ChannelEdgePolicy) error {
 
 		numChannels++

3704c0e14978e24f4bd574657f0df25c48f69e51
Oliver Gugger <gugger@gmail.com>
2019-07-22 21:52:36 +0200

    keychain: fix comment about derivation path

--- keychain/derivation.go
+++ keychain/derivation.go
@@ -45,7 +45,7 @@ var (
 // The key derivation in this file follows the following hierarchy based on
 // BIP43:
 //
-//   * m/1017'/coinType'/keyFamily/0/index
+//   * m/1017'/coinType'/keyFamily'/0/index
 type KeyFamily uint32
 
 const (
@@ -110,9 +110,9 @@ const (
 // Version 0 of our key derivation schema uses the following BIP43-like
 // derivation:
 //
-//   * m/201'/coinType'/keyFamily/0/index
+//   * m/1017'/coinType'/keyFamily'/0/index
 //
-// Our purpose is 201 (chosen arbitrary for now), and the coin type will vary
+// Our purpose is 1017 (chosen arbitrary for now), and the coin type will vary
 // based on which coin/chain the channels are being created on. The key family
 // are actually just individual "accounts" in the nomenclature of BIP43. By
 // default we assume a branch of 0 (external). Finally, the key index (which

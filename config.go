1d113c9413cc6497d81a2e6d426d2bddf52cd204
Micah Lerner <mlerner789@gmail.com>
2019-10-14 18:03:34 -0700

    config: add SafeMode configuration parameter

--- config.go
+++ config.go
@@ -273,6 +273,7 @@ type config struct {
 
 	UnsafeDisconnect   bool   `long:"unsafe-disconnect" description:"Allows the rpcserver to intentionally disconnect from peers with open channels. USED FOR TESTING ONLY."`
 	UnsafeReplay       bool   `long:"unsafe-replay" description:"Causes a link to replay the adds on its commitment txn after starting up, this enables testing of the sphinx replay logic."`
+	SafeMode           bool   `long:"safemode" description:"Start in safe mode that forbids broadcasting of all commitment transactions"`
 	MaxPendingChannels int    `long:"maxpendingchannels" description:"The maximum number of incoming pending channels permitted per peer."`
 	BackupFilePath     string `long:"backupfilepath" description:"The target location of the channel backup file"`
 

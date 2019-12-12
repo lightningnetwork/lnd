9cd40c5950bc4b8a0fcd2298e46aabe04b2ab8be
Olaoluwa Osuntokun <laolu32@gmail.com>
2019-12-11 16:55:34 -0800

    build: bump version to v0.8.2

--- build/version.go
+++ build/version.go
@@ -23,7 +23,7 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 const (
 	appMajor uint = 0
 	appMinor uint = 8
-	appPatch uint = 0
+	appPatch uint = 2
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.

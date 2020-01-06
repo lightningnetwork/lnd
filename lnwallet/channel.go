2c46640dd043a1c98306472cc7cfbae193d22b8e
Johan T. Halseth <johanth@gmail.com>
2020-01-06 11:42:02 +0100

    channeldb+lnwallet: note that balance is after subtracting commit fee
    It was incorrectly stated that the commitment balance was before
    subctracting the commit fee, which led to some confusion.

--- lnwallet/channel.go
+++ lnwallet/channel.go
@@ -502,7 +502,7 @@ type commitment struct {
 	// evaluating all the add/remove/settle log entries before the listed
 	// indexes.
 	//
-	// NOTE: This is the balance *before* subtracting any commitment fee.
+	// NOTE: This is the balance *after* subtracting any commitment fee.
 	ourBalance   lnwire.MilliSatoshi
 	theirBalance lnwire.MilliSatoshi
 

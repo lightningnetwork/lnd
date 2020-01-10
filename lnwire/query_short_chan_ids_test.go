6a0e57284b94273bb68784e20bb9bcf52cd0d654
Bastien Teinturier <bastuc@hotmail.fr>
2020-01-10 17:29:45 +0100

    QueryShortChanIds: add test for zlib empty list
    When there are no shortChannelIds in the list, there's an issue
    with the zlib encoder.
    Instead of encoding nothing, it outputs 11 bytes.
    That's because `var buf bytes.Buffer` initializes a Buffer of size 11.

--- lnwire/query_short_chan_ids_test.go
+++ lnwire/query_short_chan_ids_test.go
@@ -2,7 +2,11 @@ package lnwire
 
 import (
 	"bytes"
+	"encoding/hex"
+	"reflect"
 	"testing"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 type unsortedSidTest struct {
@@ -69,6 +73,55 @@ func TestQueryShortChanIDsUnsorted(t *testing.T) {
 			if _, ok := err.(ErrUnsortedSIDs); !ok {
 				t.Fatalf("expected ErrUnsortedSIDs, got: %T",
 					err)
+			}
+		})
+	}
+}
+
+func TestQueryShortChanIDsEmpty(t *testing.T) {
+	emptyChannelsTests := []struct {
+		name     string
+		encType  ShortChanIDEncoding
+		expected string
+	}{
+		{
+			name:     "plain",
+			encType:  EncodingSortedPlain,
+			expected: "0000000000000000000000000000000000000000000000000000000000000000000100",
+		},
+		{
+			name:     "zlib",
+			encType:  EncodingSortedZlib,
+			expected: "0000000000000000000000000000000000000000000000000000000000000000000101",
+		},
+	}
+
+	for _, test := range emptyChannelsTests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			req := QueryShortChanIDs{
+				EncodingType: test.encType,
+				ShortChanIDs: nil,
+			}
+
+			var b bytes.Buffer
+			err := req.Encode(&b, 0)
+			if err != nil {
+				t.Fatalf("unable to encode req: %v", err)
+			}
+			if hex.EncodeToString(b.Bytes()) != test.expected {
+				t.Fatalf("results don't match: expected %v got %v",
+					test.expected, hex.EncodeToString(b.Bytes()))
+			}
+
+			var req2 QueryShortChanIDs
+			err = req2.Decode(bytes.NewReader(b.Bytes()), 0)
+			if err != nil {
+				t.Fatalf("unable to decode req: %v", err)
+			}
+			if !reflect.DeepEqual(req, req2) {
+				t.Fatalf("requests don't match: expected %v got %v",
+					spew.Sdump(req), spew.Sdump(req2))
 			}
 		})
 	}

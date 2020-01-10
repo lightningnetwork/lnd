e9fa13ee9a8f6391bc8e8ff352433204e67cb241
Bastien Teinturier <bastuc@hotmail.fr>
2020-01-10 17:31:19 +0100

    QueryShortChanIds: fix handling of empty lists
    If the list is empty, no need to go in the encoding code.
    Note that we could also choose to encode `len=0`, the spec
    isn't very clear on what we should do in that case.

--- lnwire/query_short_chan_ids.go
+++ lnwire/query_short_chan_ids.go
@@ -147,6 +147,9 @@ func decodeShortChanIDs(r io.Reader) (ShortChanIDEncoding, []ShortChannelID, err
 	// Before continuing, we'll snip off the first byte of the query body
 	// as that was just the encoding type.
 	queryBody = queryBody[1:]
+	if len(queryBody) == 0 {
+		return encodingType, nil, nil
+	}
 
 	// Otherwise, depending on the encoding type, we'll decode the encode
 	// short channel ID's in a different manner.
@@ -293,6 +296,13 @@ func encodeShortChanIDs(w io.Writer, encodingType ShortChanIDEncoding,
 			return shortChanIDs[i].ToUint64() <
 				shortChanIDs[j].ToUint64()
 		})
+	}
+
+	if len(shortChanIDs) == 0 {
+		if err := WriteElements(w, uint16(1)); err != nil {
+			return err
+		}
+		return WriteElements(w, encodingType)
 	}
 
 	switch encodingType {

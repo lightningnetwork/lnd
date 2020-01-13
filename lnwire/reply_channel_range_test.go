34eede0957d500845194758cea4eb282ada8dcf0
Bastien Teinturier <bastuc@hotmail.fr>
2020-01-13 17:04:09 +0100

    ReplyChannelRange: add failing test for zlib issue
    When an empty shortChanIDs list is provided, the zlib buffer creation
    returns an error.

--- lnwire/reply_channel_range_test.go
+++ lnwire/reply_channel_range_test.go
@@ -2,7 +2,11 @@ package lnwire
 
 import (
 	"bytes"
+	"encoding/hex"
+	"reflect"
 	"testing"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 // TestReplyChannelRangeUnsorted tests that decoding a ReplyChannelRange request
@@ -28,6 +32,63 @@ func TestReplyChannelRangeUnsorted(t *testing.T) {
 			if _, ok := err.(ErrUnsortedSIDs); !ok {
 				t.Fatalf("expected ErrUnsortedSIDs, got: %T",
 					err)
+			}
+		})
+	}
+}
+
+// TestReplyChannelRangeEmpty tests encoding and decoding a ReplyChannelRange
+// that doesn't contain any channel results.
+func TestReplyChannelRangeEmpty(t *testing.T) {
+	emptyChannelsTests := []struct {
+		name       string
+		encType    ShortChanIDEncoding
+		encodedHex string
+	}{
+		{
+			name:       "empty plain encoding",
+			encType:    EncodingSortedPlain,
+			encodedHex: "0000000000000000000000000000000000000000000000000000000000000000000000010000000201000100",
+		},
+		{
+			name:       "empty zlib encoding",
+			encType:    EncodingSortedZlib,
+			encodedHex: "0000000000000000000000000000000000000000000000000000000000000000000000010000000201000101",
+		},
+	}
+
+	for _, test := range emptyChannelsTests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			req := ReplyChannelRange{
+				QueryChannelRange: QueryChannelRange{
+					FirstBlockHeight: 1,
+					NumBlocks:        2,
+				},
+				Complete:     1,
+				EncodingType: test.encType,
+				ShortChanIDs: nil,
+			}
+
+			var req2 ReplyChannelRange
+			b, _ := hex.DecodeString(test.encodedHex)
+			err := req2.Decode(bytes.NewReader(b), 0)
+			if err != nil {
+				t.Fatalf("unable to decode req: %v", err)
+			}
+			if !reflect.DeepEqual(req, req2) {
+				t.Fatalf("requests don't match: expected %v got %v",
+					spew.Sdump(req), spew.Sdump(req2))
+			}
+
+			var b2 bytes.Buffer
+			err = req.Encode(&b2, 0)
+			if err != nil {
+				t.Fatalf("unable to encode req: %v", err)
+			}
+			if !bytes.Equal(b, b2.Bytes()) {
+				t.Fatalf("encoded requests don't match: expected %x got %x",
+					b, b2.Bytes())
 			}
 		})
 	}

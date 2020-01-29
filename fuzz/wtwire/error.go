bdb20d00532f19177467547cb07d27d9dffa1d28
nsa <elzeigel@gmail.com>
2020-01-28 20:20:35 -0500

    fuzz/wtwire: adding wtwire fuzzers

---
+++ fuzz/wtwire/error.go
@@ -0,0 +1,20 @@
+// +build gofuzz
+
+package wtwirefuzz
+
+import (
+	"github.com/lightningnetwork/lnd/watchtower/wtwire"
+)
+
+// Fuzz_error is used by go-fuzz.
+func Fuzz_error(data []byte) int {
+	// Prefix with MsgError.
+	data = prefixWithMsgType(data, wtwire.MsgError)
+
+	// Create an empty message so that the FuzzHarness func can check if the
+	// max payload constraint is violated.
+	emptyMsg := wtwire.Error{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}
---
+++ fuzz/wtwire/init.go
@@ -0,0 +1,20 @@
+// +build gofuzz
+
+package wtwirefuzz
+
+import (
+	"github.com/lightningnetwork/lnd/watchtower/wtwire"
+)
+
+// Fuzz_init is used by go-fuzz.
+func Fuzz_init(data []byte) int {
+	// Prefix with MsgInit.
+	data = prefixWithMsgType(data, wtwire.MsgInit)
+
+	// Create an empty message so that the FuzzHarness func can check if the
+	// max payload constraint is violated.
+	emptyMsg := wtwire.Init{}
+
+	// Pass the message into our general fuzz harness for wire messages!
+	return harness(data, &emptyMsg)
+}

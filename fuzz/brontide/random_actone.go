116c5469bc2a51919dcac2d639f14b228641ac73
nsa <elzeigel@gmail.com>
2020-01-30 13:40:01 -0500

    fuzz/brontide: random+static harnesses for acts 1-3

---
+++ fuzz/brontide/random_actone.go
@@ -0,0 +1,30 @@
+// +build gofuzz
+
+package brontidefuzz
+
+import (
+	"github.com/lightningnetwork/lnd/brontide"
+)
+
+// Fuzz_random_actone is a go-fuzz harness for ActOne in the brontide
+// handshake.
+func Fuzz_random_actone(data []byte) int {
+	// Check if data is large enough.
+	if len(data) < brontide.ActOneSize {
+		return -1
+	}
+
+	// This will return brontide machines with random keys.
+	_, responder := getBrontideMachines()
+
+	// Copy data into [ActOneSize]byte.
+	var actOne [brontide.ActOneSize]byte
+	copy(actOne[:], data)
+
+	// Responder receives ActOne, should fail on the MAC check.
+	if err := responder.RecvActOne(actOne); err == nil {
+		nilAndPanic(nil, responder, nil)
+	}
+
+	return 1
+}
---
+++ fuzz/brontide/static_actone.go
@@ -0,0 +1,30 @@
+// +build gofuzz
+
+package brontidefuzz
+
+import (
+	"github.com/lightningnetwork/lnd/brontide"
+)
+
+// Fuzz_static_actone is a go-fuzz harness for ActOne in the brontide
+// handshake.
+func Fuzz_static_actone(data []byte) int {
+	// Check if data is large enough.
+	if len(data) < brontide.ActOneSize {
+		return -1
+	}
+
+	// This will return brontide machines with static keys.
+	_, responder := getStaticBrontideMachines()
+
+	// Copy data into [ActOneSize]byte.
+	var actOne [brontide.ActOneSize]byte
+	copy(actOne[:], data)
+
+	// Responder receives ActOne, should fail.
+	if err := responder.RecvActOne(actOne); err == nil {
+		nilAndPanic(nil, responder, nil)
+	}
+
+	return 1
+}

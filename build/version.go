8aead58288bad3c14a4c4098e5eb24f44ddfad91
Olaoluwa Osuntokun <laolu32@gmail.com>
2020-02-28 18:05:24 -0300

    build: update to version v0.9.1-beta

--- build/version.go
+++ build/version.go
@@ -27,7 +27,7 @@ const (
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
-	appPreRelease = "beta.rc1"
+	appPreRelease = "beta"
 )
 
 // Version returns the application version as a properly formed string per the

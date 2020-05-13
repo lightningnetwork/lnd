0cd6694303b5958117c00922fb2d0c85d5a79fed
Oliver Gugger <gugger@gmail.com>
2020-05-13 15:57:30 +0200

    config+lncfg: move config structs to lncfg, drop suffix

---
+++ lncfg/btcd.go
@@ -0,0 +1,11 @@
+package lncfg
+
+// Btcd holds the configuration options for the daemon's connection to btcd.
+type Btcd struct {
+	Dir        string `long:"dir" description:"The base directory that contains the node's data, logs, configuration file, etc."`
+	RPCHost    string `long:"rpchost" description:"The daemon's rpc listening address. If a port is omitted, then the default port for the selected chain parameters will be used."`
+	RPCUser    string `long:"rpcuser" description:"Username for RPC connections"`
+	RPCPass    string `long:"rpcpass" default-mask:"-" description:"Password for RPC connections"`
+	RPCCert    string `long:"rpccert" description:"File containing the daemon's certificate file"`
+	RawRPCCert string `long:"rawrpccert" description:"The raw bytes of the daemon's PEM-encoded certificate chain which will be used to authenticate the RPC connection."`
+}

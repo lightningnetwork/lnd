af01571fc61dfab2f5813050dc59c40e7c00be20
Yaacov Akiba Slama <ya@slamail.org>
2020-06-28 16:51:42 +0300

    Let invoice.Encode receive a function which hashes itself the message
    Modify the SignCompact function passed to invoice.Encode to receive the
    message before it's hashed and hash it itself.
    With this modification, the SignMessage rpc function from the signrpc
    subserver can be used and an invoice can be encoded outside of lnd.

--- zpay32/encode.go
+++ zpay32/encode.go
@@ -70,12 +70,11 @@ func (invoice *Invoice) Encode(signer MessageSigner) (string, error) {
 	}
 
 	toSign := append([]byte(hrp), taggedFieldsBytes...)
-	hash := chainhash.HashB(toSign)
 
 	// We use compact signature format, and also encoded the recovery ID
 	// such that a reader of the invoice can recover our pubkey from the
 	// signature.
-	sign, err := signer.SignCompact(hash)
+	sign, err := signer.SignCompact(toSign)
 	if err != nil {
 		return "", err
 	}
@@ -95,6 +94,7 @@ func (invoice *Invoice) Encode(signer MessageSigner) (string, error) {
 				"signature: %v", err)
 		}
 
+		hash := chainhash.HashB(toSign)
 		valid := signature.Verify(hash, invoice.Destination)
 		if !valid {
 			return "", fmt.Errorf("signature does not match " +

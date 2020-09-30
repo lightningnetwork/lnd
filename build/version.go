7f34774529fa0964d47fc418d4d2965435cbfdc0
Olaoluwa Osuntokun <laolu32@gmail.com>
2020-09-30 16:48:55 -0700

    build: bump version to v0.11.1-beta

--- build/version.go
+++ build/version.go
@@ -48,7 +48,7 @@ const (
 
 	// AppPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
-	AppPreRelease = "beta.rc5"
+	AppPreRelease = "beta"
 )
 
 func init() {

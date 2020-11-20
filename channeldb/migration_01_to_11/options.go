d6f3d3ec5e6b5528bfa35add36bdf39176954301
yyforyongyu <yy2452@columbia.edu>
2020-11-20 23:04:27 +0800

    migration: add DBTimeout param in kvdb.Create/kvdb.Open

--- channeldb/migration_01_to_11/options.go
+++ channeldb/migration_01_to_11/options.go
@@ -1,5 +1,7 @@
 package migration_01_to_11
 
+import "time"
+
 const (
 	// DefaultRejectCacheSize is the default number of rejectCacheEntries to
 	// cache for use in the rejection cache of incoming gossip traffic. This
@@ -10,6 +12,10 @@ const (
 	// in order to reply to gossip queries. This produces a cache size of
 	// around 40MB.
 	DefaultChannelCacheSize = 20000
+
+	// DefaultDBTimeout specifies the default timeout value when opening
+	// the bbolt database.
+	DefaultDBTimeout = time.Second * 60
 )
 
 // Options holds parameters for tuning and customizing a channeldb.DB.
@@ -26,6 +32,10 @@ type Options struct {
 	// freelist to disk, resulting in improved performance at the expense of
 	// increased startup time.
 	NoFreelistSync bool
+
+	// DBTimeout specifies the timeout value to use when opening the wallet
+	// database.
+	DBTimeout time.Duration
 }
 
 // DefaultOptions returns an Options populated with default values.
@@ -34,6 +44,7 @@ func DefaultOptions() Options {
 		RejectCacheSize:  DefaultRejectCacheSize,
 		ChannelCacheSize: DefaultChannelCacheSize,
 		NoFreelistSync:   true,
+		DBTimeout:        DefaultDBTimeout,
 	}
 }
 

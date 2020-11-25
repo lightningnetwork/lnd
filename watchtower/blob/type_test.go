3856acce50dff5869f36cac659deeb7f087562e7
Conner Fromknecht <conner@lightning.engineering>
2020-11-25 15:04:00 -0800

    wathtower/blob+wtserver: support anchor sessions

--- watchtower/blob/type_test.go
+++ watchtower/blob/type_test.go
@@ -123,6 +123,12 @@ func TestSupportedTypes(t *testing.T) {
 		t.Fatalf("default type %s is not supported", blob.TypeAltruistCommit)
 	}
 
+	// Assert that the altruist anchor commit types are supported.
+	if !blob.IsSupportedType(blob.TypeAltruistAnchorCommit) {
+		t.Fatalf("default type %s is not supported",
+			blob.TypeAltruistAnchorCommit)
+	}
+
 	// Assert that all claimed supported types are actually supported.
 	for _, supType := range blob.SupportedTypes() {
 		if blob.IsSupportedType(supType) {

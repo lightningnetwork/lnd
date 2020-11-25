a5c40858c91cf4882b3aee0687efab0ebf12b776
Conner Fromknecht <conner@lightning.engineering>
2020-11-25 15:07:42 -0800

    lnrpc/wtclientrpc: add anchor_client bool to all requests

--- lnrpc/wtclientrpc/wtclient.pb.gw.go
+++ lnrpc/wtclientrpc/wtclient.pb.gw.go
@@ -254,9 +254,20 @@ func local_request_WatchtowerClient_Stats_0(ctx context.Context, marshaler runti
 
 }
 
+var (
+	filter_WatchtowerClient_Policy_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_WatchtowerClient_Policy_0(ctx context.Context, marshaler runtime.Marshaler, client WatchtowerClientClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq PolicyRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_WatchtowerClient_Policy_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := client.Policy(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -266,6 +277,10 @@ func request_WatchtowerClient_Policy_0(ctx context.Context, marshaler runtime.Ma
 func local_request_WatchtowerClient_Policy_0(ctx context.Context, marshaler runtime.Marshaler, server WatchtowerClientServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq PolicyRequest
 	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_WatchtowerClient_Policy_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := server.Policy(ctx, &protoReq)
 	return msg, metadata, err

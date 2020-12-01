17180296f22784bf8a44a4dac4e512da53e2b825
MrManPew <35897350+MrManPew@users.noreply.github.com>
2020-12-01 12:52:43 -0500

    fix tabs

--- config.go
+++ config.go
@@ -504,7 +504,7 @@ func LoadConfig() (*Config, error) {
 		}
 	}
 	
-  // If user supplied alternative config, check if it exists.
+	// If user supplied alternative config, check if it exists.
 	if configFilePath != DefaultConfigFile {
 		if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
 			return nil, fmt.Errorf("command line config file does not exist " +

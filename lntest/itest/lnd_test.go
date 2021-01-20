9a85ae685990a74d859a34bbad71aae1d000be1d
Johan T. Halseth <johanth@gmail.com>
2021-01-20 13:00:29 +0100

    itest: moar coins for carol

--- lntest/itest/lnd_test.go
+++ lntest/itest/lnd_test.go
@@ -6638,6 +6638,12 @@ func testInvoiceRoutingHints(net *lntest.NetworkHarness, t *harnessTest) {
 	defer shutdownAndAssert(net, t, carol)
 
 	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
+	err = net.SendCoins(ctxt, btcutil.SatoshiPerBitcoin, carol)
+	if err != nil {
+		t.Fatalf("unable to send coins to carol: %v", err)
+	}
+
+	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
 	if err := net.ConnectNodes(ctxt, net.Alice, carol); err != nil {
 		t.Fatalf("unable to connect alice to carol: %v", err)
 	}

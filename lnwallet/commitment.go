bc238ee84c5d6688a9900f5a313d4cc12662a218
eugene <elzeigel@gmail.com>
2021-02-22 12:07:21 -0500

    lnd+lnwallet: make capacity check stricter by adding fee
    Also patches breacharbiter tests by using the correct commitment
    fee and balances.

--- lnwallet/commitment.go
+++ lnwallet/commitment.go
@@ -589,11 +589,11 @@ func (cb *CommitmentBuilder) createUnsignedCommitmentTx(ourBalance,
 	for _, txOut := range commitTx.TxOut {
 		totalOut += btcutil.Amount(txOut.Value)
 	}
-	if totalOut > cb.chanState.Capacity {
+	if totalOut+commitFee > cb.chanState.Capacity {
 		return nil, fmt.Errorf("height=%v, for ChannelPoint(%v) "+
 			"attempts to consume %v while channel capacity is %v",
 			height, cb.chanState.FundingOutpoint,
-			totalOut, cb.chanState.Capacity)
+			totalOut+commitFee, cb.chanState.Capacity)
 	}
 
 	return &unsignedCommitmentTx{

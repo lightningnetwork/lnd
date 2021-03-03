dcac5a87f4b8c18832d59fc61e6e2f28a749be18
Wilmer Paulino <wilmer.paulino@gmail.com>
2021-03-03 15:12:25 -0800

    lncfg: expose channel update rate limiting options

--- server.go
+++ server.go
@@ -823,6 +823,8 @@ func newServer(cfg *Config, listenAddrs []net.Addr,
 		SubBatchDelay:           time.Second * 5,
 		IgnoreHistoricalFilters: cfg.IgnoreHistoricalGossipFilters,
 		PinnedSyncers:           cfg.Gossip.PinnedSyncers,
+		MaxChannelUpdateBurst:   cfg.Gossip.MaxChannelUpdateBurst,
+		ChannelUpdateInterval:   cfg.Gossip.ChannelUpdateInterval,
 	},
 		nodeKeyECDH.PubKey(),
 	)

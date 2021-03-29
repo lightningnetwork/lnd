c43a9ea71f0cbf33decf0a7070624630f4f8bae5
Oliver Gugger <gugger@gmail.com>
2021-03-29 10:01:09 +0200

    lncfg+sample-lnd.conf: add --neutrino.validatechannels

--- lncfg/neutrino.go
+++ lncfg/neutrino.go
@@ -14,4 +14,5 @@ type Neutrino struct {
 	AssertFilterHeader string        `long:"assertfilterheader" description:"Optional filter header in height:hash format to assert the state of neutrino's filter header chain on startup. If the assertion does not hold, then the filter header chain will be re-synced from the genesis block."`
 	UserAgentName      string        `long:"useragentname" description:"Used to help identify ourselves to other bitcoin peers"`
 	UserAgentVersion   string        `long:"useragentversion" description:"Used to help identify ourselves to other bitcoin peers"`
+	ValidateChannels   bool          `long:"validatechannels" description:"Validate every channel in the graph during sync by downloading the containing block. This is the inverse of routing.assumechanvalid, meaning that for Neutrino the validation is turned off by default for massively increased graph sync performance. This speedup comes at the risk of using an unvalidated view of the network for routing. Overwrites the value of routing.assumechanvalid if Neutrino is used. (default: false)"`
 }

6c27de7d1116fb8b09c2d0576992f700346818f8
Olaoluwa Osuntokun <laolu32@gmail.com>
2021-04-02 15:02:08 -0700

    routing: add strict pruning as new flag, default for neutrino
    In this commit we add a new flag that controls if lnd will do strict
    zombie pruning or not. If true, then this will cause lnd to maintain a
    tighter graph as it wants both edges to always be live. We enable this by
    default for neutrino as without this, it's possible that a node never
    sees both edges begin disabled, so those edges are never actually pruned
    from the graph.

--- lncfg/routing.go
+++ lncfg/routing.go
@@ -2,5 +2,7 @@ package lncfg
 
 // Routing holds the configuration options for routing.
 type Routing struct {
-	AssumeChannelValid bool `long:"assumechanvalid" description:"DEPRECATED: This is now turned on by default for Neutrino (use neutrino.validatechannels=true to turn off) and shouldn't be used for any other backend! (default: false)"`
+	AssumeChannelValid bool `long:"assumechanvalid" description:"Skip checking channel spentness during graph validation. This speedup comes at the risk of using an unvalidated view of the network for routing. (default: false)"`
+
+	StrictZombiePruning bool `long:"strictgraphpruning" description:"If true, then the graph will be pruned more aggressively for zombies. In practice this means that edges with a single stale edge will be considered a zombie."`
 }

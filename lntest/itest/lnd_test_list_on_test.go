152ba708c80a021cebbab764b5908d1811da0ce8
Oliver Gugger <gugger@gmail.com>
2021-06-07 11:16:39 +0200

    itest: add test for fully external PSBT funding

--- lntest/itest/lnd_test_list_on_test.go
+++ lntest/itest/lnd_test_list_on_test.go
@@ -275,6 +275,10 @@ var allTestCases = []*testCase{
 		test: testPsbtChanFunding,
 	},
 	{
+		name: "psbt channel funding external",
+		test: testPsbtChanFundingExternal,
+	},
+	{
 		name: "batch channel funding",
 		test: testBatchChanFunding,
 	},

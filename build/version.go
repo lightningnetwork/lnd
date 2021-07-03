e0aea2fc94697b9ef383411309dec85e5e17245a
Olaoluwa Osuntokun <laolu32@gmail.com>
2021-07-02 17:42:29 -0700

    build: bump version to v0.13.1-beta.rc1

--- build/version.go
+++ build/version.go
@@ -44,11 +44,11 @@ const (
 	AppMinor uint = 13
 
 	// AppPatch defines the application patch for this binary.
-	AppPatch uint = 99
+	AppPatch uint = 1
 
 	// AppPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
-	AppPreRelease = "beta"
+	AppPreRelease = "beta.rc1"
 )
 
 func init() {

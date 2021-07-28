a35031ca2e9f8fad354adb1bb43ed6b41f00dbf7
Oliver Gugger <gugger@gmail.com>
2021-07-28 10:58:41 +0200

    healthcheck: fix missing import

--- healthcheck/diskcheck_js.go
+++ healthcheck/diskcheck_js.go
@@ -1,13 +1,15 @@
 package healthcheck
 
+import "fmt"
+
 // AvailableDiskSpaceRatio returns ratio of available disk space to total
 // capacity.
-func AvailableDiskSpaceRatio(path string) (float64, error) {
+func AvailableDiskSpaceRatio(_ string) (float64, error) {
 	return 0, fmt.Errorf("disk space check not supported in WebAssembly")
 }
 
 // AvailableDiskSpace returns the available disk space in bytes of the given
 // file system.
-func AvailableDiskSpace(path string) (uint64, error) {
+func AvailableDiskSpace(_ string) (uint64, error) {
 	return 0, fmt.Errorf("disk space check not supported in WebAssembly")
 }

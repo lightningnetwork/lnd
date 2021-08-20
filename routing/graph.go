73598b1fd560185aff4130f4995eb981ac9ffb91
Oliver Gugger <gugger@gmail.com>
2021-08-20 14:28:49 +0200

    routing+server: use cached graph interface

--- routing/graph.go
+++ routing/graph.go
@@ -6,9 +6,9 @@ import (
 	"github.com/lightningnetwork/lnd/routing/route"
 )
 
-// routingGraph is an abstract interface that provides information about nodes
+// Graph is an abstract interface that provides information about nodes
 // and edges to pathfinding.
-type routingGraph interface {
+type Graph interface {
 	// forEachNodeChannel calls the callback for every channel of the given node.
 	forEachNodeChannel(nodePub route.Vertex,
 		cb func(channel *channeldb.DirectedChannel) error) error
@@ -20,22 +20,22 @@ type routingGraph interface {
 	fetchNodeFeatures(nodePub route.Vertex) (*lnwire.FeatureVector, error)
 }
 
-// dbRoutingTx is a routingGraph implementation that retrieves from the
+// CachedGraph is a Graph implementation that retrieves from the
 // database.
-type dbRoutingTx struct {
+type CachedGraph struct {
 	graph  *channeldb.ChannelGraph
 	source route.Vertex
 }
 
-// newDbRoutingTx instantiates a new db-connected routing graph. It implictly
+// NewCachedGraph instantiates a new db-connected routing graph. It implictly
 // instantiates a new read transaction.
-func newDbRoutingTx(graph *channeldb.ChannelGraph) (*dbRoutingTx, error) {
+func NewCachedGraph(graph *channeldb.ChannelGraph) (*CachedGraph, error) {
 	sourceNode, err := graph.SourceNode()
 	if err != nil {
 		return nil, err
 	}
 
-	return &dbRoutingTx{
+	return &CachedGraph{
 		graph:  graph,
 		source: sourceNode.PubKeyBytes,
 	}, nil
@@ -43,8 +43,8 @@ func newDbRoutingTx(graph *channeldb.ChannelGraph) (*dbRoutingTx, error) {
 
 // forEachNodeChannel calls the callback for every channel of the given node.
 //
-// NOTE: Part of the routingGraph interface.
-func (g *dbRoutingTx) forEachNodeChannel(nodePub route.Vertex,
+// NOTE: Part of the Graph interface.
+func (g *CachedGraph) forEachNodeChannel(nodePub route.Vertex,
 	cb func(channel *channeldb.DirectedChannel) error) error {
 
 	return g.graph.ForEachNodeChannel(nodePub, cb)
@@ -52,16 +52,16 @@ func (g *dbRoutingTx) forEachNodeChannel(nodePub route.Vertex,
 
 // sourceNode returns the source node of the graph.
 //
-// NOTE: Part of the routingGraph interface.
-func (g *dbRoutingTx) sourceNode() route.Vertex {
+// NOTE: Part of the Graph interface.
+func (g *CachedGraph) sourceNode() route.Vertex {
 	return g.source
 }
 
 // fetchNodeFeatures returns the features of the given node. If the node is
 // unknown, assume no additional features are supported.
 //
-// NOTE: Part of the routingGraph interface.
-func (g *dbRoutingTx) fetchNodeFeatures(nodePub route.Vertex) (
+// NOTE: Part of the Graph interface.
+func (g *CachedGraph) fetchNodeFeatures(nodePub route.Vertex) (
 	*lnwire.FeatureVector, error) {
 
 	return g.graph.FetchNodeFeatures(nodePub)

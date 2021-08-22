6d7799373586cae952d82f0badeb7bbc0a1bcec7
3nprob <3nprob@3nprob>
2021-08-22 12:28:00 +0900

    tor+lncfg: Allow bypassing tor for specific networks/domains/hosts
    Defaults to loopback (127.0.0.0/8, ::1/128, localhost)
    
    Routing local connections over tor is practically always unintended
    and a potential security risk.

--- config.go
+++ config.go
@@ -81,6 +81,7 @@ const (
 	defaultTorControlPort          = 9051
 	defaultTorV2PrivateKeyFilename = "v2_onion_private_key"
 	defaultTorV3PrivateKeyFilename = "v3_onion_private_key"
+	defaultTorNoProxyTargets       = "localhost,::1/128,127.0.0.0/8"
 
 	// DefaultAutogenValidity is the default validity of a self-signed
 	// certificate. The value corresponds to 14 months
@@ -522,9 +523,10 @@ func DefaultConfig() Config {
 		NumGraphSyncPeers:             defaultMinPeers,
 		HistoricalSyncInterval:        discovery.DefaultHistoricalSyncInterval,
 		Tor: &lncfg.Tor{
-			SOCKS:   defaultTorSOCKS,
-			DNS:     defaultTorDNS,
-			Control: defaultTorControl,
+			SOCKS:          defaultTorSOCKS,
+			DNS:            defaultTorDNS,
+			Control:        defaultTorControl,
+			NoProxyTargets: defaultTorNoProxyTargets,
 		},
 		net: &tor.ClearNet{},
 		Workers: &lncfg.Workers{
@@ -1011,6 +1013,7 @@ func ValidateConfig(cfg Config, interceptor signal.Interceptor, fileParser,
 			DNS:                         cfg.Tor.DNS,
 			StreamIsolation:             cfg.Tor.StreamIsolation,
 			SkipProxyForClearNetTargets: cfg.Tor.SkipProxyForClearNetTargets,
+			NoProxyTargets:              cfg.Tor.NoProxyTargets,
 		}
 	}
 

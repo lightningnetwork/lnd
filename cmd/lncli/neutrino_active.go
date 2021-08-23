a9c2425e664474a9b7f6acef9c1211512f0319db
ErikEk <erik.ek@gmail.com>
2021-08-23 05:00:51 +0200

    neutrinorpc: command interface

---
+++ cmd/lncli/neutrino_active.go
@@ -0,0 +1,446 @@
+// +build neutrinorpc
+
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/lightningnetwork/lnd/lnrpc/neutrinorpc"
+	"github.com/lightningnetwork/lnd/tor"
+	"github.com/urfave/cli"
+)
+
+func getNeutrinoKitClient(ctx *cli.Context) (neutrinorpc.NeutrinoKitClient, func()) {
+	conn := getClientConn(ctx, false)
+
+	cleanUp := func() {
+		conn.Close()
+	}
+
+	return neutrinorpc.NewNeutrinoKitClient(conn), cleanUp
+}
+
+var getNeutrinoStatusCommand = cli.Command{
+	Name:     "status",
+	Usage:    "Returns the status of the running neutrino instance.",
+	Category: "Neutrino",
+	Description: "Returns the status of the light client neutrino " +
+		"instance, along with height and hash of the best block, and " +
+		"a list of connected peers.",
+	Action: actionDecorator(getNeutrinoStatus),
+}
+
+func getNeutrinoStatus(ctx *cli.Context) error {
+	ctxc := getContext()
+	client, cleanUp := getNeutrinoKitClient(ctx)
+	defer cleanUp()
+
+	req := &neutrinorpc.StatusRequest{}
+
+	resp, err := client.Status(ctxc, req)
+	if err != nil {
+		return err
+	}
+
+	printRespJSON(resp)
+
+	return nil
+}
+
+var addPeerCommand = cli.Command{
+	Name:     "addpeer",
+	Usage:    "Add a peer.",
+	Category: "Neutrino",
+	Description: "Adds a new peer that has already been connected to the " +
+		"server.",
+	ArgsUsage: "address",
+	Action:    actionDecorator(addNeutrinoPeer),
+}
+
+func addNeutrinoPeer(ctx *cli.Context) error {
+	ctxc := getContext()
+
+	// Display the command's help message if we do not have the expected
+	// number of arguments/flags.
+	if ctx.NArg() != 1 || ctx.NumFlags() > 0 {
+		return cli.ShowCommandHelp(ctx, "addpeer")
+	}
+
+	client, cleanUp := getNeutrinoKitClient(ctx)
+	defer cleanUp()
+
+	// Check if valid address.
+	ips, err := resolveName(ctx.Args().First())
+	if err != nil {
+		return err
+	}
+
+	req := &neutrinorpc.AddPeerRequest{
+		PeerAddrs: ips[0].String(),
+	}
+
+	// Add a peer to the neutrino server.
+	resp, err := client.AddPeer(ctxc, req)
+	if err != nil {
+		return err
+	}
+
+	printRespJSON(resp)
+
+	return nil
+}
+
+var disconnectPeerCommand = cli.Command{
+	Name:     "disconnectpeer",
+	Usage:    "Disconnect a peer.",
+	Category: "Neutrino",
+	Description: "Disconnects a peer by target address. Both outbound and" +
+		"inbound nodes will be searched for the target node. An error " +
+		"message will be returned if the peer was not found.",
+	ArgsUsage: "address",
+	Action:    actionDecorator(disconnectNeutrinoPeer),
+}
+
+func disconnectNeutrinoPeer(ctx *cli.Context) error {
+	ctxc := getContext()
+
+	// Display the command's help message if we do not have the expected
+	// number of arguments/flags.
+	if ctx.NArg() != 1 || ctx.NumFlags() > 0 {
+		return cli.ShowCommandHelp(ctx, "disconnetpeer")
+	}
+
+	client, cleanUp := getNeutrinoKitClient(ctx)
+	defer cleanUp()
+
+	// Check if valid address.
+	ips, err := resolveName(ctx.Args().First())
+	if err != nil {
+		return err
+	}
+
+	req := &neutrinorpc.DisconnectPeerRequest{
+		PeerAddrs: ips[0].String(),
+	}
+
+	// Disconnect a peer to the neutrino server.
+	resp, err := client.DisconnectPeer(ctxc, req)
+	if err != nil {
+		return err
+	}
+
+	printRespJSON(resp)
+
+	return nil
+}
+
+var isBannedCommand = cli.Command{
+	Name:        "isbanned",
+	Usage:       "Get ban status.",
+	Category:    "Neutrino",
+	Description: "Returns true if the peer is banned, otherwise false.",
+	ArgsUsage:   "address",
+	Action:      actionDecorator(isBanned),
+}
+
+func isBanned(ctx *cli.Context) error {
+	ctxc := getContext()
+
+	if ctx.NArg() != 1 {
+		return fmt.Errorf("address argument is missing")
+	}
+	client, cleanUp := getNeutrinoKitClient(ctx)
+	defer cleanUp()
+
+	// Check if valid address.
+	ips, err := resolveName(ctx.Args().First())
+	if err != nil {
+		return err
+	}
+
+	req := &neutrinorpc.IsBannedRequest{
+		PeerAddrs: ips[0].String(),
+	}
+
+	// Check if the peer is banned.
+	resp, err := client.IsBanned(ctxc, req)
+	if err != nil {
+		return err
+	}
+
+	printRespJSON(resp)
+
+	return nil
+}
+
+var getBlockHeaderCommand = cli.Command{
+	Name:        "getblockheader",
+	Usage:       "Get a block header.",
+	Category:    "Neutrino",
+	Description: "Returns a block header with a particular block hash.",
+	ArgsUsage:   "hash",
+	Action:      actionDecorator(getBlockHeader),
+}
+
+func getBlockHeader(ctx *cli.Context) error {
+	ctxc := getContext()
+	args := ctx.Args()
+
+	if !args.Present() {
+		return fmt.Errorf("block hash argument is missing")
+	}
+
+	client, cleanUp := getNeutrinoKitClient(ctx)
+	defer cleanUp()
+
+	req := &neutrinorpc.GetBlockHeaderRequest{
+		Hash: ctx.Args().First(),
+	}
+
+	resp, err := client.GetBlockHeader(ctxc, req)
+	if err != nil {
+		return err
+	}
+
+	printRespJSON(resp)
+
+	return nil
+}
+
+var getBlockCommand = cli.Command{
+	Name:        "getblock",
+	Usage:       "Get a block.",
+	Category:    "Neutrino",
+	Description: "Returns a block with a particular block hash.",
+	ArgsUsage:   "hash",
+	Action:      actionDecorator(getBlock),
+}
+
+func getBlock(ctx *cli.Context) error {
+	ctxc := getContext()
+	args := ctx.Args()
+
+	if !args.Present() {
+		return fmt.Errorf("block hash argument is missing")
+	}
+
+	client, cleanUp := getNeutrinoKitClient(ctx)
+	defer cleanUp()
+
+	req := &neutrinorpc.GetBlockRequest{
+		Hash: args.First(),
+	}
+
+	resp, err := client.GetBlock(ctxc, req)
+	if err != nil {
+		return err
+	}
+
+	printRespJSON(resp)
+
+	return nil
+}
+
+var getCFilterCommand = cli.Command{
+	Name:        "getcfilter",
+	Usage:       "Get a compact filter.",
+	Category:    "Neutrino",
+	Description: "Returns a compact filter of a particular block.",
+	ArgsUsage:   "hash",
+	Action:      actionDecorator(getCFilter),
+}
+
+func getCFilter(ctx *cli.Context) error {
+	ctxc := getContext()
+	args := ctx.Args()
+
+	if !args.Present() {
+		return fmt.Errorf("block hash argument missing")
+	}
+
+	client, cleanUp := getNeutrinoKitClient(ctx)
+	defer cleanUp()
+
+	req := &neutrinorpc.GetCFilterRequest{Hash: args.First()}
+
+	resp, err := client.GetCFilter(ctxc, req)
+	if err != nil {
+		return err
+	}
+
+	printRespJSON(resp)
+
+	return nil
+}
+
+var findTxsCommand = cli.Command{
+	Name:     "txscan",
+	Usage:    "Scan for transactions.",
+	Category: "Neutrino",
+	Description: "Trigger a rescan for transactions containing any of the " +
+		"given addresses. This may take some time depending on the initial " +
+		"conditions.",
+	ArgsUsage: "[flags] <addr> <addr> <addr>",
+	Flags: []cli.Flag{
+		cli.Int64Flag{
+			Name: "start_block",
+			Usage: "Start the rescan at this block height. If no start block is " +
+				"specified, the scan will be started from current best block.",
+		},
+		cli.Int64Flag{
+			Name:  "end_block",
+			Usage: "End the rescan at this block height.",
+		},
+		cli.Int64Flag{
+			Name:  "timeout",
+			Usage: "Rescan timeout (in seconds).",
+		},
+	},
+	Action: actionDecorator(findTxsFunc),
+}
+
+func findTxsFunc(ctx *cli.Context) error {
+	ctxc := getContext()
+	args := ctx.Args()
+
+	if !args.Present() {
+		return fmt.Errorf("txs argument missing")
+	}
+
+	if !ctx.IsSet("end_block") {
+		return fmt.Errorf("end_block must be set")
+	}
+
+	if !ctx.IsSet("timeout") {
+		return fmt.Errorf("timeout must be set")
+	}
+
+	client, cleanUp := getNeutrinoKitClient(ctx)
+	defer cleanUp()
+
+	var txs = make([]string, ctx.NArg())
+	for i := range args {
+		txs[i] = args.Get(i)
+	}
+
+	startBlock := ctx.Int64("start_block")
+	endBlock := ctx.Int64("end_block")
+	timeout := ctx.Int64("timeout")
+
+	if endBlock == 0 {
+		return fmt.Errorf(
+			"end_block must be greater than zero",
+		)
+	} else if endBlock < startBlock {
+		return fmt.Errorf(
+			"end_block must be greater than start_block",
+		)
+	}
+
+	if timeout < 0 {
+		return fmt.Errorf("timeout can not be negative")
+	}
+
+	req := &neutrinorpc.TxScanRequest{
+		Addrs:      txs,
+		StartBlock: startBlock,
+		EndBlock:   endBlock,
+		Timeout:    timeout,
+	}
+
+	resp, err := client.TxScan(ctxc, req)
+	if err != nil {
+		return err
+	}
+
+	printRespJSON(resp)
+
+	return nil
+}
+
+var stopTxScanCommad = cli.Command{
+	Name:        "stoptxscan",
+	Usage:       "Stop any tx scan.",
+	Category:    "Neutrino",
+	Description: "",
+	ArgsUsage:   "",
+	Action:      actionDecorator(stopTxScanFunc),
+}
+
+func stopTxScanFunc(ctx *cli.Context) error {
+	ctxc := getContext()
+
+	client, cleanUp := getNeutrinoKitClient(ctx)
+	defer cleanUp()
+
+	req := &neutrinorpc.StopTxScanRequest{}
+
+	resp, err := client.StopTxScan(ctxc, req)
+	if err != nil {
+		return err
+	}
+
+	printRespJSON(resp)
+
+	return nil
+}
+
+// neutrinoCommands will return the set of commands to enable for neutrinorpc
+// builds.
+func neutrinoCommands() []cli.Command {
+	return []cli.Command{
+		{
+			Name:        "neutrino",
+			Category:    "Neutrino",
+			Usage:       "Interact with a running neutrino instance.",
+			Description: "",
+			Subcommands: []cli.Command{
+				getNeutrinoStatusCommand,
+				addPeerCommand,
+				disconnectPeerCommand,
+				isBannedCommand,
+				getBlockCommand,
+				getBlockHeaderCommand,
+				getCFilterCommand,
+				findTxsCommand,
+				stopTxScanCommad,
+			},
+		},
+	}
+}
+
+func resolveName(host string) ([]net.IP, error) {
+	if tor.IsOnionHost(host) {
+		// Neutrino internally uses btcd's address
+		// manager which only operates on an IP level
+		// and does not understand onion hosts. We need
+		// to turn an onion host into a fake
+		// representation of an IP address to make it
+		// possible to connect to a block filter backend
+		// that serves on an Onion v2 hidden service.
+		fakeIP, err := tor.OnionHostToFakeIP(host)
+		if err != nil {
+			return nil, err
+		}
+
+		return []net.IP{fakeIP}, nil
+	}
+
+	addrs, err := net.LookupHost(host)
+	if err != nil {
+		return nil, err
+	}
+
+	ips := make([]net.IP, 0, len(addrs))
+	for _, strIP := range addrs {
+		ip := net.ParseIP(strIP)
+		if ip == nil {
+			continue
+		}
+
+		ips = append(ips, ip)
+	}
+
+	return ips, nil
+}

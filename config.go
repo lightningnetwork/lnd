0599171d65f1d35fddcc33b05da1a40f791fb2d2
Oliver Gugger <gugger@gmail.com>
2021-09-10 14:04:42 +0200

    multi: extract wallet initialization
    With this commit we extract the wallet creation/unlocking and
    initialization completely out of the main function. This will allow us
    to use custom implementations in the future.

--- config.go
+++ config.go
@@ -1505,13 +1505,16 @@ func (c *Config) graphDatabaseDir() string {
 
 // ImplementationConfig returns the configuration of what actual implementations
 // should be used when creating the chain control.
-func (c *Config) ImplementationConfig() *ImplementationCfg {
-	defaultImpl := &DefaultWalletImpl{}
+func (c *Config) ImplementationConfig(
+	interceptor signal.Interceptor) *ImplementationCfg {
+
+	defaultImpl := NewDefaultWalletImpl(c, ltndLog, interceptor)
 	return &ImplementationCfg{
-		GrpcRegistrar:     defaultImpl,
-		RestRegistrar:     defaultImpl,
-		ExternalValidator: defaultImpl,
-		DatabaseBuilder:   NewDefaultDatabaseBuilder(c, ltndLog),
+		GrpcRegistrar:       defaultImpl,
+		RestRegistrar:       defaultImpl,
+		ExternalValidator:   defaultImpl,
+		DatabaseBuilder:     NewDefaultDatabaseBuilder(c, ltndLog),
+		ChainControlBuilder: defaultImpl,
 	}
 }
 

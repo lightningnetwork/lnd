5d1bcaeb616477b83b2e63a5ab61d43cf9655990
Harsha Goli <harshagoli@gmail.com>
2021-09-26 21:57:04 -0400

    scripts: add arshbot key to verify script

--- scripts/verify-install.sh
+++ scripts/verify-install.sh
@@ -20,6 +20,7 @@ KEYS+=("E4D85299674B2D31FAA1892E372CBD7633C61696 https://keybase.io/roasbeef/pgp
 KEYS+=("729E9D9D92C75A5FBFEEE057B5DD717BEF7CA5B1 https://keybase.io/wpaulino/pgp_keys.asc")
 KEYS+=("7E81EF6B9989A9CC93884803118759E83439A9B1 https://keybase.io/eugene_/pgp_keys.asc")
 KEYS+=("7AB3D7F5911708842796513415BAADA29DA20D26 https://keybase.io/halseth/pgp_keys.asc")
+KEYS+=("E97A1AB6C77A1D2B72F50A6F90E00CCB1C74C611 https://keybase.io/arshbot/pgp_keys.asc")
 
 function check_command() {
   echo -n "Checking if $1 is installed... "

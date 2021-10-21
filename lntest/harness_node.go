85377a613c6cfe773ef50a499a777e68f0867edf
yyforyongyu <yy2452@columbia.edu>
2021-10-22 05:29:45 +0800

    itest: move watcher logic out of harness node

--- lntest/harness_node.go
+++ lntest/harness_node.go
@@ -305,6 +305,7 @@ type policyUpdateMap map[string]map[string][]*lnrpc.RoutingPolicy
 // harness. Each HarnessNode instance also fully embeds an RPC client in
 // order to pragmatically drive the node.
 type HarnessNode struct {
+	mu  sync.RWMutex
 	Cfg *BaseNodeConfig
 
 	// NodeID is a unique identifier for the node within a NetworkHarness.
@@ -330,8 +331,15 @@ type HarnessNode struct {
 	openChans        map[wire.OutPoint]int
 	openChanWatchers map[wire.OutPoint][]chan struct{}
 
-	closedChans       map[wire.OutPoint]struct{}
+	closedChans       map[wire.OutPoint]*lnrpc.ClosedChannelUpdate
 	closeChanWatchers map[wire.OutPoint][]chan struct{}
+
+	// numChanUpdates records the number of channel updates seen by each
+	// channel.
+	numChanUpdates map[wire.OutPoint]int
+
+	// nodeUpdates records the node announcements seen by each node.
+	nodeUpdates map[string][]*lnrpc.NodeUpdate
 
 	// policyUpdates stores a slice of seen polices by each advertising
 	// node and the outpoint.
@@ -447,13 +455,14 @@ func newNode(cfg *BaseNodeConfig) (*HarnessNode, error) {
 		chanWatchRequests: make(chan *chanWatchRequest),
 		openChans:         make(map[wire.OutPoint]int),
 		openChanWatchers:  make(map[wire.OutPoint][]chan struct{}),
-
-		closedChans:       make(map[wire.OutPoint]struct{}),
+		closedChans: make(
+			map[wire.OutPoint]*lnrpc.ClosedChannelUpdate,
+		),
 		closeChanWatchers: make(map[wire.OutPoint][]chan struct{}),
-
-		policyUpdates: policyUpdateMap{},
-
-		postgresDbName: dbName,
+		numChanUpdates:    make(map[wire.OutPoint]int),
+		nodeUpdates:       make(map[string][]*lnrpc.NodeUpdate),
+		policyUpdates:     policyUpdateMap{},
+		postgresDbName:    dbName,
 	}, nil
 }
 
@@ -509,14 +518,14 @@ func (hn *HarnessNode) String() string {
 		Name        string
 		PubKey      string
 		OpenChans   map[string]int
-		ClosedChans map[string]struct{}
+		ClosedChans map[string]int
 		NodeCfg     nodeCfg
 	}{
 		NodeID:      hn.NodeID,
 		Name:        hn.Cfg.Name,
 		PubKey:      hn.PubKeyStr,
 		OpenChans:   make(map[string]int),
-		ClosedChans: make(map[string]struct{}),
+		ClosedChans: make(map[string]int),
 		NodeCfg: nodeCfg{
 			LogFilenamePrefix: hn.Cfg.LogFilenamePrefix,
 			ExtraArgs:         hn.Cfg.ExtraArgs,
@@ -533,8 +542,8 @@ func (hn *HarnessNode) String() string {
 	for outpoint, count := range hn.openChans {
 		nodeState.OpenChans[outpoint.String()] = count
 	}
-	for outpoint, count := range hn.closedChans {
-		nodeState.ClosedChans[outpoint.String()] = count
+	for outpoint := range hn.closedChans {
+		nodeState.ClosedChans[outpoint.String()] = 1
 	}
 
 	stateBytes, err := json.MarshalIndent(nodeState, "", "\t")
@@ -1221,231 +1230,6 @@ func (hn *HarnessNode) kill() error {
 	return hn.cmd.Process.Kill()
 }
 
-type chanWatchType uint8
-
-const (
-	// watchOpenChannel specifies that this is a request to watch an open
-	// channel event.
-	watchOpenChannel chanWatchType = iota
-
-	// watchCloseChannel specifies that this is a request to watch a close
-	// channel event.
-	watchCloseChannel
-
-	// watchPolicyUpdate specifies that this is a request to watch a policy
-	// update event.
-	watchPolicyUpdate
-)
-
-// closeChanWatchRequest is a request to the lightningNetworkWatcher to be
-// notified once it's detected within the test Lightning Network, that a
-// channel has either been added or closed.
-type chanWatchRequest struct {
-	chanPoint wire.OutPoint
-
-	chanWatchType chanWatchType
-
-	eventChan chan struct{}
-
-	advertisingNode    string
-	policy             *lnrpc.RoutingPolicy
-	includeUnannounced bool
-}
-
-func (hn *HarnessNode) checkChanPointInGraph(chanPoint wire.OutPoint) bool {
-	ctxt, cancel := context.WithTimeout(hn.runCtx, DefaultTimeout)
-	defer cancel()
-
-	chanGraph, err := hn.DescribeGraph(ctxt, &lnrpc.ChannelGraphRequest{})
-	if err != nil {
-		return false
-	}
-
-	targetChanPoint := chanPoint.String()
-	for _, chanEdge := range chanGraph.Edges {
-		candidateChanPoint := chanEdge.ChanPoint
-		if targetChanPoint == candidateChanPoint {
-			return true
-		}
-	}
-
-	return false
-}
-
-// lightningNetworkWatcher is a goroutine which is able to dispatch
-// notifications once it has been observed that a target channel has been
-// closed or opened within the network. In order to dispatch these
-// notifications, the GraphTopologySubscription client exposed as part of the
-// gRPC interface is used.
-func (hn *HarnessNode) lightningNetworkWatcher() {
-	defer hn.wg.Done()
-
-	graphUpdates := make(chan *lnrpc.GraphTopologyUpdate)
-
-	// Start a goroutine to receive graph updates.
-	hn.wg.Add(1)
-	go func() {
-		defer hn.wg.Done()
-		err := hn.receiveTopologyClientStream(graphUpdates)
-
-		if err != nil {
-			hn.PrintErrf("receive topology client stream "+
-				"got err:%v", err)
-		}
-	}()
-
-	for {
-		select {
-		// A new graph update has just been received, so we'll examine
-		// the current set of registered clients to see if we can
-		// dispatch any requests.
-		case graphUpdate := <-graphUpdates:
-			hn.handleChannelEdgeUpdates(graphUpdate.ChannelUpdates)
-			hn.handleClosedChannelUpdate(graphUpdate.ClosedChans)
-			// TODO(yy): handle node updates too
-
-		// A new watch request, has just arrived. We'll either be able
-		// to dispatch immediately, or need to add the client for
-		// processing later.
-		case watchRequest := <-hn.chanWatchRequests:
-			switch watchRequest.chanWatchType {
-			case watchOpenChannel:
-				// TODO(roasbeef): add update type also, checks
-				// for multiple of 2
-				hn.handleOpenChannelWatchRequest(watchRequest)
-
-			case watchCloseChannel:
-				hn.handleCloseChannelWatchRequest(watchRequest)
-
-			case watchPolicyUpdate:
-				hn.handlePolicyUpdateWatchRequest(watchRequest)
-			}
-
-		case <-hn.runCtx.Done():
-			return
-		}
-	}
-}
-
-// WaitForNetworkChannelOpen will block until a channel with the target
-// outpoint is seen as being fully advertised within the network. A channel is
-// considered "fully advertised" once both of its directional edges has been
-// advertised within the test Lightning Network.
-func (hn *HarnessNode) WaitForNetworkChannelOpen(
-	chanPoint *lnrpc.ChannelPoint) error {
-
-	ctxt, cancel := context.WithTimeout(hn.runCtx, DefaultTimeout)
-	defer cancel()
-
-	eventChan := make(chan struct{})
-
-	op, err := MakeOutpoint(chanPoint)
-	if err != nil {
-		return fmt.Errorf("failed to create outpoint for %v "+
-			"got err: %v", chanPoint, err)
-	}
-
-	hn.chanWatchRequests <- &chanWatchRequest{
-		chanPoint:     op,
-		eventChan:     eventChan,
-		chanWatchType: watchOpenChannel,
-	}
-
-	select {
-	case <-eventChan:
-		return nil
-	case <-ctxt.Done():
-		return fmt.Errorf("channel:%s not opened before timeout: %s",
-			op, hn)
-	}
-}
-
-// WaitForNetworkChannelClose will block until a channel with the target
-// outpoint is seen as closed within the network. A channel is considered
-// closed once a transaction spending the funding outpoint is seen within a
-// confirmed block.
-func (hn *HarnessNode) WaitForNetworkChannelClose(
-	chanPoint *lnrpc.ChannelPoint) error {
-
-	ctxt, cancel := context.WithTimeout(hn.runCtx, DefaultTimeout)
-	defer cancel()
-
-	eventChan := make(chan struct{})
-
-	op, err := MakeOutpoint(chanPoint)
-	if err != nil {
-		return fmt.Errorf("failed to create outpoint for %v "+
-			"got err: %v", chanPoint, err)
-	}
-
-	hn.chanWatchRequests <- &chanWatchRequest{
-		chanPoint:     op,
-		eventChan:     eventChan,
-		chanWatchType: watchCloseChannel,
-	}
-
-	select {
-	case <-eventChan:
-		return nil
-	case <-ctxt.Done():
-		return fmt.Errorf("channel:%s not closed before timeout: "+
-			"%s", op, hn)
-	}
-}
-
-// WaitForChannelPolicyUpdate will block until a channel policy with the target
-// outpoint and advertisingNode is seen within the network.
-func (hn *HarnessNode) WaitForChannelPolicyUpdate(
-	advertisingNode string, policy *lnrpc.RoutingPolicy,
-	chanPoint *lnrpc.ChannelPoint, includeUnannounced bool) error {
-
-	ctxt, cancel := context.WithTimeout(hn.runCtx, DefaultTimeout)
-	defer cancel()
-
-	eventChan := make(chan struct{})
-
-	op, err := MakeOutpoint(chanPoint)
-	if err != nil {
-		return fmt.Errorf("failed to create outpoint for %v"+
-			"got err: %v", chanPoint, err)
-	}
-
-	ticker := time.NewTicker(wait.PollInterval)
-	defer ticker.Stop()
-
-	for {
-		select {
-		// Send a watch request every second.
-		case <-ticker.C:
-			// Did the event can close in the meantime? We want to
-			// avoid a "close of closed channel" panic since we're
-			// re-using the same event chan for multiple requests.
-			select {
-			case <-eventChan:
-				return nil
-			default:
-			}
-
-			hn.chanWatchRequests <- &chanWatchRequest{
-				chanPoint:          op,
-				eventChan:          eventChan,
-				chanWatchType:      watchPolicyUpdate,
-				policy:             policy,
-				advertisingNode:    advertisingNode,
-				includeUnannounced: includeUnannounced,
-			}
-
-		case <-eventChan:
-			return nil
-
-		case <-ctxt.Done():
-			return fmt.Errorf("channel:%s policy not updated "+
-				"before timeout: [%s:%v] %s", op,
-				advertisingNode, policy, hn.String())
-		}
-	}
-}
-
 // WaitForBlockchainSync waits for the target node to be fully synchronized
 // with the blockchain. If the passed context object has a set timeout, it will
 // continually poll until the timeout has elapsed. In the case that the chain
@@ -1516,301 +1300,6 @@ func (hn *HarnessNode) WaitForBalance(expectedBalance btcutil.Amount,
 func (hn *HarnessNode) PrintErrf(format string, a ...interface{}) {
 	fmt.Printf("itest error from [node:%s]: %s\n", // nolint:forbidigo
 		hn.Cfg.Name, fmt.Sprintf(format, a...))
-}
-
-// handleChannelEdgeUpdates takes a series of channel edge updates, extracts
-// the outpoints, and saves them to harness node's internal state.
-func (hn *HarnessNode) handleChannelEdgeUpdates(
-	updates []*lnrpc.ChannelEdgeUpdate) {
-
-	// For each new channel, we'll increment the number of
-	// edges seen by one.
-	for _, newChan := range updates {
-		op, err := MakeOutpoint(newChan.ChanPoint)
-		if err != nil {
-			hn.PrintErrf("failed to create outpoint for %v "+
-				"got err: %v", newChan.ChanPoint, err)
-			return
-		}
-		hn.openChans[op]++
-
-		// For this new channel, if the number of edges seen is less
-		// than two, then the channel hasn't been fully announced yet.
-		if numEdges := hn.openChans[op]; numEdges < 2 {
-			return
-		}
-
-		// Otherwise, we'll notify all the registered watchers and
-		// remove the dispatched watchers.
-		for _, eventChan := range hn.openChanWatchers[op] {
-			close(eventChan)
-		}
-		delete(hn.openChanWatchers, op)
-
-		// Check whether there's a routing policy update. If so, save
-		// it to the node state.
-		if newChan.RoutingPolicy == nil {
-			continue
-		}
-
-		// Append the policy to the slice.
-		node := newChan.AdvertisingNode
-		policies := hn.policyUpdates[op.String()]
-
-		// If the map[op] is nil, we need to initialize the map first.
-		if policies == nil {
-			policies = make(map[string][]*lnrpc.RoutingPolicy)
-		}
-		policies[node] = append(
-			policies[node], newChan.RoutingPolicy,
-		)
-		hn.policyUpdates[op.String()] = policies
-	}
-}
-
-// handleOpenChannelWatchRequest processes a watch open channel request by
-// checking the number of the edges seen for a given channel point. If the
-// number is no less than 2 then the channel is considered open. Otherwise, we
-// will attempt to find it in its channel graph. If neither can be found, the
-// request is added to a watch request list than will be handled by
-// handleChannelEdgeUpdates.
-func (hn *HarnessNode) handleOpenChannelWatchRequest(req *chanWatchRequest) {
-	targetChan := req.chanPoint
-
-	// If this is an open request, then it can be dispatched if the number
-	// of edges seen for the channel is at least two.
-	if numEdges := hn.openChans[targetChan]; numEdges >= 2 {
-		close(req.eventChan)
-		return
-	}
-
-	// Before we add the channel to our set of open clients, we'll check to
-	// see if the channel is already in the channel graph of the target
-	// node. This lets us handle the case where a node has already seen a
-	// channel before a notification has been requested, causing us to miss
-	// it.
-	chanFound := hn.checkChanPointInGraph(targetChan)
-	if chanFound {
-		close(req.eventChan)
-		return
-	}
-
-	// Otherwise, we'll add this to the list of open channel watchers for
-	// this out point.
-	hn.openChanWatchers[targetChan] = append(
-		hn.openChanWatchers[targetChan],
-		req.eventChan,
-	)
-}
-
-// handleClosedChannelUpdate takes a series of closed channel updates, extracts
-// the outpoints, saves them to harness node's internal state, and notifies all
-// registered clients.
-func (hn *HarnessNode) handleClosedChannelUpdate(
-	updates []*lnrpc.ClosedChannelUpdate) {
-
-	// For each channel closed, we'll mark that we've detected a channel
-	// closure while lnd was pruning the channel graph.
-	for _, closedChan := range updates {
-		op, err := MakeOutpoint(closedChan.ChanPoint)
-		if err != nil {
-			hn.PrintErrf("failed to create outpoint for %v "+
-				"got err: %v", closedChan.ChanPoint, err)
-			return
-		}
-
-		hn.closedChans[op] = struct{}{}
-
-		// As the channel has been closed, we'll notify all register
-		// watchers.
-		for _, eventChan := range hn.closeChanWatchers[op] {
-			close(eventChan)
-		}
-		delete(hn.closeChanWatchers, op)
-	}
-}
-
-// handleCloseChannelWatchRequest processes a watch close channel request by
-// checking whether the given channel point can be found in the node's internal
-// state. If not, the request is added to a watch request list than will be
-// handled by handleCloseChannelWatchRequest.
-func (hn *HarnessNode) handleCloseChannelWatchRequest(req *chanWatchRequest) {
-	targetChan := req.chanPoint
-
-	// If this is a close request, then it can be immediately dispatched if
-	// we've already seen a channel closure for this channel.
-	if _, ok := hn.closedChans[targetChan]; ok {
-		close(req.eventChan)
-		return
-	}
-
-	// Otherwise, we'll add this to the list of close channel watchers for
-	// this out point.
-	hn.closeChanWatchers[targetChan] = append(
-		hn.closeChanWatchers[targetChan],
-		req.eventChan,
-	)
-}
-
-type topologyClient lnrpc.Lightning_SubscribeChannelGraphClient
-
-// newTopologyClient creates a topology client.
-func (hn *HarnessNode) newTopologyClient(
-	ctx context.Context) (topologyClient, error) {
-
-	req := &lnrpc.GraphTopologySubscription{}
-	client, err := hn.rpc.LN.SubscribeChannelGraph(ctx, req)
-	if err != nil {
-		return nil, fmt.Errorf("%s(%d): unable to create topology "+
-			"client: %v (%s)", hn.Name(), hn.NodeID, err,
-			time.Now().String())
-	}
-
-	return client, nil
-}
-
-// receiveTopologyClientStream initializes a topologyClient to subscribe
-// topology update events. Due to a race condition between the ChannelRouter
-// starting and us making the subscription request, it's possible for our graph
-// subscription to fail. In that case, we will retry the subscription until it
-// succeeds or fail after 10 seconds.
-//
-// NOTE: must be run as a goroutine.
-func (hn *HarnessNode) receiveTopologyClientStream(
-	receiver chan *lnrpc.GraphTopologyUpdate) error {
-
-	// Create a topology client to receive graph updates.
-	client, err := hn.newTopologyClient(hn.runCtx)
-	if err != nil {
-		return fmt.Errorf("create topologyClient failed: %w", err)
-	}
-
-	// We use the context to time out when retrying graph subscription.
-	ctxt, cancel := context.WithTimeout(hn.runCtx, DefaultTimeout)
-	defer cancel()
-
-	for {
-		update, err := client.Recv()
-
-		switch {
-		case err == nil:
-			// Good case. We will send the update to the receiver.
-
-		case strings.Contains(err.Error(), "router not started"):
-			// If the router hasn't been started, we will retry
-			// every 200 ms until it has been started or fail
-			// after the ctxt is timed out.
-			select {
-			case <-ctxt.Done():
-				return fmt.Errorf("graph subscription: " +
-					"router not started before timeout")
-			case <-time.After(wait.PollInterval):
-			case <-hn.runCtx.Done():
-				return nil
-			}
-
-			// Re-create the topology client.
-			client, err = hn.newTopologyClient(hn.runCtx)
-			if err != nil {
-				return fmt.Errorf("create topologyClient "+
-					"failed: %v", err)
-			}
-
-			continue
-
-		case strings.Contains(err.Error(), "EOF"):
-			// End of subscription stream. Do nothing and quit.
-			return nil
-
-		case strings.Contains(err.Error(), context.Canceled.Error()):
-			// End of subscription stream. Do nothing and quit.
-			return nil
-
-		default:
-			// An expected error is returned, return and leave it
-			// to be handled by the caller.
-			return fmt.Errorf("graph subscription err: %w", err)
-		}
-
-		// Send the update or quit.
-		select {
-		case receiver <- update:
-		case <-hn.runCtx.Done():
-			return nil
-		}
-	}
-}
-
-// handlePolicyUpdateWatchRequest checks that if the expected policy can be
-// found either in the node's interval state or describe graph response. If
-// found, it will signal the request by closing the event channel. Otherwise it
-// does nothing but returns nil.
-func (hn *HarnessNode) handlePolicyUpdateWatchRequest(req *chanWatchRequest) {
-	op := req.chanPoint
-
-	// Get a list of known policies for this chanPoint+advertisingNode
-	// combination. Start searching in the node state first.
-	policies, ok := hn.policyUpdates[op.String()][req.advertisingNode]
-
-	if !ok {
-		// If it cannot be found in the node state, try searching it
-		// from the node's DescribeGraph.
-		policyMap := hn.getChannelPolicies(req.includeUnannounced)
-		policies, ok = policyMap[op.String()][req.advertisingNode]
-		if !ok {
-			return
-		}
-	}
-
-	// Check if there's a matched policy.
-	for _, policy := range policies {
-		if CheckChannelPolicy(policy, req.policy) == nil {
-			close(req.eventChan)
-			return
-		}
-	}
-}
-
-// getChannelPolicies queries the channel graph and formats the policies into
-// the format defined in type policyUpdateMap.
-func (hn *HarnessNode) getChannelPolicies(include bool) policyUpdateMap {
-	ctxt, cancel := context.WithTimeout(hn.runCtx, DefaultTimeout)
-	defer cancel()
-
-	graph, err := hn.rpc.LN.DescribeGraph(ctxt, &lnrpc.ChannelGraphRequest{
-		IncludeUnannounced: include,
-	})
-	if err != nil {
-		hn.PrintErrf("DescribeGraph got err: %v", err)
-		return nil
-	}
-
-	policyUpdates := policyUpdateMap{}
-
-	for _, e := range graph.Edges {
-		policies := policyUpdates[e.ChanPoint]
-
-		// If the map[op] is nil, we need to initialize the map first.
-		if policies == nil {
-			policies = make(map[string][]*lnrpc.RoutingPolicy)
-		}
-
-		if e.Node1Policy != nil {
-			policies[e.Node1Pub] = append(
-				policies[e.Node1Pub], e.Node1Policy,
-			)
-		}
-
-		if e.Node2Policy != nil {
-			policies[e.Node2Pub] = append(
-				policies[e.Node2Pub], e.Node2Policy,
-			)
-		}
-
-		policyUpdates[e.ChanPoint] = policies
-	}
-
-	return policyUpdates
 }
 
 // renameFile is a helper to rename (log) files created during integration

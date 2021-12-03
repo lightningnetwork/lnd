bdc837e91f0485532a042cc6434041ea1e3d6ab7
positiveblue <jordi.montes.sanabria@gmail.com>
2021-12-03 13:15:37 -0800

    lnrpc: fix `log.go` files
    The `log.go` file of each subserver is a copy and paste of the others.
    All of them have the same typo, an extra space in a comment, and the
    the definition of a function not used anywhere.

--- lnrpc/verrpc/log.go
+++ lnrpc/verrpc/log.go
@@ -5,7 +5,7 @@ import (
 	"github.com/lightningnetwork/lnd/build"
 )
 
-// log is a logger that is initialized with no output filters.  This
+// log is a logger that is initialized with no output filters. This
 // means the package will not perform any logging by default until the caller
 // requests it.
 var log btclog.Logger

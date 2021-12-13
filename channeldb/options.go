b147d589c14472f70679b67d7fd385231d6298d5
Oliver Gugger <gugger@gmail.com>
2021-12-13 10:53:58 +0100

    channeldb: add NoMigration mode
    In case the channeldb package is used as a library in external tools, it
    can be useful to allow read-only access to a DB. This allows such a
    tool to access a DB even if not all migrations were executed, which can
    be useful for recovery purposes.
    To make it possible to even start the DB with a read-only backend, we
    need to disable the automatic migration step.

--- channeldb/options.go
+++ channeldb/options.go
@@ -50,6 +50,11 @@ type Options struct {
 	// path finding.
 	UseGraphCache bool
 
+	// NoMigration specifies that underlying backend was opened in read-only
+	// mode and migrations shouldn't be performed. This can be useful for
+	// applications that use the channeldb package as a library.
+	NoMigration bool
+
 	// clock is the time source used by the database.
 	clock clock.Clock
 
@@ -71,6 +76,7 @@ func DefaultOptions() Options {
 		ChannelCacheSize:      DefaultChannelCacheSize,
 		PreAllocCacheNumNodes: DefaultPreAllocCacheNumNodes,
 		UseGraphCache:         true,
+		NoMigration:           false,
 		clock:                 clock.NewDefaultClock(),
 	}
 }
@@ -133,6 +139,14 @@ func OptionAutoCompactMinAge(minAge time.Duration) OptionModifier {
 func OptionSetBatchCommitInterval(interval time.Duration) OptionModifier {
 	return func(o *Options) {
 		o.BatchCommitInterval = interval
+	}
+}
+
+// OptionNoMigration allows the database to be opened in read only mode by
+// disabling migrations.
+func OptionNoMigration(b bool) OptionModifier {
+	return func(o *Options) {
+		o.NoMigration = b
 	}
 }
 

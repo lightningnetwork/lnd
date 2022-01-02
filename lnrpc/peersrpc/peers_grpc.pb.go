cfe864a05d27acb1928c875480f732c738c190fb
positiveblue <jordi.montes.sanabria@gmail.com>
2022-01-02 14:32:14 -0800

    lnrpc: add `updatenodeannouncement` to `Peers` service
    New endpoint for the `Peers` subrpc.

--- lnrpc/peersrpc/peers_grpc.pb.go
+++ lnrpc/peersrpc/peers_grpc.pb.go
@@ -3,7 +3,10 @@
 package peersrpc
 
 import (
+	context "context"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -15,6 +18,10 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type PeersClient interface {
+	// lncli: peers updatenodeannouncement
+	//UpdateNodeAnnouncement allows the caller to update the node parameters
+	//and broadcasts a new version of the node announcement to its peers.
+	UpdateNodeAnnouncement(ctx context.Context, in *NodeAnnouncementUpdateRequest, opts ...grpc.CallOption) (*NodeAnnouncementUpdateResponse, error)
 }
 
 type peersClient struct {
@@ -25,10 +32,23 @@ func NewPeersClient(cc grpc.ClientConnInterface) PeersClient {
 	return &peersClient{cc}
 }
 
+func (c *peersClient) UpdateNodeAnnouncement(ctx context.Context, in *NodeAnnouncementUpdateRequest, opts ...grpc.CallOption) (*NodeAnnouncementUpdateResponse, error) {
+	out := new(NodeAnnouncementUpdateResponse)
+	err := c.cc.Invoke(ctx, "/peersrpc.Peers/UpdateNodeAnnouncement", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // PeersServer is the server API for Peers service.
 // All implementations must embed UnimplementedPeersServer
 // for forward compatibility
 type PeersServer interface {
+	// lncli: peers updatenodeannouncement
+	//UpdateNodeAnnouncement allows the caller to update the node parameters
+	//and broadcasts a new version of the node announcement to its peers.
+	UpdateNodeAnnouncement(context.Context, *NodeAnnouncementUpdateRequest) (*NodeAnnouncementUpdateResponse, error)
 	mustEmbedUnimplementedPeersServer()
 }
 
@@ -36,6 +56,9 @@ type PeersServer interface {
 type UnimplementedPeersServer struct {
 }
 
+func (UnimplementedPeersServer) UpdateNodeAnnouncement(context.Context, *NodeAnnouncementUpdateRequest) (*NodeAnnouncementUpdateResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateNodeAnnouncement not implemented")
+}
 func (UnimplementedPeersServer) mustEmbedUnimplementedPeersServer() {}
 
 // UnsafePeersServer may be embedded to opt out of forward compatibility for this service.
@@ -49,13 +72,36 @@ func RegisterPeersServer(s grpc.ServiceRegistrar, srv PeersServer) {
 	s.RegisterService(&Peers_ServiceDesc, srv)
 }
 
+func _Peers_UpdateNodeAnnouncement_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(NodeAnnouncementUpdateRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PeersServer).UpdateNodeAnnouncement(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/peersrpc.Peers/UpdateNodeAnnouncement",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PeersServer).UpdateNodeAnnouncement(ctx, req.(*NodeAnnouncementUpdateRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Peers_ServiceDesc is the grpc.ServiceDesc for Peers service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
 var Peers_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "peersrpc.Peers",
 	HandlerType: (*PeersServer)(nil),
-	Methods:     []grpc.MethodDesc{},
-	Streams:     []grpc.StreamDesc{},
-	Metadata:    "peersrpc/peers.proto",
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "UpdateNodeAnnouncement",
+			Handler:    _Peers_UpdateNodeAnnouncement_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "peersrpc/peers.proto",
 }

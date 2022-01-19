1a8f094503cc118b193483e90059fd468ec70364
Andras Banki-Horvath <bhandras@gmail.com>
2022-01-19 20:51:10 +0100

    lnrpc: add the devrpc development only subserver + devrpc.ImportGraph
    This commits adds the devrpc package which implements a subserver that
    adds clean separation for RPC calls useful for development and
    debugging. This subserver is only compiled in if the dev tag is set.
    Furthermore the commit adds the devrpc.ImportGraph call which can
    import a graph dump obtained from another node by calling DescribeGraph.
    Since the graph dump does not include the auth proofs, the imported
    channels will be considered private.

---
+++ lnrpc/devrpc/config_active.go
@@ -0,0 +1,19 @@
+//go:build dev
+// +build dev
+
+package devrpc
+
+import (
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/lightningnetwork/lnd/channeldb"
+)
+
+// Config is the primary configuration struct for the DEV RPC server. It
+// contains all the items required for the rpc server to carry out its
+// duties. Any fields with struct tags are meant to be parsed as normal
+// configuration options, while if able to be populated, the latter fields MUST
+// also be specified.
+type Config struct {
+	ActiveNetParams *chaincfg.Params
+	GraphDB         *channeldb.ChannelGraph
+}

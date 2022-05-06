1a72fa7be6afa7c529b5386086e4255dc2b75fc2
ErikEk <erik.ek@gmail.com>
2022-05-06 21:40:36 +0200

    neutrinorpc: add getblockhash functionality

--- lnrpc/neutrinorpc/neutrinokit.pb.json.go
+++ lnrpc/neutrinorpc/neutrinokit.pb.json.go
@@ -197,4 +197,29 @@ func RegisterNeutrinoKitJSONCallbacks(registry map[string]func(ctx context.Conte
 		}
 		callback(string(respBytes), nil)
 	}
+
+	registry["neutrinorpc.NeutrinoKit.GetBlockHash"] = func(ctx context.Context,
+		conn *grpc.ClientConn, reqJSON string, callback func(string, error)) {
+
+		req := &GetBlockHashRequest{}
+		err := marshaler.Unmarshal([]byte(reqJSON), req)
+		if err != nil {
+			callback("", err)
+			return
+		}
+
+		client := NewNeutrinoKitClient(conn)
+		resp, err := client.GetBlockHash(ctx, req)
+		if err != nil {
+			callback("", err)
+			return
+		}
+
+		respBytes, err := marshaler.Marshal(resp)
+		if err != nil {
+			callback("", err)
+			return
+		}
+		callback(string(respBytes), nil)
+	}
 }

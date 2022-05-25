1502ce73eadd28cf22763f40004e7621f5cef47a
ziggie <ziggie1984@protonmail.com>
2022-05-25 14:49:32 +0200

    cmd/lncli: make hop hint inclusion opt-in for addholdinvoice

--- cmd/lncli/invoicesrpc_active.go
+++ cmd/lncli/invoicesrpc_active.go
@@ -184,7 +184,7 @@ var addHoldInvoiceCommand = cli.Command{
 				"specified, an expiry of 3600 seconds (1 hour) " +
 				"is implied.",
 		},
-		cli.BoolTFlag{
+		cli.BoolFlag{
 			Name: "private",
 			Usage: "encode routing hints in the invoice with " +
 				"private channels in order to assist the " +

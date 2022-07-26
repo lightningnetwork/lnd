f35429cd77f9cd765b2de9da860807e6af4ec1eb
Greg Weber <1183+gregwebs@users.noreply.github.com>
2022-07-26 11:43:45 -0500

    use the O_SYNC flag

--- io/write.go
+++ io/write.go
@@ -14,7 +14,7 @@ func WriteFileToDisk(file string, fileBytes []byte, perm fs.FileMode) error {
 	if err != nil {
 		return err
 	}
-	return writeSyncClose(f, fileBytes)
+	return writeClose(f, fileBytes)
 }
 
 // WriteFileTransactional does a write with sync like WriteFileToDisk
@@ -25,7 +25,7 @@ func WriteFileTransactional(file string, fileBytes []byte, perm fs.FileMode) err
 	if err != nil {
 		return err
 	}
-	err = writeSyncClose(f, fileBytes)
+	err = writeClose(f, fileBytes)
 	if err != nil {
 		_ = os.Remove(file)
 	}
@@ -37,18 +37,15 @@ func WriteFileTransactional(file string, fileBytes []byte, perm fs.FileMode) err
 // However it takes an open handle as an argument instead of a file:
 // this is at least useful for testing purposes.
 func WriteRemoveOnError(f *os.File, fileBytes []byte) error {
-	err := writeSyncClose(f, fileBytes)
+	err := writeClose(f, fileBytes)
 	if err != nil {
 		_ = os.Remove(f.Name())
 	}
 	return err
 }
 
-func writeSyncClose(f *os.File, fileBytes []byte) error {
+func writeClose(f *os.File, fileBytes []byte) error {
 	_, err := f.Write(fileBytes)
-	if err2 := f.Sync(); err2 != nil && err == nil {
-		err = err2
-	}
 	if err2 := f.Close(); err2 != nil && err == nil {
 		err = err2
 	}
@@ -56,5 +53,5 @@ func writeSyncClose(f *os.File, fileBytes []byte) error {
 }
 
 func createFileForWrite(file string, perm fs.FileMode) (*os.File, error) {
-	return os.OpenFile(file, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, perm)
+	return os.OpenFile(file, os.O_WRONLY|os.O_CREATE|os.O_TRUNC|os.O_SYNC, perm)
 }

aff2ed3a6a118d664049835c325a5b69e977172f
Olaoluwa Osuntokun <laolu32@gmail.com>
2022-10-09 18:45:23 -0700

    build: bump version to v0.15.2

--- build/version.go
+++ build/version.go
@@ -44,7 +44,7 @@ const (
 	AppMinor uint = 15
 
 	// AppPatch defines the application patch for this binary.
-	AppPatch uint = 1
+	AppPatch uint = 2
 
 	// AppPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.

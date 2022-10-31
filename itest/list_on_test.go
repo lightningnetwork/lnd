eb31ff553b1d2555061a295e2ccb348b73f1b2bf
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2022-10-31 11:31:41 -0400

    lntest/itest: add coverage for querying routes to blinded paths

--- itest/list_on_test.go
+++ itest/list_on_test.go
@@ -546,4 +546,8 @@ var allTestCases = []*lntest.TestCase{
 		Name:     "update pending open channels",
 		TestFunc: testUpdateOnPendingOpenChannels,
 	},
+	{
+		Name:     "query blinded route",
+		TestFunc: testQueryBlindedRoutes,
+	},
 }

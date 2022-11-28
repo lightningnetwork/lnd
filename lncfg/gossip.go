542bbbbc30e520894194b2b9653fcf7cd3520af4
Oliver Gugger <gugger@gmail.com>
2022-11-28 09:55:18 +0100

    lncfg: fix gossip.pinned-syncers flag description
    The gossip.pinned-syncers flag is declared as a string slice which means
    it should not be a comma separated list of elements but the flag should
    be specified multiple times instead.

--- lncfg/gossip.go
+++ lncfg/gossip.go
@@ -9,7 +9,7 @@ import (
 
 //nolint:lll
 type Gossip struct {
-	PinnedSyncersRaw []string `long:"pinned-syncers" description:"A set of peers that should always remain in an active sync state, which can be used to closely synchronize the routing tables of two nodes. The value should be comma separated list of hex-encoded pubkeys. Connected peers matching this pubkey will remain active for the duration of the connection and not count towards the NumActiveSyncer count."`
+	PinnedSyncersRaw []string `long:"pinned-syncers" description:"A set of peers that should always remain in an active sync state, which can be used to closely synchronize the routing tables of two nodes. The value should be a hex-encoded pubkey, the flag can be specified multiple times to add multiple peers. Connected peers matching this pubkey will remain active for the duration of the connection and not count towards the NumActiveSyncer count."`
 
 	PinnedSyncers discovery.PinnedSyncers
 

2ff0abde87e71179b976fbdb4d316da329ce11ae
ziggie <ziggie1984@protonmail.com>
2023-01-11 12:14:59 +0100

    routerrpc: add payment hash to htlc events
    Adds the payment hash to a particular set of htlc events
    (Forwards, ForwardFails, Settle, LinkFail) of the grpc stream.
    
    In addition the grpc tests for the routerrpc package are adapted
    for the subscribehtlcevents.

--- lnrpc/routerrpc/router_server_test.go
+++ lnrpc/routerrpc/router_server_test.go
@@ -6,31 +6,34 @@ import (
 	"time"
 
 	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/htlcswitch"
 	"github.com/lightningnetwork/lnd/lnrpc"
+	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/queue"
 	"github.com/lightningnetwork/lnd/routing"
+	"github.com/lightningnetwork/lnd/subscribe"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
 )
 
-type streamMock struct {
+type streamPaymentsMock struct {
 	grpc.ServerStream
 	ctx            context.Context
 	sentFromServer chan *lnrpc.Payment
 }
 
-func makeStreamMock(ctx context.Context) *streamMock {
-	return &streamMock{
+func makeStreamPaymentMock(ctx context.Context) *streamPaymentsMock {
+	return &streamPaymentsMock{
 		ctx:            ctx,
 		sentFromServer: make(chan *lnrpc.Payment, 10),
 	}
 }
 
-func (m *streamMock) Context() context.Context {
+func (m *streamPaymentsMock) Context() context.Context {
 	return m.ctx
 }
 
-func (m *streamMock) Send(p *lnrpc.Payment) error {
+func (m *streamPaymentsMock) Send(p *lnrpc.Payment) error {
 	m.sentFromServer <- p
 	return nil
 }
@@ -78,7 +81,7 @@ func TestTrackPaymentsReturnsOnCancelContext(t *testing.T) {
 	towerMock := makeControlTowerMock()
 
 	streamCtx, cancelStream := context.WithCancel(context.Background())
-	stream := makeStreamMock(streamCtx)
+	stream := makeStreamPaymentMock(streamCtx)
 
 	server := &Server{
 		cfg: &Config{
@@ -88,7 +91,7 @@ func TestTrackPaymentsReturnsOnCancelContext(t *testing.T) {
 		},
 	}
 
-	// Cancel stream immediately
+	// Cancel stream immediately.
 	cancelStream()
 
 	// Make sure the call returns.
@@ -106,7 +109,7 @@ func TestTrackPaymentsInflightUpdates(t *testing.T) {
 	towerMock := makeControlTowerMock()
 
 	streamCtx, cancelStream := context.WithCancel(context.Background())
-	stream := makeStreamMock(streamCtx)
+	stream := makeStreamPaymentMock(streamCtx)
 	defer cancelStream()
 
 	server := &Server{
@@ -168,7 +171,7 @@ func TestTrackPaymentsNoInflightUpdates(t *testing.T) {
 	towerMock.queue.Start()
 
 	streamCtx, cancelStream := context.WithCancel(context.Background())
-	stream := makeStreamMock(streamCtx)
+	stream := makeStreamPaymentMock(streamCtx)
 	defer cancelStream()
 
 	server := &Server{
@@ -213,4 +216,176 @@ func TestTrackPaymentsNoInflightUpdates(t *testing.T) {
 	// Only the final states should be sent to the client.
 	payment := <-stream.sentFromServer
 	require.Equal(t, lnrpc.Payment_SUCCEEDED, payment.Status)
+}
+
+type streamHtlcEventsMock struct {
+	grpc.ServerStream
+	ctx            context.Context //nolint
+	sentFromServer chan *HtlcEvent
+}
+
+func (m *streamHtlcEventsMock) Send(p *HtlcEvent) error {
+	m.sentFromServer <- p
+	return nil
+}
+
+func (m *streamHtlcEventsMock) Context() context.Context {
+	return m.ctx
+}
+
+func makeStreamHtlcEventsMock(ctx context.Context) *streamHtlcEventsMock {
+	return &streamHtlcEventsMock{
+		ctx:            ctx,
+		sentFromServer: make(chan *HtlcEvent, 20),
+	}
+}
+
+// TestSubscribeHtlcEventsReturnsOnCancelContext tests whether
+// SubscribeHtlcEvents returns when the stream context is cancelled.
+func TestSubscribeHtlcEventsReturnsOnCancelContext(t *testing.T) {
+	t.Parallel()
+	request := &SubscribeHtlcEventsRequest{}
+
+	streamCtx, cancelStream := context.WithCancel(context.Background())
+	stream := makeStreamHtlcEventsMock(streamCtx)
+
+	// Create server mock for htlc propagation.
+	htlcServerMock := subscribe.NewServer()
+	err := htlcServerMock.Start()
+	if err != nil {
+		t.Fatalf("starting htlc server failed with: %s", err)
+	}
+	defer func() {
+		err := htlcServerMock.Stop()
+		if err != nil {
+			t.Fatalf("failed stopping htlc server: %s", err)
+		}
+	}()
+
+	server := &Server{
+		cfg: &Config{
+			RouterBackend: &RouterBackend{
+				SubscribeHtlcEvents: func() (
+					*subscribe.Client, error) {
+
+					return htlcServerMock.Subscribe()
+				},
+			},
+		},
+	}
+
+	// Cancel stream immediately.
+	cancelStream()
+
+	// Make sure the call returns.
+	err = server.SubscribeHtlcEvents(request, stream)
+	require.Equal(t, context.Canceled, err)
+}
+
+// TestHtlcEvents tests whether all htlc events are propagated to the client.
+func TestSubscribeHtlcEvents(t *testing.T) {
+	t.Parallel()
+	request := &SubscribeHtlcEventsRequest{}
+
+	// Create server mock for htlc propagation.
+	htlcNotifierMock := subscribe.NewServer()
+	err := htlcNotifierMock.Start()
+	if err != nil {
+		t.Fatalf("starting htlc server failed with: %s", err)
+	}
+	defer func() {
+		err := htlcNotifierMock.Stop()
+		if err != nil {
+			t.Fatalf("failed stopping htlc server: %s", err)
+		}
+	}()
+
+	streamCtx, cancelStream := context.WithCancel(context.Background())
+	stream := makeStreamHtlcEventsMock(streamCtx)
+	defer cancelStream()
+
+	server := &Server{
+		cfg: &Config{
+			RouterBackend: &RouterBackend{
+				SubscribeHtlcEvents: func() (
+					*subscribe.Client, error) {
+
+					return htlcNotifierMock.Subscribe()
+				},
+			},
+		},
+	}
+
+	// Listen to htlc events in a goroutine.
+	go func() {
+		err := server.SubscribeHtlcEvents(request, stream)
+		require.Equal(t, context.Canceled, err)
+	}()
+
+	// Make sure the client is subscribed before we send htlc events.
+	time.Sleep(50 * time.Millisecond)
+
+	// Send all types of HTLC events through the server and see whether
+	// the client receives the right types in right order.
+	err = htlcNotifierMock.SendUpdate(&htlcswitch.ForwardingFailEvent{})
+	if err != nil {
+		t.Fatalf("failed sending ForwardFailEvent to the "+
+			"htlcNotifier: %s", err)
+	}
+	err = htlcNotifierMock.SendUpdate(&htlcswitch.SettleEvent{})
+	if err != nil {
+		t.Fatalf("failed sending SettleEvent to the "+
+			"htlcNotifier: %s", err)
+	}
+	err = htlcNotifierMock.SendUpdate(&htlcswitch.FinalHtlcEvent{})
+	if err != nil {
+		t.Fatalf("failed sending FinalHtlcEvent to the "+
+			"htlcNotifier: %s", err)
+	}
+	// Make sure the LinkError is populated otherwise the test fails.
+	err = htlcNotifierMock.SendUpdate(&htlcswitch.LinkFailEvent{
+		LinkError: htlcswitch.NewLinkError(
+			lnwire.NewFeeInsufficient(0, lnwire.ChannelUpdate{}),
+		),
+	})
+	if err != nil {
+		t.Fatalf("failed sending LinkFailEvent to the "+
+			"htlcNotifier: %s", err)
+	}
+
+	// Wait until there's 5 update or the deadline is exceeded.
+	deadline := time.Now().Add(1 * time.Second)
+	for {
+		if len(stream.sentFromServer) == 5 {
+			break
+		}
+
+		if time.Now().After(deadline) {
+			require.FailNow(t, "deadline exceeded.")
+		}
+	}
+
+	// 5 updates should be sent to the client. The server sends one initial
+	//  subscribe request we need to account for.
+	require.Len(t, stream.sentFromServer, 5)
+
+	htlcEvent := <-stream.sentFromServer
+	_, ok := htlcEvent.Event.(*HtlcEvent_SubscribedEvent)
+	require.True(t, ok)
+
+	htlcEvent = <-stream.sentFromServer
+	_, ok = htlcEvent.Event.(*HtlcEvent_ForwardFailEvent)
+	require.True(t, ok)
+
+	htlcEvent = <-stream.sentFromServer
+	_, ok = htlcEvent.Event.(*HtlcEvent_SettleEvent)
+	require.True(t, ok)
+
+	htlcEvent = <-stream.sentFromServer
+	_, ok = htlcEvent.Event.(*HtlcEvent_FinalHtlcEvent)
+	require.True(t, ok)
+
+	htlcEvent = <-stream.sentFromServer
+	_, ok = htlcEvent.Event.(*HtlcEvent_LinkFailEvent)
+	require.True(t, ok)
 }

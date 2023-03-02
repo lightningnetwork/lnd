52d6fa12d4637144e2aaf3efd99c2c4df1395522
Olaoluwa Osuntokun <laolu32@gmail.com>
2023-03-01 21:46:31 -0800

    input: update the mock signer for the musig2 API changes

--- lntest/mock/signer.go
+++ lntest/mock/signer.go
@@ -55,10 +55,9 @@ func (d *DummySigner) ComputeInputScript(tx *wire.MsgTx,
 // all signing parties must be provided, including the public key of the local
 // signing key. If nonces of other parties are already known, they can be
 // submitted as well to reduce the number of method calls necessary later on.
-func (d *DummySigner) MuSig2CreateSession(input.MuSig2Version,
-	keychain.KeyLocator, []*btcec.PublicKey, *input.MuSig2Tweaks,
-	[][musig2.PubNonceSize]byte,
-	...musig2.SessionOption) (*input.MuSig2SessionInfo, error) {
+func (d *DummySigner) MuSig2CreateSession(input.MuSig2Version, keychain.KeyLocator,
+	[]*btcec.PublicKey, *input.MuSig2Tweaks,
+	[][musig2.PubNonceSize]byte, ...musig2.SessionOption) (*input.MuSig2SessionInfo, error) {
 
 	return nil, nil
 }

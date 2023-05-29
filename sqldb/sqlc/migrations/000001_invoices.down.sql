e3663b99424c217eab1526134f6ce65e10e2d59c
positiveblue <jordi.montes.sanabria@gmail.com>
2023-05-29 10:54:35 -0700

    sqldb: add invoices SQL schema
    This is the schema for "ordinal" BOLT11 invoices.
    
    The invoices table aims to keep an entry for each invoice, BOLT11 or not,
    that will be supported.
    
    Invoice related HTLCs will be stored in a separete table than forwarded
    htlcs.
    
    SQLite does not support `SEQUENCE`. We achieve atomic autoincrementals
    using primary keys with autoincrement/serial. An invoice `AddIndex`
    translates to `invoices(id)` while `SettleIndex` is `invoice_payments(id)`.

---
+++ sqldb/sqlc/migrations/000001_invoices.down.sql
@@ -0,0 +1,19 @@
+DROP INDEX IF EXISTS invoice_payments_invoice_id_idx;
+DROP INDEX IF EXISTS invoice_payments_settled_at_idx;
+DROP TABLE IF EXISTS invoice_payments;
+
+DROP INDEX IF EXISTS invoice_htlc_custom_records_htlc_id_idx;
+DROP TABLE IF EXISTS invoice_htlc_custom_records;
+
+DROP INDEX IF EXISTS invoice_htlc_invoice_id_idx;
+DROP TABLE IF EXISTS invoice_htlcs;
+
+DROP INDEX IF EXISTS invoice_feature_invoice_id_idx;
+DROP TABLE IF EXISTS invoice_features;
+
+DROP INDEX IF EXISTS invoices_created_at_idx;
+DROP INDEX IF EXISTS invoices_state_idx;
+DROP INDEX IF EXISTS invoices_payment_addr_idx;
+DROP INDEX IF EXISTS invoices_preimage_idx;
+DROP INDEX IF EXISTS invoices_hash_idx;
+DROP TABLE IF EXISTS invoices;

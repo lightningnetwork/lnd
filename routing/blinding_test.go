796f9a7a25e160aa3e52e82c0018349cc4d29ad8
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2023-05-31 15:28:09 -0400

    routing: add representation of blinded payments
    This commit adds a representation of blinded payments, which include a
    blinded path and aggregate routing parameters to be used in payment to
    the path.

---
+++ routing/blinding_test.go
@@ -0,0 +1,70 @@
+package routing
+
+import (
+	"testing"
+
+	sphinx "github.com/lightningnetwork/lightning-onion"
+	"github.com/stretchr/testify/require"
+)
+
+// TestBlindedPathValidation tests validation of blinded paths.
+func TestBlindedPathValidation(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		payment *BlindedPayment
+		err     error
+	}{
+		{
+			name:    "no path",
+			payment: &BlindedPayment{},
+			err:     ErrNoBlindedPath,
+		},
+		{
+			name: "insufficient hops",
+			payment: &BlindedPayment{
+				BlindedPath: &sphinx.BlindedPath{
+					BlindedHops: []*sphinx.BlindedHopInfo{},
+				},
+			},
+			err: ErrInsufficientBlindedHops,
+		},
+		{
+			name: "maximum < minimum",
+			payment: &BlindedPayment{
+				BlindedPath: &sphinx.BlindedPath{
+					BlindedHops: []*sphinx.BlindedHopInfo{
+						{},
+					},
+				},
+				HtlcMaximum: 10,
+				HtlcMinimum: 20,
+			},
+			err: ErrHTLCRestrictions,
+		},
+		{
+			name: "valid",
+			payment: &BlindedPayment{
+				BlindedPath: &sphinx.BlindedPath{
+					BlindedHops: []*sphinx.BlindedHopInfo{
+						{},
+					},
+				},
+				HtlcMaximum: 15,
+				HtlcMinimum: 5,
+			},
+		},
+	}
+
+	for _, testCase := range tests {
+		testCase := testCase
+
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			err := testCase.payment.Validate()
+			require.ErrorIs(t, err, testCase.err)
+		})
+	}
+}

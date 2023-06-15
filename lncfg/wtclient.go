255d8bb355e54d9baff2643e1878aedbf27f73ee
Elle Mouton <elle.mouton@gmail.com>
2023-06-15 08:39:44 +0200

    lncfg: remove deprecated PrivateTowerURIs from Wtclient cfg
    This commit removes the `PrivateTowerURIs` member from the `WtClient`
    config struct. This field has been deprecated since v0.8.0-beta and
    currently, LND would fail to start if the field was specified.

--- lncfg/wtclient.go
+++ lncfg/wtclient.go
@@ -15,10 +15,6 @@ type WtClient struct {
 	// back up channel states with registered watchtowers.
 	Active bool `long:"active" description:"Whether the daemon should use private watchtowers to back up revoked channel states."`
 
-	// PrivateTowerURIs specifies the lightning URIs of the towers the
-	// watchtower client should send new backups to.
-	PrivateTowerURIs []string `long:"private-tower-uris" description:"(Deprecated) Specifies the URIs of private watchtowers to use in backing up revoked states. URIs must be of the form <pubkey>@<addr>. Only 1 URI is supported at this time, if none are provided the tower will not be enabled."`
-
 	// SweepFeeRate specifies the fee rate in sat/byte to be used when
 	// constructing justice transactions sent to the tower.
 	SweepFeeRate uint64 `long:"sweep-fee-rate" description:"Specifies the fee rate in sat/byte to be used when constructing justice transactions sent to the watchtower."`
@@ -58,15 +54,6 @@ func DefaultWtClientCfg() *WtClient {
 //
 // NOTE: Part of the Validator interface.
 func (c *WtClient) Validate() error {
-	// TODO(wilmer): remove in v0.9.0 release.
-	if len(c.PrivateTowerURIs) > 0 {
-		return fmt.Errorf("the `wtclient.private-tower-uris` option " +
-			"has been deprecated as of v0.8.0-beta and will be " +
-			"removed in v0.9.0-beta. To setup watchtowers for " +
-			"the client, set `wtclient.active` and run " +
-			"`lncli wtclient -h` for more information")
-	}
-
 	if c.SweepFeeRate == 0 {
 		return fmt.Errorf("sweep-fee-rate must be non-zero")
 	}

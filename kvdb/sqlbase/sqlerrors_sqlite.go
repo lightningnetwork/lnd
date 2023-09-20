329fcc64982d13927a6bb04ec65b0004cba2380e
Olaoluwa Osuntokun <laolu32@gmail.com>
2023-09-19 18:12:50 -0700

    kvdb+sqldb: update SQL error parsing to account for non wrapped errs
    Some sub-systems like btcwallet will return an error from the database,
    but they won't properly wrap it. As a result, we were unable to actually
    catch the serialization errors in the first place. To work around this,
    we'll now attempt to parse the error string directly.

--- kvdb/sqlbase/sqlerrors_sqlite.go
+++ kvdb/sqlbase/sqlerrors_sqlite.go
@@ -5,6 +5,7 @@ package sqlbase
 import (
 	"errors"
 	"fmt"
+	"strings"
 
 	"modernc.org/sqlite"
 	sqlite3 "modernc.org/sqlite/lib"
@@ -13,6 +14,21 @@ import (
 // parseSqliteError attempts to parse a sqlite error as a database agnostic
 // SQL error.
 func parseSqliteError(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	// If the error isn't wrapped properly, the errors.As call with fail,
+	// so we'll also try to check the expected error message directly.
+	// This is taken from:
+	// https://gitlab.com/cznic/sqlite/-/blob/v1.25.0/sqlite.go#L75.
+	const sqliteErrMsg = "SQLITE_BUSY"
+	if strings.Contains(err.Error(), sqliteErrMsg) {
+		return &ErrSerializationError{
+			DBError: err,
+		}
+	}
+
 	var sqliteErr *sqlite.Error
 	if !errors.As(err, &sqliteErr) {
 		return nil

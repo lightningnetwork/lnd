c88e270a4f67f902a0b698c6d53299b3dc447fbb
Olaoluwa Osuntokun <laolu32@gmail.com>
2023-09-19 18:18:51 -0700

    kvdb: fix bug in commit retry loop
    In this commit, we fix a bug in the commit retry loop, we'll now make
    sure that if we get an error on commit, we'll map it to the SQL error
    then attempt to decide if we need to retry or not.

--- kvdb/sqlbase/db.go
+++ kvdb/sqlbase/db.go
@@ -330,16 +330,20 @@ func (db *db) executeTransaction(f func(tx walletdb.ReadWriteTx) error,
 			return fnErr
 		}
 
-		dbErr := tx.Commit()
-		if IsSerializationError(dbErr) {
+		commitErr := tx.Commit()
+		if commitErr != nil {
 			_ = tx.Rollback()
 
-			if waitBeforeRetry(i) {
-				continue
+			dbErr := MapSQLError(fnErr)
+			if IsSerializationError(dbErr) {
+
+				if waitBeforeRetry(i) {
+					continue
+				}
 			}
 		}
 
-		return dbErr
+		return commitErr
 	}
 
 	// If we get to this point, then we weren't able to successfully commit

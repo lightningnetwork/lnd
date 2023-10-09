72f94091e5088f8b91e66526b6ab50da8d049f5b
Elle Mouton <elle.mouton@gmail.com>
2023-10-09 13:37:20 +0200

    channeldb: prep waiting proof store for taproot proofs
    TODO: will need to add a migration for this new encoding format.

--- channeldb/waitingproof_test.go
+++ channeldb/waitingproof_test.go
@@ -1,11 +1,11 @@
 package channeldb
 
 import (
-	"reflect"
+	"errors"
+	"math/rand"
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
-	"github.com/go-errors/errors"
+	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/stretchr/testify/require"
 )
@@ -18,40 +18,75 @@ func TestWaitingProofStore(t *testing.T) {
 	db, err := MakeTestDB(t)
 	require.NoError(t, err, "failed to make test database")
 
-	proof1 := NewWaitingProof(true, &lnwire.AnnounceSignatures{
+	proof1 := NewLegacyWaitingProof(true, &lnwire.AnnounceSignatures{
 		NodeSignature:    wireSig,
 		BitcoinSignature: wireSig,
-		ExtraOpaqueData:  make([]byte, 0),
+		ShortChannelID: lnwire.ShortChannelID{
+			BlockHeight: 1000,
+		},
+		ExtraOpaqueData: make([]byte, 0),
 	})
 
+	// No agg nonce.
+	proof2 := NewTaprootWaitingProof(true, &lnwire.AnnouncementSignatures2{
+		ShortChannelID: lnwire.ShortChannelID{
+			BlockHeight: 2000,
+		},
+		PartialSignature: *randPartialSig(t),
+		ExtraOpaqueData:  make([]byte, 0),
+	}, nil)
+
+	// With agg nonce.
+	priv, err := btcec.NewPrivateKey()
+	require.NoError(t, err)
+
+	proof3 := NewTaprootWaitingProof(true, &lnwire.AnnouncementSignatures2{
+		ShortChannelID: lnwire.ShortChannelID{
+			BlockHeight: 2000,
+		},
+		PartialSignature: *randPartialSig(t),
+		ExtraOpaqueData:  make([]byte, 0),
+	}, priv.PubKey())
+
+	proofs := []*WaitingProof{
+		proof1,
+		proof2,
+		proof3,
+	}
+
 	store, err := NewWaitingProofStore(db)
-	if err != nil {
-		t.Fatalf("unable to create the waiting proofs storage: %v",
-			err)
-	}
+	require.NoError(t, err)
 
-	if err := store.Add(proof1); err != nil {
-		t.Fatalf("unable add proof to storage: %v", err)
-	}
+	for _, proof := range proofs {
+		require.NoError(t, store.Add(proof))
 
-	proof2, err := store.Get(proof1.Key())
-	require.NoError(t, err, "unable retrieve proof from storage")
-	if !reflect.DeepEqual(proof1, proof2) {
-		t.Fatalf("wrong proof retrieved: expected %v, got %v",
-			spew.Sdump(proof1), spew.Sdump(proof2))
-	}
+		p2, err := store.Get(proof.Key())
+		require.NoError(t, err, "unable retrieve proof from storage")
+		require.Equal(t, proof, p2)
 
-	if _, err := store.Get(proof1.OppositeKey()); err != ErrWaitingProofNotFound {
-		t.Fatalf("proof shouldn't be found: %v", err)
-	}
+		_, err = store.Get(proof.OppositeKey())
+		require.ErrorIs(t, err, ErrWaitingProofNotFound)
 
-	if err := store.Remove(proof1.Key()); err != nil {
-		t.Fatalf("unable remove proof from storage: %v", err)
+		err = store.Remove(proof.Key())
+		require.NoError(t, err)
 	}
 
 	if err := store.ForAll(func(proof *WaitingProof) error {
 		return errors.New("storage should be empty")
 	}, func() {}); err != nil && err != ErrWaitingProofNotFound {
 		t.Fatal(err)
+	}
+}
+
+func randPartialSig(t *testing.T) *lnwire.PartialSig {
+	var sigBytes [32]byte
+	_, err := rand.Read(sigBytes[:])
+	require.NoError(t, err)
+
+	var s btcec.ModNScalar
+	s.SetByteSlice(sigBytes[:])
+
+	return &lnwire.PartialSig{
+		Sig: s,
 	}
 }

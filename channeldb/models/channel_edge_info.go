31b94200c0016dfc04bb31f8d97671e230653ae7
Elle Mouton <elle.mouton@gmail.com>
2023-10-27 09:40:02 +0200

    channeldb: add ChannelEdgeInfo2
    And ensure that it implements the ChannelEdgeInfo interface.

--- channeldb/models/channel_edge_info.go
+++ channeldb/models/channel_edge_info.go
@@ -352,3 +352,212 @@ func (c *ChannelEdgeInfo1) FundingScript() ([]byte, error) {
 // A compile-time check to ensure that ChannelEdgeInfo1 implements the
 // ChannelEdgeInfo interface.
 var _ ChannelEdgeInfo = (*ChannelEdgeInfo1)(nil)
+
+// ChannelEdgeInfo2 describes the information about a channel announced with
+// lnwire.ChannelAnnouncement2 that we will persist.
+type ChannelEdgeInfo2 struct {
+	lnwire.ChannelAnnouncement2
+
+	// ChannelPoint is the funding outpoint of the channel. This can be
+	// used to uniquely identify the channel within the channel graph.
+	ChannelPoint wire.OutPoint
+
+	// AuthProof is the authentication proof for this channel.
+	AuthProof *ChannelAuthProof2
+
+	nodeKey1 *btcec.PublicKey
+	nodeKey2 *btcec.PublicKey
+}
+
+// Copy returns a copy of the ChannelEdgeInfo.
+//
+// NOTE: this is part of the ChannelEdgeInfo interface.
+func (c *ChannelEdgeInfo2) Copy() ChannelEdgeInfo {
+	return &ChannelEdgeInfo2{
+		ChannelAnnouncement2: lnwire.ChannelAnnouncement2{
+			ChainHash:       c.ChainHash,
+			Features:        c.Features,
+			ShortChannelID:  c.ShortChannelID,
+			Capacity:        c.Capacity,
+			NodeID1:         c.NodeID1,
+			NodeID2:         c.NodeID2,
+			BitcoinKey1:     c.BitcoinKey1,
+			BitcoinKey2:     c.BitcoinKey2,
+			MerkleRootHash:  c.MerkleRootHash,
+			ExtraOpaqueData: c.ExtraOpaqueData,
+		},
+		ChannelPoint: c.ChannelPoint,
+		AuthProof:    c.AuthProof,
+	}
+}
+
+// Node1Bytes returns bytes of the public key of node 1.
+//
+// NOTE: this is part of the ChannelEdgeInfo interface.
+func (c *ChannelEdgeInfo2) Node1Bytes() [33]byte {
+	return c.NodeID1
+}
+
+// Node2Bytes returns bytes of the public key of node 2.
+//
+// NOTE: this is part of the ChannelEdgeInfo interface.
+func (c *ChannelEdgeInfo2) Node2Bytes() [33]byte {
+	return c.NodeID2
+}
+
+// GetChainHash returns the hash of the genesis block of the chain that the edge
+// is on.
+//
+// NOTE: this is part of the ChannelEdgeInfo interface.
+func (c *ChannelEdgeInfo2) GetChainHash() chainhash.Hash {
+	return c.ChainHash
+}
+
+// GetChanID returns the channel ID.
+//
+// NOTE: this is part of the ChannelEdgeInfo interface.
+func (c *ChannelEdgeInfo2) GetChanID() uint64 {
+	return c.ShortChannelID.ToUint64()
+}
+
+// GetAuthProof returns the ChannelAuthProof for the edge.
+//
+// NOTE: this is part of the models.ChannelEdgeInfo interface.
+func (c *ChannelEdgeInfo2) GetAuthProof() ChannelAuthProof {
+	// Cant just return AuthProof cause you then run into the
+	// nil interface gotcha.
+	if c.AuthProof == nil {
+		return nil
+	}
+
+	return c.AuthProof
+}
+
+// GetCapacity returns the capacity of the channel.
+//
+// NOTE: this is part of the models.ChannelEdgeInfo interface.
+func (c *ChannelEdgeInfo2) GetCapacity() btcutil.Amount {
+	return btcutil.Amount(c.Capacity)
+}
+
+// SetAuthProof sets the proof of the channel.
+//
+// NOTE: this is part of the ChannelEdgeInfo interface.
+func (c *ChannelEdgeInfo2) SetAuthProof(proof ChannelAuthProof) error {
+	if proof == nil {
+		c.AuthProof = nil
+
+		return nil
+	}
+
+	p, ok := proof.(*ChannelAuthProof2)
+	if !ok {
+		return fmt.Errorf("expected type ChannelAuthProof2 for "+
+			"ChannelEdgeInfo2, got %T", proof)
+	}
+
+	c.AuthProof = p
+
+	return nil
+}
+
+// GetChanPoint returns the outpoint of the funding transaction of the channel.
+//
+// NOTE: this is part of the ChannelEdgeInfo interface.
+func (c *ChannelEdgeInfo2) GetChanPoint() wire.OutPoint {
+	return c.ChannelPoint
+}
+
+// FundingScript returns the pk script for the funding output of the
+// channel.
+//
+// NOTE: this is part of the ChannelEdgeInfo interface.
+func (c *ChannelEdgeInfo2) FundingScript() ([]byte, error) {
+	var (
+		pubKey1 *btcec.PublicKey
+		pubKey2 *btcec.PublicKey
+		err     error
+	)
+	c.BitcoinKey1.WhenSome(func(key [33]byte) {
+		pubKey1, err = btcec.ParsePubKey(key[:])
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	c.BitcoinKey2.WhenSome(func(key [33]byte) {
+		pubKey2, err = btcec.ParsePubKey(key[:])
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO(elle): account for all types.
+	if pubKey1 == nil || pubKey2 == nil {
+		return nil, fmt.Errorf("both bitcoin keys must be set")
+	}
+	if c.MerkleRootHash.IsSome() {
+		return nil, fmt.Errorf("dont know how to handle merkle " +
+			"root hash yet")
+	}
+
+	fundingScript, _, err := input.GenTaprootFundingScript(
+		pubKey1, pubKey2, 0,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return fundingScript, nil
+}
+
+// NodeKey1 is the identity public key of the "first" node that was involved in
+// the creation of this channel. A node is considered "first" if the
+// lexicographical ordering the its serialized public key is "smaller" than
+// that of the other node involved in channel creation.
+//
+// NOTE: By having this method to access an attribute, we ensure we only need
+// to fully deserialize the pubkey if absolutely necessary.
+//
+// NOTE: this is part of the ChannelEdgeInfo interface.
+func (c *ChannelEdgeInfo2) NodeKey1() (*btcec.PublicKey, error) {
+	if c.nodeKey1 != nil {
+		return c.nodeKey1, nil
+	}
+
+	key, err := btcec.ParsePubKey(c.NodeID1[:])
+	if err != nil {
+		return nil, err
+	}
+	c.nodeKey1 = key
+
+	return key, nil
+}
+
+// NodeKey2 is the identity public key of the "second" node that was
+// involved in the creation of this channel. A node is considered
+// "second" if the lexicographical ordering the its serialized public
+// key is "larger" than that of the other node involved in channel
+// creation.
+//
+// NOTE: By having this method to access an attribute, we ensure we only need
+// to fully deserialize the pubkey if absolutely necessary.
+//
+// NOTE: this is part of the ChannelEdgeInfo interface.
+func (c *ChannelEdgeInfo2) NodeKey2() (*btcec.PublicKey, error) {
+	if c.nodeKey2 != nil {
+		return c.nodeKey2, nil
+	}
+
+	key, err := btcec.ParsePubKey(c.NodeID2[:])
+	if err != nil {
+		return nil, err
+	}
+	c.nodeKey2 = key
+
+	return key, nil
+}
+
+// A compile-time check to ensure that ChannelEdgeInfo2 implements the
+// ChannelEdgeInfo interface.
+var _ ChannelEdgeInfo = (*ChannelEdgeInfo2)(nil)

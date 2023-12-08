3459bcca1b488c1505132d7db45c7d94c24c3b4a
Keagan McClelland <keagan.mcclelland@gmail.com>
2023-12-08 12:50:43 -0800

    htlcswitch: define state machine for quiescence
    htlcswitch: add sendOwedStfu method to quiescer

---
+++ htlcswitch/quiescer_test.go
@@ -0,0 +1,131 @@
+package htlcswitch
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/lightningnetwork/lnd/fn"
+	"github.com/lightningnetwork/lnd/lntypes"
+	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/stretchr/testify/require"
+)
+
+var cid = lnwire.ChannelID(bytes.Repeat([]byte{0x00}, 32))
+
+type quiescerTestHarness struct {
+	pendingUpdates lntypes.Dual[uint64]
+	quiescer       quiescer
+	conn           <-chan lnwire.Stfu
+}
+
+func initQuiescerTestHarness() *quiescerTestHarness {
+	conn := make(chan lnwire.Stfu, 1)
+	harness := &quiescerTestHarness{
+		pendingUpdates: lntypes.Dual[uint64]{},
+		conn:           conn,
+	}
+
+	harness.quiescer = newQuiescer(quiescerCfg{
+		chanID: cid,
+		numPendingUpdates: func(whoseUpdate lntypes.ChannelParty,
+			_ lntypes.ChannelParty) uint64 {
+
+			return harness.pendingUpdates.GetForParty(whoseUpdate)
+		},
+		sendMsg: func(msg lnwire.Stfu) error {
+			conn <- msg
+			return nil
+		},
+	})
+
+	return harness
+}
+
+func TestQuiescerDoubleRecvInvalid(t *testing.T) {
+	t.Parallel()
+
+	harness := initQuiescerTestHarness()
+
+	msg := lnwire.Stfu{
+		ChanID:    cid,
+		Initiator: true,
+	}
+
+	err := harness.quiescer.recvStfu(msg)
+	require.NoError(t, err)
+	err = harness.quiescer.recvStfu(msg)
+	require.Error(t, err)
+}
+
+func TestQuiescerPendingUpdatesRecvInvalid(t *testing.T) {
+	t.Parallel()
+
+	harness := initQuiescerTestHarness()
+
+	msg := lnwire.Stfu{
+		ChanID:    cid,
+		Initiator: true,
+	}
+
+	harness.pendingUpdates.SetForParty(lntypes.Remote, 1)
+	err := harness.quiescer.recvStfu(msg)
+	require.Error(t, err)
+}
+
+func TestQuiescenceRemoteInit(t *testing.T) {
+	t.Parallel()
+
+	harness := initQuiescerTestHarness()
+
+	msg := lnwire.Stfu{
+		ChanID:    cid,
+		Initiator: true,
+	}
+
+	harness.pendingUpdates.SetForParty(lntypes.Local, 1)
+
+	err := harness.quiescer.recvStfu(msg)
+	require.NoError(t, err)
+
+	err = harness.quiescer.sendOwedStfu()
+	require.NoError(t, err)
+
+	select {
+	case <-harness.conn:
+		t.Fatalf("stfu sent when not expected")
+	default:
+	}
+
+	harness.pendingUpdates.SetForParty(lntypes.Local, 0)
+	err = harness.quiescer.sendOwedStfu()
+	require.NoError(t, err)
+
+	select {
+	case msg := <-harness.conn:
+		require.False(t, msg.Initiator)
+	default:
+		t.Fatalf("stfu not sent when expected")
+	}
+}
+
+func TestQuiescenceInitiator(t *testing.T) {
+	t.Parallel()
+
+	harness := initQuiescerTestHarness()
+	require.True(t, harness.quiescer.quiescenceInitiator().IsErr())
+
+	// Receive
+	msg := lnwire.Stfu{
+		ChanID:    cid,
+		Initiator: true,
+	}
+	require.NoError(t, harness.quiescer.recvStfu(msg))
+	require.True(t, harness.quiescer.quiescenceInitiator().IsErr())
+
+	// Send
+	require.NoError(t, harness.quiescer.sendOwedStfu())
+	require.Equal(
+		t, harness.quiescer.quiescenceInitiator(),
+		fn.Ok(lntypes.Remote),
+	)
+}

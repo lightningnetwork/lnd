de578538f62135aac30ea51972204d581f1b0d06
Keagan McClelland <keagan.mcclelland@gmail.com>
2023-12-11 21:41:31 -0800

    htlcswitch: implement stfu response

--- htlcswitch/link.go
+++ htlcswitch/link.go
@@ -380,6 +380,10 @@ type channelLink struct {
 	// our next CommitSig.
 	incomingCommitHooks hookMap
 
+	// quiescer is the state machine that tracks where this channel is with
+	// respect to the quiescence protocol.
+	quiescer
+
 	wg   sync.WaitGroup
 	quit chan struct{}
 }
@@ -447,6 +451,11 @@ func NewChannelLink(cfg ChannelLinkConfig,
 
 	logPrefix := fmt.Sprintf("ChannelLink(%v):", channel.ChannelPoint())
 
+	qsm := quiescer{
+		chanID:   lnwire.NewChanIDFromOutPoint(channel.ChannelPoint()),
+		weOpened: channel.IsInitiator(),
+	}
+
 	return &channelLink{
 		cfg:                 cfg,
 		channel:             channel,
@@ -456,6 +465,7 @@ func NewChannelLink(cfg ChannelLinkConfig,
 		flushHooks:          newHookMap(),
 		outgoingCommitHooks: newHookMap(),
 		incomingCommitHooks: newHookMap(),
+		quiescer:            qsm,
 		quit:                make(chan struct{}),
 	}
 }
@@ -2276,6 +2286,10 @@ func (l *channelLink) handleUpstreamMsg(msg lnwire.Message) {
 			}
 		}
 
+		// If we need to send out an Stfu, this would be the time to do
+		// so.
+		l.drivePendingStfuSends()
+
 		// Now that we have finished processing the incoming CommitSig
 		// and sent out our RevokeAndAck, we invoke the flushHooks if
 		// the channel state is clean.
@@ -2387,6 +2401,45 @@ func (l *channelLink) handleUpstreamMsg(msg lnwire.Message) {
 		// Update the mailbox's feerate as well.
 		l.mailBox.SetFeeRate(fee)
 
+	case *lnwire.Stfu:
+		handleErr := func(err error) {
+			l.log.Error(err.Error())
+			l.fail(
+				LinkFailureError{
+					code:             ErrInvalidUpdate,
+					FailureAction:    LinkFailureDisconnect,
+					PermanentFailure: false,
+					Warning:          true,
+				},
+				err.Error(),
+			)
+		}
+
+		remoteOnLocal := l.channel.NumRemoteUpdatesPendingOnLocal()
+		remoteOnRemote := l.channel.NumRemoteUpdatesPendingOnRemote()
+		if remoteOnLocal > 0 || remoteOnRemote > 0 {
+			// This is invalid according to the spec, drop the
+			// connection.
+			err := fmt.Errorf(
+				"received stfu while remote updates pending "+
+					"on Local (%d) or Remote (%d)",
+				remoteOnLocal,
+				remoteOnRemote,
+			)
+			handleErr(err)
+
+			return
+		}
+
+		err := l.quiescer.recvStfu(*msg)
+		if err != nil {
+			handleErr(err)
+			return
+		}
+
+		// If we can immediately send an Stfu response back , we will.
+		l.drivePendingStfuSends()
+
 	// In the case where we receive a warning message from our peer, just
 	// log it and move on. We choose not to disconnect from our peer,
 	// although we "MAY" do so according to the specification.
@@ -2417,6 +2470,49 @@ func (l *channelLink) handleUpstreamMsg(msg lnwire.Message) {
 		l.log.Warnf("received unknown message of type %T", msg)
 	}
 
+}
+
+// drivePendingStfuSends checks to see if we owe any Stfu messages to our peer.
+// If we do then we will send them and record that send. We can owe our peer an
+// Stfu either if we have a pending request to quiesce the channel or our peer
+// has sent us an Stfu to which we haven't yet responded.
+func (l *channelLink) drivePendingStfuSends() {
+	if !l.quiescer.oweStfu() {
+		return
+	}
+
+	l.log.Debug("stfu owed")
+
+	handleErr := func(err error) {
+		l.log.Error(err.Error())
+		l.fail(
+			LinkFailureError{
+				code:             ErrInvalidUpdate,
+				FailureAction:    LinkFailureDisconnect,
+				PermanentFailure: false,
+				Warning:          true,
+			},
+			err.Error(),
+		)
+	}
+
+	localOnRemote := l.channel.NumLocalUpdatesPendingOnRemote()
+	localOnLocal := l.channel.NumLocalUpdatesPendingOnLocal()
+	if localOnRemote == 0 && localOnLocal == 0 {
+		l.log.Debug("all local updates synced, sending stfu")
+		oStfu, err := l.quiescer.sendStfu()
+		if err != nil {
+			handleErr(err)
+			return
+		}
+		oStfu.WhenSome(func(stfu lnwire.Stfu) {
+			err := l.cfg.Peer.SendMessage(false, &stfu)
+			if err != nil {
+				handleErr(err)
+				return
+			}
+		})
+	}
 }
 
 // ackDownStreamPackets is responsible for removing htlcs from a link's mailbox

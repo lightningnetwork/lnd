c6e48beafb33e5d3a29e7adad1f9e112bda4a108
yyforyongyu <yy2452@columbia.edu>
2024-01-16 15:21:32 +0800

    CI: upgrade bitcoind version to 25
    We remove the env variable used in workflows and specify the version in
    a single file to make sure we won't misconfig again.

--- scripts/install_bitcoind.sh
+++ scripts/install_bitcoind.sh
@@ -2,9 +2,9 @@
 
 set -ev
 
-BITCOIND_VERSION=${BITCOIN_VERSION:-25.0}
+BITCOIND_VERSION=25
 
 docker pull lightninglabs/bitcoin-core:$BITCOIND_VERSION
 CONTAINER_ID=$(docker create lightninglabs/bitcoin-core:$BITCOIND_VERSION)
-sudo docker cp $CONTAINER_ID:/opt/bitcoin-$BITCOIND_VERSION/bin/bitcoind /usr/local/bin/bitcoind
+sudo docker cp $CONTAINER_ID:/opt/bitcoin-$BITCOIND_VERSION.0/bin/bitcoind /usr/local/bin/bitcoind
 docker rm $CONTAINER_ID

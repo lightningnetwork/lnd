bf8df499ea6f6e07d0666e22f1752704058badde
Olaoluwa Osuntokun <laolu32@gmail.com>
2024-01-31 19:09:29 -0800

    htlcswitch: change LinkDirection to be a type alias
    In this commit, we change LinkDirection to be a type alias. This makes
    creating wrapper structs/interfaces easier, as we don't need to leak
    htlcswitch.LinkDirection, instead we can accept a bool.

--- htlcswitch/interfaces.go
+++ htlcswitch/interfaces.go
@@ -182,7 +182,7 @@ type FlushHookID uint64
 
 // LinkDirection is used to query and change any link state on a per-direction
 // basis.
-type LinkDirection bool
+type LinkDirection = bool
 
 const (
 	// Incoming is the direction from the remote peer to our node.

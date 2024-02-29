d6ef2a122f833b7916608be87a5f824fa18722f0
George Tsagkarelis <george.tsagkarelis@gmail.com>
2024-02-29 13:07:37 +0100

    routerrpc: add XAddLocalChanAliases & XDeleteLocalChanAliases

--- rpcserver.go
+++ rpcserver.go
@@ -761,7 +761,7 @@ func (r *rpcServer) addDeps(s *server, macService *macaroons.Service,
 		s.sweeper, tower, s.towerClientMgr, r.cfg.net.ResolveTCPAddr,
 		genInvoiceFeatures, genAmpInvoiceFeatures,
 		s.getNodeAnnouncement, s.updateAndBrodcastSelfNode, parseAddr,
-		rpcsLog, s.aliasMgr.GetPeerAlias,
+		rpcsLog, s.aliasMgr,
 	)
 	if err != nil {
 		return err
@@ -8548,17 +8548,8 @@ func (r *rpcServer) ListAliases(ctx context.Context,
 		AliasMaps: make([]*lnrpc.AliasMap, 0),
 	}
 
-	for base, set := range mapAliases {
-		rpcMap := &lnrpc.AliasMap{
-			BaseScid: base.ToUint64(),
-		}
-		for _, alias := range set {
-			rpcMap.Aliases = append(
-				rpcMap.Aliases, alias.ToUint64(),
-			)
-		}
-		resp.AliasMaps = append(resp.AliasMaps, rpcMap)
-	}
+	// Now we need to parse the created mappings into an rpc response.
+	resp.AliasMaps = lnrpc.MarshalAliasMap(mapAliases)
 
 	return resp, nil
 }

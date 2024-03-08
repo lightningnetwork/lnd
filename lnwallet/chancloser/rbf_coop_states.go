71a79d3134fa9e0b9c821873fbf3b22bfbe80cd9
Olaoluwa Osuntokun <laolu32@gmail.com>
2024-03-07 20:08:13 -0800

    lnwallet/chancloser: add fee rate to ClosePending
    This'll be useful to communicate what the new fee rate is to an RPC caller.

--- lnwallet/chancloser/rbf_coop_states.go
+++ lnwallet/chancloser/rbf_coop_states.go
@@ -630,6 +630,9 @@ type LocalOfferSent struct {
 	// ProposedFee is the fee we proposed to the remote party.
 	ProposedFee btcutil.Amount
 
+	// ProposedFeeRate is the fee rate we proposed to the remote party.
+	ProposedFeeRate chainfee.SatPerVByte
+
 	// LocalSig is the signature we sent to the remote party.
 	LocalSig lnwire.Sig
 }
@@ -668,6 +671,9 @@ func (l *LocalOfferSent) IsTerminal() bool {
 type ClosePending struct {
 	// CloseTx is the pending close transaction.
 	CloseTx *wire.MsgTx
+
+	// FeeRate is the fee rate of the closing transaction.
+	FeeRate chainfee.SatPerVByte
 }
 
 // ShouldRouteTo returns true if the target state should process the target

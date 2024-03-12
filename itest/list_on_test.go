a015ab2a05aa1b955c14a16dec99c241b1e7b9af
Keagan McClelland <keagan.mcclelland@gmail.com>
2024-03-12 12:26:01 -0700

    itest+lntest: add itest for Quiesce RPC method

--- itest/list_on_test.go
+++ itest/list_on_test.go
@@ -574,4 +574,8 @@ var allTestCases = []*lntest.TestCase{
 		Name:     "coop close with htlcs",
 		TestFunc: testCoopCloseWithHtlcs,
 	},
+	{
+		Name:     "quiescence",
+		TestFunc: testQuiescence,
+	},
 }

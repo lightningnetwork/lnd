eeee2979e59bbd1182529430fe81bec995730e4c
Olaoluwa Osuntokun <laolu32@gmail.com>
2024-03-13 10:47:12 -0400

    lnwallet/chanfunding: add optional tapscript root

--- lnwallet/chanfunding/canned_assembler.go
+++ lnwallet/chanfunding/canned_assembler.go
@@ -58,6 +58,14 @@ type ShimIntent struct {
 	// generate an aggregate key to use as the taproot-native multi-sig
 	// output.
 	musig2 bool
+
+	// tapscriptRoot is the root of the tapscript tree that will be used to
+	// create the funding output. This field will only be utilized if the
+	// MuSig2 flag above is set to true.
+	//
+	// TODO(roasbeef): fold above into new chan type? sum type like thing,
+	// includes the tapscript root, etc
+	tapscriptRoot fn.Option[chainhash.Hash]
 }
 
 // FundingOutput returns the witness script, and the output that creates the
@@ -79,7 +87,7 @@ func (s *ShimIntent) FundingOutput() ([]byte, *wire.TxOut, error) {
 		// the keys are sorted before use.
 		return input.GenTaprootFundingScript(
 			s.localKey.PubKey, s.remoteKey, int64(totalAmt),
-			fn.None[chainhash.Hash](),
+			s.tapscriptRoot,
 		)
 	}
 

c0f5ac958d853a93701cbde32f297c7c100e80be
yyforyongyu <yy2452@columbia.edu>
2024-04-03 00:59:30 +0800

    x

--- itest/list_on_test.go
+++ itest/list_on_test.go
@@ -586,4 +586,8 @@ var allTestCases = []*lntest.TestCase{
 		Name:     "sweep commit output and anchor",
 		TestFunc: testSweepCommitOutputAndAnchor,
 	},
+	{
+		Name:     "sweep htlcs",
+		TestFunc: testSweepHTLCs,
+	},
 }

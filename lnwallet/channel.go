9cc8fa41963c839f46161e7d1990b65ee9447286
Olaoluwa Osuntokun <laolu32@gmail.com>
2024-04-01 20:07:15 -0700

    lnwallet: add custom tlv blob to internal commitment struct
    In this commit, we also add the custom TLV blob to the internal commitment struct that we use within the in-memory commitment linked list.
    
    This'll be useful to ensure that we're tracking the current blob for our in memory commitment for when we need to write it to disk.

--- lnwallet/channel.go
+++ lnwallet/channel.go
@@ -32,6 +32,7 @@ import (
 	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/shachain"
+	"github.com/lightningnetwork/lnd/tlv"
 )
 
 var (
@@ -546,6 +547,10 @@ type commitment struct {
 	// on this commitment transaction.
 	incomingHTLCs []PaymentDescriptor
 
+	// customBlob stores opaque bytes that may be used by custom channels
+	// to store extra data for a given commitment state.
+	customBlob fn.Option[tlv.Blob]
+
 	// [outgoing|incoming]HTLCIndex is an index that maps an output index
 	// on the commitment transaction to the payment descriptor that
 	// represents the HTLC output.
@@ -726,6 +731,7 @@ func (c *commitment) toDiskCommit(ourCommit bool) *channeldb.ChannelCommitment {
 		CommitTx:        c.txn,
 		CommitSig:       c.sig,
 		Htlcs:           make([]channeldb.HTLC, 0, numHtlcs),
+		CustomBlob:      c.customBlob,
 	}
 
 	for _, htlc := range c.outgoingHTLCs {

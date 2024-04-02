ac911da436ba10c95afb1f2e0444e63c29363a60
Andras Banki-Horvath <bhandras@gmail.com>
2024-04-02 15:16:07 +0200

    kvdb: remove the unused SQL error mappers

--- kvdb/sqlbase/sqlerrors_no_postgres.go
+++
@@ -1,9 +0,0 @@
-//go:build !kvdb_postgres
-
-package sqlbase
-
-// parsePostgresError attempts to parse a postgres error as a database agnostic
-// SQL error.
-func parsePostgresError(err error) error {
-	return nil
-}
--- kvdb/sqlbase/sqlerrors_no_sqlite.go
+++
@@ -1,9 +0,0 @@
-//go:build !kvdb_sqlite || (windows && (arm || 386)) || (linux && (ppc64 || mips || mipsle || mips64))
-
-package sqlbase
-
-// parseSqliteError attempts to parse a sqlite error as a database agnostic
-// SQL error.
-func parseSqliteError(err error) error {
-	return nil
-}

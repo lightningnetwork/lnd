716d4c0f44795d958196c650f730999412dbac35
Olaoluwa Osuntokun <laolu32@gmail.com>
2024-04-04 17:27:20 -0700

    funding: create new AuxFundingController interface
    In this commit, we make a new `AuxFundingController` interface capable of processing messages off the wire. In addition, we can use it to abstract away details w.r.t how we obtain a `AuxFundingDesc` for a given channel.
    
    We'll now use this whenever we get a channel funding request, to make sure we pass along the custom state that a channel may require.

--- funding/manager.go
+++ funding/manager.go
@@ -539,6 +539,12 @@ type Config struct {
 	// AliasManager is an implementation of the aliasHandler interface that
 	// abstracts away the handling of many alias functions.
 	AliasManager aliasHandler
+
+	// AuxFundingController is an optional controller that can be used to
+	// modify the way we handle certain custom chanenl types. It's also
+	// able to automatically handle new custom protocol messages related to
+	// the funding process.
+	AuxFundingController fn.Option[AuxFundingController]
 }
 
 // Manager acts as an orchestrator/bridge between the wallet's
@@ -1604,6 +1610,15 @@ func (f *Manager) fundeeProcessOpenChannel(peer lnpeer.Peer,
 		return
 	}
 
+	// At this point, if we have an AuxFundingController active, we'll
+	// check to see if we have any aux info that we should carry along for
+	// this pid.
+	auxFundingDesc := fn.MapOption(
+		func(a AuxFundingController) fn.Option[lnwallet.AuxFundingDesc] {
+			return a.DescFromPendingChanID(msg.PendingChannelID)
+		},
+	)(f.cfg.AuxFundingController)
+
 	req := &lnwallet.InitFundingReserveMsg{
 		ChainHash:        &msg.ChainHash,
 		PendingChanID:    msg.PendingChannelID,
@@ -1620,6 +1635,7 @@ func (f *Manager) fundeeProcessOpenChannel(peer lnpeer.Peer,
 		ZeroConf:         zeroConf,
 		OptionScidAlias:  scid,
 		ScidAliasFeature: scidFeatureVal,
+		AuxFundingDesc:   fn.FlattenOption(auxFundingDesc),
 	}
 
 	reservation, err := f.cfg.Wallet.InitChannelReservation(req)
@@ -4594,6 +4610,15 @@ func (f *Manager) handleInitFundingMsg(msg *InitFundingMsg) {
 		scidFeatureVal = true
 	}
 
+	// At this point, if we have an AuxFundingController active, we'll
+	// check to see if we have any aux info that we should carry along for
+	// this pid.
+	auxFundingDesc := fn.MapOption(
+		func(a AuxFundingController) fn.Option[lnwallet.AuxFundingDesc] {
+			return a.DescFromPendingChanID(chanID)
+		},
+	)(f.cfg.AuxFundingController)
+
 	req := &lnwallet.InitFundingReserveMsg{
 		ChainHash:         &msg.ChainHash,
 		PendingChanID:     chanID,
@@ -4617,6 +4642,7 @@ func (f *Manager) handleInitFundingMsg(msg *InitFundingMsg) {
 		OptionScidAlias:   scid,
 		ScidAliasFeature:  scidFeatureVal,
 		Memo:              msg.Memo,
+		AuxFundingDesc:    fn.FlattenOption(auxFundingDesc),
 	}
 
 	reservation, err := f.cfg.Wallet.InitChannelReservation(req)

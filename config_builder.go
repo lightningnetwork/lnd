c98aeaa28581a316ea67a7a0df5be311441fa8da
Olaoluwa Osuntokun <laolu32@gmail.com>
2024-04-04 17:48:11 -0700

    config+serer: add AuxFundingController as top level cfg option

--- config_builder.go
+++ config_builder.go
@@ -34,6 +34,7 @@ import (
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/clock"
 	"github.com/lightningnetwork/lnd/fn"
+	"github.com/lightningnetwork/lnd/funding"
 	"github.com/lightningnetwork/lnd/invoices"
 	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/kvdb"
@@ -150,6 +151,12 @@ type ImplementationCfg struct {
 	// MsgRouter is an optional message router that if set will be used in
 	// place of a new balnk default message router.
 	MsgRouter fn.Option[protofsm.MsgRouter]
+
+	// AuxFundingController is an optional controller that can be used to
+	// modify the way we handle certain custom chanenl types. It's also
+	// able to automatically handle new custom protocol messages related to
+	// the funding process.
+	AuxFundingController fn.Option[funding.AuxFundingController]
 }
 
 // DefaultWalletImpl is the default implementation of our normal, btcwallet

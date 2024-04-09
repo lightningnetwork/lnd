9b869f95bf35dd0c12069e6f9eb5c9eb480092fc
Olaoluwa Osuntokun <laolu32@gmail.com>
2024-04-08 19:47:26 -0700

    lnwallet: add WithAuxSigner option to channel

--- lnwallet/channel.go
+++ lnwallet/channel.go
@@ -1377,6 +1377,10 @@ type LightningChannel struct {
 	// signatures, of which there may be hundreds.
 	sigPool *SigPool
 
+	// auxSigner is a special signer used to obtain opaque signatures for
+	// custom channel variants.
+	auxSigner fn.Option[AuxSigner]
+
 	// Capacity is the total capacity of this channel.
 	Capacity btcutil.Amount
 
@@ -1441,6 +1445,7 @@ type channelOpts struct {
 	remoteNonce *musig2.Nonces
 
 	leafStore fn.Option[AuxLeafStore]
+	auxSigner fn.Option[AuxSigner]
 
 	skipNonceInit bool
 }
@@ -1476,6 +1481,13 @@ func WithSkipNonceInit() ChannelOpt {
 func WithLeafStore(store AuxLeafStore) ChannelOpt {
 	return func(o *channelOpts) {
 		o.leafStore = fn.Some[AuxLeafStore](store)
+	}
+}
+
+// WithAuxSigner is used to specify a custom aux signer for the channel.
+func WithAuxSigner(signer AuxSigner) ChannelOpt {
+	return func(o *channelOpts) {
+		o.auxSigner = fn.Some[AuxSigner](signer)
 	}
 }
 
@@ -1522,6 +1534,7 @@ func NewLightningChannel(signer input.Signer,
 	lc := &LightningChannel{
 		Signer:            signer,
 		leafStore:         opts.leafStore,
+		auxSigner:         opts.auxSigner,
 		sigPool:           sigPool,
 		currentHeight:     localCommit.CommitHeight,
 		remoteCommitChain: newCommitmentChain(),

8a7167a7b7508b0840a1c748e871d003f0eaccb5
Olaoluwa Osuntokun <laolu32@gmail.com>
2024-04-17 23:35:44 -0700

    funding+lnwallet: finish hook up new aux funding flow
    For the initiator, once we get the signal that the PSBT has been
    finalized, we'll call into the aux funder to get the funding desc. For
    the responder, once we receive the funding_created message, we'll do the
    same.
    
    We now also have local+remote aux leaves for the commitment transaction.

--- funding/aux_funding.go
+++ funding/aux_funding.go
@@ -2,6 +2,7 @@ package funding
 
 import (
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/fn"
 	"github.com/lightningnetwork/lnd/lnwallet"
 	"github.com/lightningnetwork/lnd/protofsm"
@@ -17,16 +18,46 @@ type AuxFundingController interface {
 	// handle custom messages specific to the funding type.
 	protofsm.MsgEndpoint
 
-	// DescPendingChanID takes a pending channel ID, that may already be
+	// DescFromPendingChanID takes a pending channel ID, that may already be
 	// known due to prior custom channel messages, and maybe returns an aux
 	// funding desc which can be used to modify how a channel is funded.
-	//
-	// TODO(roasbeef): erorr on validation if fail due to invalid root
-	// match?
-	DescFromPendingChanID(PendingChanID) fn.Option[lnwallet.AuxFundingDesc]
+	DescFromPendingChanID(pid PendingChanID,
+		openChan *channeldb.OpenChannel,
+		localKeyRing, remoteKeyRing lnwallet.CommitmentKeyRing,
+		initiator bool) (fn.Option[lnwallet.AuxFundingDesc], error)
 
 	// DeriveTapscriptRoot takes a pending channel ID and maybe returns a
 	// tapscript root that should be used when creating any musig2 sessions
 	// for a channel.
-	DeriveTapscriptRoot(PendingChanID) fn.Option[chainhash.Hash]
+	DeriveTapscriptRoot(PendingChanID) (fn.Option[chainhash.Hash], error)
+}
+
+// descFromPendingChanID takes a pending channel ID, that may already be
+// known due to prior custom channel messages, and maybe returns an aux
+// funding desc which can be used to modify how a channel is funded.
+func descFromPendingChanID(controller fn.Option[AuxFundingController],
+	chanID PendingChanID, openChan *channeldb.OpenChannel,
+	localKeyRing, remoteKeyRing lnwallet.CommitmentKeyRing,
+	initiator bool) (fn.Option[lnwallet.AuxFundingDesc], error) {
+
+	if controller.IsNone() {
+		return fn.None[lnwallet.AuxFundingDesc](), nil
+	}
+
+	return controller.UnsafeFromSome().DescFromPendingChanID(
+		chanID, openChan, localKeyRing, remoteKeyRing, initiator,
+	)
+}
+
+// deriveTapscriptRoot takes a pending channel ID and maybe returns a
+// tapscript root that should be used when creating any musig2 sessions
+// for a channel.
+func deriveTapscriptRoot(controller fn.Option[AuxFundingController],
+	chanID PendingChanID) (fn.Option[chainhash.Hash], error) {
+
+	if controller.IsNone() {
+		return fn.None[chainhash.Hash](), nil
+	}
+
+	return controller.UnsafeFromSome().DeriveTapscriptRoot(chanID)
 }

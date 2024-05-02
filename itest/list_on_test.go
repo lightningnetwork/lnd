01210248600b939eaad5babfb609ef4192e06070
Calvin Zachman <calvin.zachman@protonmail.com>
2024-05-02 14:26:50 -0400

    itest: add track_onion test

--- itest/list_on_test.go
+++ itest/list_on_test.go
@@ -743,6 +743,10 @@ var allTestCases = []*lntest.TestCase{
 		Name:     "send onion",
 		TestFunc: testSendOnion,
 	},
+	{
+		Name:     "track onion",
+		TestFunc: testTrackOnion,
+	},
 }
 
 // appendPrefixed is used to add a prefix to each test name in the subtests
--- itest/lnd_sendonion_test.go
+++ itest/lnd_sendonion_test.go
@@ -1,7 +1,12 @@
 package itest
 
 import (
+	"context"
+
+	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/btcutil"
+	sphinx "github.com/lightningnetwork/lightning-onion"
+	"github.com/lightningnetwork/lnd/htlcswitch"
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/lightningnetwork/lnd/lnrpc/switchrpc"
 	"github.com/lightningnetwork/lnd/lntest"
@@ -127,4 +132,153 @@ func testSendOnion(ht *lntest.HarnessTest) {
 
 	// The invoice should show as settled for Dave.
 	ht.AssertInvoiceSettled(dave, invoices[0].PaymentAddr)
+}
+
+// testTrackOnion exercises the SwitchRPC server's TrackOnion endpoint,
+// confirming that we can receive the result of an onion dispatch and decrypt
+// the error result. We also verify that the error received from the dispatched
+// onion is the same whether error is processed by the server or the rpc client.
+func testTrackOnion(ht *lntest.HarnessTest) {
+	// Create a four-node context consisting of Alice, Bob and two new
+	// nodes: Carol and Dave. This will provide a 4 node, 3 channel topology.
+	// Alice will make a  channel with Bob, and Bob with Carol, and Carol
+	// with Dave such that we arrive at the network topology:
+	//     Alice -> Bob -> Carol -> Dave
+	alice := ht.NewNodeWithCoins("Alice", nil)
+	bob := ht.NewNodeWithCoins("Bob", nil)
+	carol := ht.NewNode("carol", nil)
+	dave := ht.NewNode("dave", nil)
+
+	// Connect nodes to ensure propagation of channels.
+	ht.EnsureConnected(alice, bob)
+	ht.EnsureConnected(bob, carol)
+	ht.EnsureConnected(carol, dave)
+
+	const chanAmt = btcutil.Amount(100000)
+
+	// Open a channel with 100k satoshis between Alice and Bob with Alice
+	// being the sole funder of the channel.
+	chanPointAlice := ht.OpenChannel(
+		alice, bob, lntest.OpenChannelParams{Amt: chanAmt},
+	)
+	defer ht.CloseChannel(alice, chanPointAlice)
+
+	// We'll create Dave and establish a channel between Bob and Carol.
+	ht.FundCoins(btcutil.SatoshiPerBitcoin, dave)
+	chanPointBob := ht.OpenChannel(
+		bob, carol, lntest.OpenChannelParams{Amt: chanAmt},
+	)
+	defer ht.CloseChannel(bob, chanPointBob)
+
+	// Next, we'll create Carol and establish a channel to from her to Dave.
+	ht.FundCoins(btcutil.SatoshiPerBitcoin, carol)
+	chanPointCarol := ht.OpenChannel(
+		carol, dave, lntest.OpenChannelParams{Amt: chanAmt},
+	)
+	defer ht.CloseChannel(carol, chanPointCarol)
+
+	// Make sure Alice knows the channel between Bob and Carol.
+	ht.AssertChannelInGraph(alice, chanPointBob)
+	ht.AssertChannelInGraph(alice, chanPointCarol)
+
+	const paymentAmt = 10000
+
+	// Query for routes to pay from Alice to Dave.
+	routesReq := &lnrpc.QueryRoutesRequest{
+		PubKey: dave.PubKeyStr,
+		Amt:    paymentAmt,
+	}
+	routes := alice.RPC.QueryRoutes(routesReq)
+	route := routes.Routes[0]
+
+	finalHop := route.Hops[len(route.Hops)-1]
+	finalHop.MppRecord = &lnrpc.MPPRecord{
+		PaymentAddr:  ht.Random32Bytes(),
+		TotalAmtMsat: int64(lnwire.NewMSatFromSatoshis(paymentAmt)),
+	}
+
+	// Build the onion to use for our payment.
+	paymentHash := ht.Random32Bytes()
+	onionReq := &switchrpc.BuildOnionRequest{
+		Route:       route,
+		PaymentHash: paymentHash,
+	}
+	onionResp := alice.RPC.BuildOnion(onionReq)
+
+	// Dispatch a payment via SendOnion.
+	firstHop := bob.PubKey
+	sendReq := &switchrpc.SendOnionRequest{
+		FirstHopPubkey: firstHop[:],
+		Amount:         route.TotalAmtMsat,
+		Timelock:       route.TotalTimeLock,
+		PaymentHash:    paymentHash,
+		OnionBlob:      onionResp.OnionBlob,
+		AttemptId:      1,
+	}
+
+	resp := alice.RPC.SendOnion(sendReq)
+	require.True(ht, resp.Success, "expected successful onion send")
+	require.Empty(ht, resp.ErrorMessage, "unexpected failure to send onion")
+
+	serverErrorStr := ""
+	clientErrorStr := ""
+
+	// Track the payment providing all necessary information to delegate
+	// error decryption to the server. We expect this to fail as Dave is not
+	// expecting payment.
+	ctxt, _ := context.WithTimeout(context.Background(), defaultTimeout)
+	trackReq := &switchrpc.TrackOnionRequest{
+		AttemptId:   1,
+		PaymentHash: paymentHash,
+		SessionKey:  onionResp.SessionKey,
+		HopPubkeys:  onionResp.HopPubkeys,
+	}
+	trackResp, err := alice.RPC.Switch.TrackOnion(ctxt, trackReq)
+	require.Nil(ht, err, "unexpected onion tracking error")
+	require.NotEmpty(ht, trackResp.ErrorMessage,
+		"expected onion tracking error")
+
+	serverErrorStr = trackResp.ErrorMessage
+
+	// Now we'll track the same payment attempt, but we'll specify that
+	// we want to handle the error decryption ourselves client side.
+	trackReq = &switchrpc.TrackOnionRequest{
+		AttemptId:   1,
+		PaymentHash: paymentHash,
+	}
+	trackResp, err = alice.RPC.Switch.TrackOnion(ctxt, trackReq)
+	require.Nil(ht, err, "unexpected onion tracking error")
+	require.NotNil(ht, trackResp.EncryptedError, "expected encrypted error")
+
+	// Decrypt and inspect the error from the TrackOnion RPC response.
+	sessionKey, _ := btcec.PrivKeyFromBytes(onionResp.SessionKey)
+	var pubKeys []*btcec.PublicKey
+	for _, keyBytes := range onionResp.HopPubkeys {
+		pubKey, err := btcec.ParsePubKey(keyBytes)
+		if err != nil {
+			ht.Fatalf("Failed to parse public key: %v", err)
+		}
+		pubKeys = append(pubKeys, pubKey)
+	}
+
+	// Construct the circuit to create the error decryptor
+	circuit := &sphinx.Circuit{
+		SessionKey:  sessionKey,
+		PaymentPath: pubKeys,
+	}
+	errorDecryptor := &htlcswitch.SphinxErrorDecrypter{
+		OnionErrorDecrypter: sphinx.NewOnionErrorDecrypter(circuit),
+	}
+
+	// Simulate an RPC client decrypting the onion error.
+	encryptedError := lnwire.OpaqueReason(trackResp.EncryptedError)
+	forwardingError, err := errorDecryptor.DecryptError(encryptedError)
+	require.Nil(ht, err, "unable to decrypt error")
+
+	clientErrorStr = forwardingError.Error()
+
+	serverFwdErr, err := switchrpc.ParseForwardingError(serverErrorStr)
+	require.Nil(ht, err, "expected to parse forwarding error from server")
+	require.Equal(ht, serverFwdErr.Error(), clientErrorStr, "expect error "+
+		"message to match whether handled by client or server")
 }

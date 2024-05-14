0c5301e69de64dcfd4afdcc8b87245c68c87bac6
Viktor Tigerström <vtigerstrom@gmail.com>
2024-05-14 17:06:19 +0200

    multi: start RpcServer before dependencies exist
    To enable an outbound remote signer to connect to lnd before all
    dependencies for the RPC sub-servers are created, we need to separate
    the process of adding dependencies to the sub-servers from starting
    them. Prior to this commit, the RPC sub-servers could only be started
    once all dependencies were in place. This limitation prevented accepting
    an incoming connection request from an outbound remote signer (e.g., a
    walletrpc.SignCoordinatorStreams RPC call) to the WalletKitServer before
    it was started. Therefore, we need to start at least the WalletKitServer
    and the main RPC server before creating the rest of the dependencies.
    
    This commit refactors the logic for the main RPC server and sub-servers,
    allowing them to start before dependencies are inserted into the
    sub-servers. The WalletState for the InterceptorChain is only set to
    RpcActive after all dependencies have been created and inserted,
    ensuring that RPC requests won't be allowed into the sub-servers before
    the dependencies exist. An upcoming commit will set the state to
    AllowRemoteSigner before all dependencies are created, enabling an
    outbound remote signer to connect when needed.

--- lnrpc/devrpc/driver.go
+++ lnrpc/devrpc/driver.go
@@ -9,12 +9,13 @@ import (
 	"github.com/lightningnetwork/lnd/lnrpc"
 )
 
-// createNewSubServer is a helper method that will create the new sub server
-// given the main config dispatcher method. If we're unable to find the config
+// getConfig is a helper method that will fetch the config for sub-server given
+// the main config dispatcher method. If we're unable to find the config
 // that is meant for us in the config dispatcher, then we'll exit with an
-// error.
-func createNewSubServer(configRegistry lnrpc.SubServerConfigDispatcher) (
-	*Server, lnrpc.MacaroonPerms, error) {
+// error. If enforceDependencies is set to true, the function also verifies that
+// the dependencies in the config are properly set.
+func getConfig(configRegistry lnrpc.SubServerConfigDispatcher,
+	enforceDependencies bool) (*Config, error) {
 
 	// We'll attempt to look up the config that we expect, according to our
 	// subServerName name. If we can't find this, then we'll exit with an
@@ -22,32 +23,43 @@ func createNewSubServer(configRegistry lnrpc.SubServerConfigDispatcher) (
 	// config.
 	subServerConf, ok := configRegistry.FetchConfig(subServerName)
 	if !ok {
-		return nil, nil, fmt.Errorf("unable to find config for "+
-			"subserver type %s", subServerName)
+		return nil, fmt.Errorf("unable to find config for subserver "+
+			"type %s", subServerName)
 	}
 
 	// Now that we've found an object mapping to our service name, we'll
 	// ensure that it's the type we need.
 	config, ok := subServerConf.(*Config)
 	if !ok {
-		return nil, nil, fmt.Errorf("wrong type of config for "+
-			"subserver %s, expected %T got %T", subServerName,
-			&Config{}, subServerConf)
+		return nil, fmt.Errorf("wrong type of config for subserver "+
+			"%s, expected %T got %T", subServerName, &Config{},
+			subServerConf)
 	}
 
+	if enforceDependencies {
+		if err := verifyDependencies(config); err != nil {
+			return nil, err
+		}
+	}
+
+	return config, nil
+}
+
+// verifyDependencies ensures that the dependencies in the config are properly
+// set.
+func verifyDependencies(config *Config) error {
 	// Before we try to make the new service instance, we'll perform
 	// some sanity checks on the arguments to ensure that they're useable.
 	if config.ActiveNetParams == nil {
-		return nil, nil, fmt.Errorf("ActiveNetParams must be set to " +
-			"create DevRPC")
-	}
-
-	if config.GraphDB == nil {
-		return nil, nil, fmt.Errorf("GraphDB must be set to create " +
+		return fmt.Errorf("ActiveNetParams must be set to create " +
 			"DevRPC")
 	}
 
-	return New(config)
+	if config.GraphDB == nil {
+		return fmt.Errorf("GraphDB must be set to create DevRPC")
+	}
+
+	return nil
 }
 
 func init() {

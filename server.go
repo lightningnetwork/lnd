6a06421a40bc8d94805811e41b1d4627f8978b2a
Viktor Torstensson <viktor.t.git@gmail.com>
2024-05-14 16:39:21 +0200

    lnd: add `RemoteSignerClient` instance on startup
    This commit adds a `RemoteSignerClient` instance to the main `lnd`
    server. The specific type of `RemoteSignerClient` used will depend on
    the configuration.
    
    If `watchonlynode.enable` is set to `true`, an `OutboundClient` instance
    will be used. Otherwise, a `NoOpClient` instance will be utilized.

--- server.go
+++ server.go
@@ -395,6 +395,8 @@ type server struct {
 
 	tlsManager *TLSManager
 
+	remoteSignerClient rpcwallet.RemoteSignerClient
+
 	// featureMgr dispatches feature vectors for various contexts within the
 	// daemon.
 	featureMgr *feature.Manager
@@ -589,8 +591,8 @@ func newServer(ctx context.Context, cfg *Config, listenAddrs []net.Addr,
 	chansToRestore walletunlocker.ChannelsToRecover,
 	chanPredicate chanacceptor.ChannelAcceptor,
 	torController *tor.Controller, tlsManager *TLSManager,
-	leaderElector cluster.LeaderElector,
-	implCfg *ImplementationCfg) (*server, error) {
+	leaderElector cluster.LeaderElector, implCfg *ImplementationCfg,
+	remoteSignerClient rpcwallet.RemoteSignerClient) (*server, error) {
 
 	var (
 		err         error
@@ -734,6 +736,8 @@ func newServer(ctx context.Context, cfg *Config, listenAddrs []net.Addr,
 		onionMessageServer: subscribe.NewServer(),
 
 		tlsManager: tlsManager,
+
+		remoteSignerClient: remoteSignerClient,
 
 		featureMgr: featureMgr,
 		quit:       make(chan struct{}),
@@ -2187,6 +2191,14 @@ func (s *server) Start(ctx context.Context) error {
 			}
 		}
 
+		ctx := context.TODO()
+
+		cleanup = cleanup.add(s.remoteSignerClient.Stop)
+		if err := s.remoteSignerClient.Start(ctx); err != nil {
+			startErr = err
+			return
+		}
+
 		// Start the notification server. This is used so channel
 		// management goroutines can be notified when a funding
 		// transaction reaches a sufficient number of confirmations, or
@@ -2705,6 +2717,10 @@ func (s *server) Stop() error {
 		if err := s.cc.BestBlockTracker.Stop(); err != nil {
 			srvrLog.Warnf("Unable to stop BestBlockTracker: %v",
 				err)
+		}
+		if err := s.remoteSignerClient.Stop(); err != nil {
+			srvrLog.Warnf("Unable to stop remote signer "+
+				"client: %v", err)
 		}
 		if err := s.chanEventStore.Stop(); err != nil {
 			srvrLog.Warnf("Unable to stop ChannelEventStore: %v",

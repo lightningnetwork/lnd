c9997a77449c65d5c4093ba8bf02711101119b60
Viktor Tigerström <vtigerstrom@gmail.com>
2024-05-14 17:43:19 +0200

    rpcwallet: add InboundRemoteSigner implementation
    This commit wraps the current remote signer implementation in the new
    RemoteSigner interface within an InboundRemoteSigner struct.

--- lnwallet/rpcwallet/remote_signer.go
+++ lnwallet/rpcwallet/remote_signer.go
@@ -2,11 +2,18 @@ package rpcwallet
 
 import (
 	"context"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/lightningnetwork/lnd/lnrpc/signrpc"
 	"github.com/lightningnetwork/lnd/lnrpc/walletrpc"
+	"github.com/lightningnetwork/lnd/macaroons"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"gopkg.in/macaroon.v2"
 )
 
 type (
@@ -90,4 +97,162 @@ type RemoteSignerRequests interface {
 	// the corresponding response.
 	SignPsbt(ctx context.Context, in *walletrpc.SignPsbtRequest,
 		opts ...grpc.CallOption) (*walletrpc.SignPsbtResponse, error)
+}
+
+// InboundRemoteSigner is an abstraction of the connection to an inbound remote
+// signer. An inbound remote signer is a remote signer that allows the
+// watch-only node to connect to it via an inbound GRPC connection.
+type InboundRemoteSigner struct {
+	// Embedded signrpc.SignerClient and walletrpc.WalletKitClient to
+	// implement the RemoteSigner interface.
+	signrpc.SignerClient
+
+	walletrpc.WalletKitClient
+
+	// The host:port of the remote signer node.
+	rpcHost string
+
+	// The path to the TLS certificate of the remote signer node.
+	tlsCertPath string
+
+	// The path to the macaroon of the remote signer node.
+	macaroonPath string
+
+	// The timeout for the connection to the remote signer node.
+	timeout time.Duration
+}
+
+// NewInboundRemoteSigner creates a new InboundRemoteSigner instance.
+// The function sets up a connection to the remote signer node.
+// The returned function is a cleanup function that should be called to close
+// the connection when the remote signer is no longer needed.
+func NewInboundRemoteSigner(rpcHost string, tlsCertPath string,
+	macaroonPath string,
+	timeout time.Duration) (*InboundRemoteSigner, func(), error) {
+
+	rpcConn, err := connect(rpcHost, tlsCertPath, macaroonPath, timeout)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error connecting to the remote "+
+			"signing node through RPC: %v", err)
+	}
+
+	cleanUp := func() {
+		rpcConn.Close()
+	}
+
+	remoteSigner := &InboundRemoteSigner{
+		SignerClient:    signrpc.NewSignerClient(rpcConn),
+		WalletKitClient: walletrpc.NewWalletKitClient(rpcConn),
+		rpcHost:         rpcHost,
+		tlsCertPath:     tlsCertPath,
+		macaroonPath:    macaroonPath,
+		timeout:         timeout,
+	}
+
+	return remoteSigner, cleanUp, nil
+}
+
+// Run feeds lnd with the incoming stream set up by an outbound remote signer
+// and then blocks until the stream is closed. Lnd can then send any requests to
+// the remote signer through the stream.
+//
+// NOTE: This is part of the RemoteSigner interface.
+func (*InboundRemoteSigner) Run(_ StreamServer) error {
+	// If lnd has been configured to use an inbound remote signer, it should
+	// not allow an outbound remote signer to connect.
+	return errors.New("not supported when remotesigner.signertype is set " +
+		"to \"inbound\"")
+}
+
+// Ready blocks and returns nil when the remote signer is ready to accept
+// requests.
+//
+// NOTE: This is part of the RemoteSigner interface.
+func (r *InboundRemoteSigner) Ready() error {
+	// The inbound remote signer is ready as soon we have connected to the
+	// remote signer node in the constructor. Therefore, we always return
+	// nil here to signal that we are ready.
+	return nil
+}
+
+// Ping verifies that the remote signer is still responsive.
+//
+// NOTE: This is part of the RemoteSigner interface.
+func (r *InboundRemoteSigner) Ping(timeout time.Duration) error {
+	conn, err := connect(
+		r.rpcHost, r.tlsCertPath, r.macaroonPath, timeout,
+	)
+	if err != nil {
+		return fmt.Errorf("error connecting to the remote "+
+			"signing node through RPC: %v", err)
+	}
+
+	return conn.Close()
+}
+
+// Timeout returns the set connection timeout for the remote signer.
+//
+// NOTE: This is part of the RemoteSigner interface.
+func (r *InboundRemoteSigner) Timeout() time.Duration {
+	return r.timeout
+}
+
+// A compile time assertion to ensure InboundRemoteSigner meets the
+// RemoteSigner interface.
+var _ RemoteSigner = (*InboundRemoteSigner)(nil)
+
+// connect tries to establish an RPC connection to the given host:port with the
+// supplied certificate and macaroon.
+func connect(hostPort, tlsCertPath, macaroonPath string,
+	timeout time.Duration) (*grpc.ClientConn, error) {
+
+	certBytes, err := os.ReadFile(tlsCertPath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading TLS cert file %v: %w",
+			tlsCertPath, err)
+	}
+
+	cp := x509.NewCertPool()
+	if !cp.AppendCertsFromPEM(certBytes) {
+		return nil, fmt.Errorf("credentials: failed to append " +
+			"certificate")
+	}
+
+	macBytes, err := os.ReadFile(macaroonPath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading macaroon file %v: %w",
+			macaroonPath, err)
+	}
+	mac := &macaroon.Macaroon{}
+	if err := mac.UnmarshalBinary(macBytes); err != nil {
+		return nil, fmt.Errorf("error decoding macaroon: %w", err)
+	}
+
+	macCred, err := macaroons.NewMacaroonCredential(mac)
+	if err != nil {
+		return nil, fmt.Errorf("error creating creds: %w", err)
+	}
+
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(credentials.NewClientTLSFromCert(
+			cp, "",
+		)),
+		grpc.WithPerRPCCredentials(macCred),
+		grpc.WithBlock(),
+	}
+
+	ctxt, cancel := context.WithTimeout(context.Background(), timeout)
+	// In the blocking case, ctx can be used to cancel or expire the pending
+	// connection. Once this function returns, the cancellation and
+	// expiration of ctx will be noop. Users should call ClientConn.Close to
+	// terminate all the pending operations after this function returns.
+	defer cancel()
+
+	conn, err := grpc.DialContext(ctxt, hostPort, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("unable to connect to RPC server: %w",
+			err)
+	}
+
+	return conn, nil
 }

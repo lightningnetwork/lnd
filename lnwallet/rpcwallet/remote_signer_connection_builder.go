b5473010617dfa740c4c0c9753ec2f9987948316
Viktor Torstensson <viktor.t.git@gmail.com>
2024-05-14 16:15:00 +0200

    rpcwallet: add `RemoteSignerConnectionBuilder`
    `RemoteSignerConnectionBuilder` is a helper that creates instances of the
    RemoteSigner connection interface based on the `lncfg.RemoteSigner`
    config.

---
+++ lnwallet/rpcwallet/remote_signer_connection_builder.go
@@ -0,0 +1,41 @@
+package rpcwallet
+
+import (
+	"context"
+	"errors"
+
+	"github.com/lightningnetwork/lnd/lncfg"
+)
+
+// RemoteSignerConnectionBuilder creates instances of the RemoteSignerConnection
+// interface, based on the provided configuration.
+type RemoteSignerConnectionBuilder struct {
+	cfg *lncfg.RemoteSigner
+}
+
+// NewRemoteSignerConnectionBuilder creates a new instance of the
+// RemoteSignerBuilder.
+func NewRemoteSignerConnectionBuilder(
+	cfg *lncfg.RemoteSigner) *RemoteSignerConnectionBuilder {
+
+	return &RemoteSignerConnectionBuilder{cfg}
+}
+
+// Build creates a new RemoteSignerConnection instance. If the configuration
+// specifies that an inbound remote signer should be used, a new
+// OutboundConnection is created. If the configuration specifies that an
+// outbound remote signer should be used, a new InboundConnection is created.
+// The function returns the created RemoteSignerConnection instance, and a
+// cleanup function that should be called when the RemoteSignerConnection is no
+// longer needed.
+func (b *RemoteSignerConnectionBuilder) Build(
+	ctx context.Context) (RemoteSignerConnection, error) {
+
+	if !b.cfg.Enable {
+		// This should be unreachable, but this is an extra sanity check
+		return nil, errors.New("remote signer not enabled in " +
+			"config")
+	}
+
+	return NewOutboundConnection(ctx, b.cfg.ConnectionCfg)
+}

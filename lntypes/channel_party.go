f9ed02afcb5540714b019011b0385425c1d8ee87
Keagan McClelland <keagan.mcclelland@gmail.com>
2024-05-22 16:55:41 -0700

    lntypes: Add Dual[A] primitive type
    This commit introduces a new type Dual[A] to make it easier to
    manage symmetric configurations or state for lightning channels.

--- lntypes/channel_party.go
+++ lntypes/channel_party.go
@@ -1,5 +1,7 @@
 package lntypes
 
+import "fmt"
+
 // ChannelParty is a type used to have an unambiguous description of which node
 // is being referred to. This eliminates the need to describe as "local" or
 // "remote" using bool.
@@ -18,4 +20,41 @@ const (
 // Not inverts the role of the ChannelParty.
 func (p ChannelParty) CounterParty() ChannelParty {
 	return ChannelParty((byte(p) + 1) % 2)
+}
+
+// Dual represents a structure when we are tracking the same parameter for both
+// the Local and Remote parties.
+type Dual[A any] struct {
+	// Local is the value tracked for the Local ChannelParty
+	Local A
+
+	// Remote is the value tracked for the Remote ChannelParty
+	Remote A
+}
+
+// ForParty gives Dual an access method that takes a ChannelParty as an
+// argument. It is included for ergonomics in cases where the ChannelParty is
+// in a variable and which party determines how we want to access the Dual.
+func (d Dual[A]) ForParty(p ChannelParty) A {
+	switch p {
+	case Local:
+		return d.Local
+	case Remote:
+		return d.Remote
+	default:
+		panic(
+			fmt.Sprintf(
+				"switch default triggered in ForParty: %v", p,
+			),
+		)
+	}
+}
+
+// MapDual applies the function argument to both the Local and Remote fields of
+// the Dual[A] and returns a Dual[B] with that function applied.
+func MapDual[A, B any](f func(A) B, d Dual[A]) Dual[B] {
+	return Dual[B]{
+		Local:  f(d.Local),
+		Remote: f(d.Remote),
+	}
 }

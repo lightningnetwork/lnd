5044fb5bab8867a58a470724c30d18fa3fcd39d3
Keagan McClelland <keagan.mcclelland@gmail.com>
2024-05-24 15:07:32 -0700

    lnwallet: update updateLog.modifiedHtlcs to use fn.Set

--- lnwallet/channel.go
+++ lnwallet/channel.go
@@ -25,6 +25,7 @@ import (
 	"github.com/lightningnetwork/lnd/chainntnfs"
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/channeldb/models"
+	"github.com/lightningnetwork/lnd/fn"
 	"github.com/lightningnetwork/lnd/input"
 	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/lntypes"

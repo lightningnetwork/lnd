01ac713aecbe4d428bf76008e175ef152275e8ca
yyforyongyu <yong2452@gmail.com>
2024-06-27 08:41:53 +0800

    chainio: implement `Blockbeat`
    In this commit, a minimal implementation of `Blockbeat` is added to
    synchronize block heights, which will be used in `ChainArb`, `Sweeper`,
    and `TxPublisher` so blocks are processed sequentially among them.

--- chainio/interface.go
+++ chainio/interface.go
@@ -1,9 +1,11 @@
 package chainio
 
+import "github.com/btcsuite/btclog/v2"
+
 // Blockbeat defines an interface that can be used by subsystems to retrieve
-// block data. It is sent by the BlockbeatDispatcher whenever a new block is
-// received. Once the subsystem finishes processing the block, it must signal
-// it by calling NotifyBlockProcessed.
+// block data. It is sent by the BlockbeatDispatcher to all the registered
+// consumers whenever a new block is received. Once the consumer finishes
+// processing the block, it must signal it by calling `NotifyBlockProcessed`.
 //
 // The blockchain is a state machine - whenever there's a state change, it's
 // manifested in a block. The blockbeat is a way to notify subsystems of this
@@ -11,8 +13,19 @@ package chainio
 // other words, subsystems must react to this state change and should consider
 // being driven by the blockbeat in their own state machines.
 type Blockbeat interface {
+	// blockbeat is a private interface that's only used in this package.
+	blockbeat
+
 	// Height returns the current block height.
 	Height() int32
+}
+
+// blockbeat defines a set of private methods used in this package to make
+// interaction with the blockbeat easier.
+type blockbeat interface {
+	// logger returns the internal logger used by the blockbeat which has a
+	// block height prefix.
+	logger() btclog.Logger
 }
 
 // Consumer defines a blockbeat consumer interface. Subsystems that need block

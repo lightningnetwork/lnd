b1cf61a9477f2011d048da2f3d6b344212ad393a
rkfg <rkfg@rkfg.me>
2024-06-27 15:53:18 +0300

    Update fee after channel becomes active

--- htlcswitch/link.go
+++ htlcswitch/link.go
@@ -533,7 +533,9 @@ func (l *channelLink) Start() error {
 		}()
 	}
 
-	l.updateFeeTimer = time.NewTimer(l.randomFeeUpdateTimeout())
+	// this timer will fire immediately after the channel is ready
+	// and it will reset to a random interval after that
+	l.updateFeeTimer = time.NewTimer(0)
 
 	l.wg.Add(1)
 	go l.htlcManager()

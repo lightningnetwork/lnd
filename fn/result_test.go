8971c4c3aec03357fd9e2bf608e6c4da63635037
Keagan McClelland <keagan.mcclelland@gmail.com>
2024-08-13 16:36:07 -0700

    fn: add operations to Result API

--- fn/result_test.go
+++ fn/result_test.go
@@ -2,7 +2,9 @@ package fn
 
 import (
 	"errors"
+	"fmt"
 	"testing"
+	"testing/quick"
 
 	"github.com/stretchr/testify/require"
 )
@@ -16,4 +18,30 @@ func TestOkToSome(t *testing.T) {
 	require.Equal(
 		t, Err[uint8](errors.New("err")).OkToSome(), None[uint8](),
 	)
+}
+
+func TestMapOk(t *testing.T) {
+	inc := func(i int) int {
+		return i + 1
+	}
+	f := func(i int) bool {
+		ok := Ok(i)
+		return MapOk(inc)(ok) == Ok(inc(i))
+	}
+
+	require.NoError(t, quick.Check(f, nil))
+}
+
+func TestFlattenResult(t *testing.T) {
+	f := func(i int) bool {
+		e := fmt.Errorf("error")
+
+		x := FlattenResult(Ok(Ok(i))) == Ok(i)
+		y := FlattenResult(Ok(Err[int](e))) == Err[int](e)
+		z := FlattenResult(Err[Result[int]](e)) == Err[int](e)
+
+		return x && y && z
+	}
+
+	require.NoError(t, quick.Check(f, nil))
 }

18445db7fa4696201e2e4cf2fa44ccd34bf83117
Elle Mouton <elle.mouton@gmail.com>
2024-08-21 12:58:01 +0200

    channeldb: add encoding for ChannelEdgePolicy2
    Similarly to the previous commit, here we add the encoding for the new
    ChannelEdgePolicy2. This is done in the same was as for
    ChannelEdgeInfo2:
    - a 0xff prefix
    - followed by a type-byte
    - followed by the TLV encoding of the ChannelEdgePolicy2.

--- channeldb/graph.go
+++ channeldb/graph.go
@@ -311,7 +311,13 @@ func (c *ChannelGraph) getChannelMap(edges kvdb.RBucket) (
 			return err
 		}
 
-		channelMap[key] = edge
+		e, ok := edge.(*models.ChannelEdgePolicy1)
+		if !ok {
+			return fmt.Errorf("expected "+
+				"*models.ChannelEdgePolicy1, got: %T", edge)
+		}
+
+		channelMap[key] = e
 
 		return nil
 	})
@@ -2387,7 +2393,14 @@ func (c *ChannelGraph) FilterChannelRange(startHeight,
 					return err
 				}
 
-				chanInfo.Node1UpdateTimestamp = edge.LastUpdate
+				e, ok := edge.(*models.ChannelEdgePolicy1)
+				if !ok {
+					return fmt.Errorf("expected "+
+						"*models.ChannelEdgePolicy1, "+
+						"got %T", edge)
+				}
+
+				chanInfo.Node1UpdateTimestamp = e.LastUpdate
 			}
 
 			rawPolicy = edges.Get(node2Key)
@@ -2402,7 +2415,14 @@ func (c *ChannelGraph) FilterChannelRange(startHeight,
 					return err
 				}
 
-				chanInfo.Node2UpdateTimestamp = edge.LastUpdate
+				e, ok := edge.(*models.ChannelEdgePolicy1)
+				if !ok {
+					return fmt.Errorf("expected "+
+						"*models.ChannelEdgePolicy1, "+
+						"got %T", edge)
+				}
+
+				chanInfo.Node2UpdateTimestamp = e.LastUpdate
 			}
 
 			channelsPerBlock[cid.BlockHeight] = append(

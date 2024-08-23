9e535694a36884614e9e202ce21afeec46ee7387
Viktor Torstensson <viktor.t.git@gmail.com>
2024-08-23 15:37:22 +0200

    lnd: add new `remotesigner` macaroon entity
    This commit introduces a new macaroon entity that grants the caller
    access to connect a remote signer to LND, provided the LND instance is
    configured to allow an inbound connection from an outbound remote
    signer.

--- rpcserver.go
+++ rpcserver.go
@@ -184,6 +184,10 @@ var (
 			Entity: "macaroon",
 			Action: "write",
 		},
+		{
+			Entity: "remotesigner",
+			Action: "generate",
+		},
 	}
 
 	// invoicePermissions is a slice of all the entities that allows a user
@@ -218,7 +222,7 @@ var (
 	// implemented.
 	validActions  = []string{"read", "write", "generate"}
 	validEntities = []string{
-		"onchain", "offchain", "address", "message",
+		"onchain", "offchain", "address", "message", "remotesigner",
 		"peers", "info", "invoices", "signer", "macaroon",
 		macaroons.PermissionEntityCustomURI,
 	}

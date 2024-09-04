6678f0845a8ba27ecad32d11585df99eaca7b4c5
Keagan McClelland <keagan.mcclelland@gmail.com>
2024-09-04 16:16:53 -0700

    lnwallet: use CommitChainEpochHistory to determine CsvDelay during Breach
    This commit changes the way we create breach retributions to use the
    CsvDelay we compute from the CommitChainEpochHistory so as to account
    for the possibility that the channel parameters have changed since
    opening.

--- lnwallet/channel_test.go
+++ lnwallet/channel_test.go
@@ -9807,7 +9807,7 @@ func TestCreateHtlcRetribution(t *testing.T) {
 
 	// Create the htlc retribution.
 	hr, err := createHtlcRetribution(
-		aliceChannel.channelState, keyRing, commitHash,
+		aliceChannel.channelState, 0,keyRing, commitHash,
 		dummyPrivate, leaseExpiry, htlc, fn.None[CommitAuxLeaves](),
 	)
 	// Expect no error.
@@ -10012,7 +10012,7 @@ func TestCreateBreachRetribution(t *testing.T) {
 
 			br, our, their, err := createBreachRetribution(
 				tc.revocationLog, tx,
-				aliceChannel.channelState, keyRing,
+				aliceChannel.channelState, 0, keyRing,
 				dummyPrivate, leaseExpiry,
 				fn.None[CommitAuxLeaves](),
 			)
@@ -10071,7 +10071,7 @@ func TestCreateBreachRetributionLegacy(t *testing.T) {
 
 	// Create the breach retribution using the legacy format.
 	br, ourAmt, theirAmt, err := createBreachRetributionLegacy(
-		&revokedLog, aliceChannel.channelState, keyRing,
+		&revokedLog, aliceChannel.channelState, 0, keyRing,
 		dummyPrivate, ourScript, theirScript, leaseExpiry,
 	)
 	require.NoError(t, err)

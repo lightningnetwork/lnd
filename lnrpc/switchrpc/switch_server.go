76b0b47ea80c6d7c28f8e4ac14e53f6ddb6749a4
Calvin Zachman <calvin.zachman@protonmail.com>
2024-09-13 13:06:58 -0500

    switchrpc: subserver functionality

---
+++ lnrpc/switchrpc/switch_server.go
@@ -0,0 +1,188 @@
+//go:build switchrpc
+// +build switchrpc
+
+package switchrpc
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/lightningnetwork/lnd/lnrpc"
+	"github.com/lightningnetwork/lnd/macaroons"
+	grpc "google.golang.org/grpc"
+	"gopkg.in/macaroon-bakery.v2/bakery"
+)
+
+const (
+	// subServerName is the name of the sub rpc server. We'll use this name
+	// to register ourselves, and we also require that the main
+	// SubServerConfigDispatcher instance recognize it as the name of our
+	// RPC service.
+	subServerName = "SwitchRPC"
+)
+
+var (
+	// macaroonOps are the set of capabilities that our minted macaroon (if
+	// it doesn't already exist) will have.
+	macaroonOps = []bakery.Op{
+		{
+			Entity: "offchain",
+			Action: "read",
+		},
+		{
+			Entity: "offchain",
+			Action: "write",
+		},
+	}
+
+	// macPermissions maps RPC calls to the permissions they require.
+	macPermissions = map[string][]bakery.Op{}
+
+	// DefaultSwitchMacFilename is the default name of the switch macaroon
+	// that we expect to find via a file handle within the main
+	// configuration file in this package.
+	DefaultSwitchMacFilename = "switch.macaroon"
+)
+
+// ServerShell is a shell struct holding a reference to the actual sub-server.
+// It is used to register the gRPC sub-server with the root server before we
+// have the necessary dependencies to populate the actual sub-server.
+type ServerShell struct {
+	SwitchServer
+}
+
+type Server struct {
+	cfg *Config
+
+	// Required by the grpc-gateway/v2 library for forward compatibility.
+	// Must be after the atomically used variables to not break struct
+	// alignment.
+	UnimplementedSwitchServer
+}
+
+// New creates a new instance of the SwitchServer given a configuration struct
+// that contains all external dependencies. If the target macaroon exists, and
+// we're unable to create it, then an error will be returned. We also return
+// the set of permissions that we require as a server. At the time of writing
+// of this documentation, this is the same macaroon as the admin macaroon.
+func New(cfg *Config) (*Server, lnrpc.MacaroonPerms, error) {
+	// If the path of the router macaroon wasn't generated, then we'll
+	// assume that it's found at the default network directory.
+	if cfg.SwitchMacPath == "" {
+		cfg.SwitchMacPath = filepath.Join(
+			cfg.NetworkDir, DefaultSwitchMacFilename,
+		)
+	}
+
+	// Now that we know the full path of the switch macaroon, we can check
+	// to see if we need to create it or not. If stateless_init is set
+	// then we don't write the macaroons.
+	macFilePath := cfg.SwitchMacPath
+	if cfg.MacService != nil && !cfg.MacService.StatelessInit &&
+		!lnrpc.FileExists(macFilePath) {
+
+		log.Infof("Making macaroons for Switch RPC Server at: %v",
+			macFilePath)
+
+		// At this point, we know that the switch macaroon doesn't yet,
+		// exist, so we need to create it with the help of the main
+		// macaroon service.
+		switchMac, err := cfg.MacService.NewMacaroon(
+			context.Background(), macaroons.DefaultRootKeyID,
+			macaroonOps...,
+		)
+		if err != nil {
+			return nil, nil, err
+		}
+		switchMacBytes, err := switchMac.M().MarshalBinary()
+		if err != nil {
+			return nil, nil, err
+		}
+		err = os.WriteFile(macFilePath, switchMacBytes, 0644)
+		if err != nil {
+			_ = os.Remove(macFilePath)
+			return nil, nil, err
+		}
+	}
+
+	switchServer := &Server{
+		cfg: cfg,
+		// quit: make(chan struct{}),
+	}
+
+	return switchServer, macPermissions, nil
+}
+
+// Start launches any helper goroutines required for the Server to function.
+//
+// NOTE: This is part of the lnrpc.SubServer interface.
+func (s *Server) Start() error {
+	return nil
+}
+
+// Stop signals any active goroutines for a graceful closure.
+//
+// NOTE: This is part of the lnrpc.SubServer interface.
+func (s *Server) Stop() error {
+	return nil
+}
+
+// Name returns a unique string representation of the sub-server. This can be
+// used to identify the sub-server and also de-duplicate them.
+//
+// NOTE: This is part of the lnrpc.SubServer interface.
+func (s *Server) Name() string {
+	return subServerName
+}
+
+// RegisterWithRootServer will be called by the root gRPC server to direct a
+// sub RPC server to register itself with the main gRPC root server. Until this
+// is called, each sub-server won't be able to have
+// requests routed towards it.
+//
+// NOTE: This is part of the lnrpc.GrpcHandler interface.
+func (r *ServerShell) RegisterWithRootServer(grpcServer *grpc.Server) error {
+	// We make sure that we register it with the main gRPC server to ensure
+	// all our methods are routed properly.
+	RegisterSwitchServer(grpcServer, r)
+
+	log.Debugf("Switch RPC server successfully register with root " +
+		"gRPC server")
+
+	return nil
+}
+
+// RegisterWithRestServer will be called by the root REST mux to direct a sub
+// RPC server to register itself with the main REST mux server. Until this is
+// called, each sub-server won't be able to have requests routed towards it.
+//
+// NOTE: This is part of the lnrpc.GrpcHandler interface.
+func (r *ServerShell) RegisterWithRestServer(ctx context.Context,
+	mux *runtime.ServeMux, dest string, opts []grpc.DialOption) error {
+
+	log.Debugf("Switch REST server successfully registered with " +
+		"root REST server")
+
+	return nil
+}
+
+// CreateSubServer populates the subserver's dependencies using the passed
+// SubServerConfigDispatcher. This method should fully initialize the
+// sub-server instance, making it ready for action. It returns the macaroon
+// permissions that the sub-server wishes to pass on to the root server for all
+// methods routed towards it.
+//
+// NOTE: This is part of the lnrpc.GrpcHandler interface.
+func (r *ServerShell) CreateSubServer(configRegistry lnrpc.SubServerConfigDispatcher) (
+	lnrpc.SubServer, lnrpc.MacaroonPerms, error) {
+
+	subServer, macPermissions, err := createNewSubServer(configRegistry)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	r.SwitchServer = subServer
+	return subServer, macPermissions, nil
+}

a7760e5344a7c92a92fbcf3faad46dd918e5a31f
Calvin Zachman <calvin.zachman@protonmail.com>
2024-09-16 09:04:58 -0500

    multi: make result tracking mode configurable
    Allow node configuration to specify whether to treat
    HTLC attempts in the normal way or as being tracked
    by a remote entity. This has the practical effect of
    disabling the automatic cleanup of network result state
    within the Switch. Instead, state deletion must be completed
    via RPC.
    
    This is an all or nothing designation until we can determine
    whether it can be set on a per-HTLC attempt basis.

--- htlcswitch/switch.go
+++ htlcswitch/switch.go
@@ -213,6 +213,12 @@ type Config struct {
 	// a mailbox via AddPacket.
 	MailboxDeliveryTimeout time.Duration
 
+	// RemoteTracking determines whether HTLC attempts should be considered
+	// as tracked remotely. If true, no automated cleanup of the attempt
+	// results will occur and the switch will treat all attempts as if they
+	// are managed by a remote controller.
+	RemoteTracking bool
+
 	// MaxFeeExposure is the threshold in milli-satoshis after which we'll
 	// fail incoming or outgoing payments for a particular channel.
 	MaxFeeExposure lnwire.MilliSatoshi
@@ -534,6 +540,11 @@ func (s *Switch) GetAttemptResult(attemptID uint64, paymentHash lntypes.Hash,
 // preiodically to let the switch clean up payment results that we have
 // handled.
 func (s *Switch) CleanStore(keepPids map[uint64]struct{}) error {
+	if s.cfg.RemoteTracking {
+		log.Infof("Switch store automatic cleaning disabled.")
+		return nil
+	}
+
 	return s.networkResults.cleanStore(keepPids)
 }
 

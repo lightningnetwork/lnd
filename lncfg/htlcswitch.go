3cfb141c82eb6ab591f5574ce00953c2584b00d4
Calvin Zachman <calvin.zachman@protonmail.com>
2024-09-16 09:04:58 -0500

    multi: make result tracking mode configurable
    Allow node configuration to specify whether to treat
    HTLC attempts in the normal way or as being tracked
    by a remote entity. This has the practical effect of
    disabling the automatic cleanup of network result state
    within the Switch. Instead, state deletion must be completed
    via RPC.
    
    This is an all or nothing designation until we can determine
    whether it can be set on a per-HTLC attempt basis.

--- lncfg/htlcswitch.go
+++ lncfg/htlcswitch.go
@@ -16,6 +16,12 @@ var (
 //nolint:ll
 type Htlcswitch struct {
 	MailboxDeliveryTimeout time.Duration `long:"mailboxdeliverytimeout" description:"The timeout value when delivering HTLCs to a channel link. Setting this value too small will result in local payment failures if large number of payments are sent over a short period."`
+
+	// RemoteTracking determines whether HTLC attempts should be considered
+	// as tracked remotely. If true, no automated cleanup of the attempt
+	// results will occur and the switch will treat all attempts as if they
+	// are managed by a remote controller.
+	RemoteTracking bool `long:"remotetracking" description:"If true, all HTLC attempts will be marked as remotely tracked, allowing for external control over when the attempts are marked for deletion."`
 }
 
 // Validate checks the values configured for htlcswitch.

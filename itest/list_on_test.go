b32fe456910ce7ebe2e7dfc4b48157fe215bd6ea
Andras Banki-Horvath <bhandras@gmail.com>
2024-09-17 15:20:25 +0200

    itest: add integration test for invoice migration

--- itest/list_on_test.go
+++ itest/list_on_test.go
@@ -714,4 +714,8 @@ var allTestCases = []*lntest.TestCase{
 		Name:     "quiescence",
 		TestFunc: testQuiescence,
 	},
+	{
+		Name:     "invoice migration",
+		TestFunc: testInvoiceMigration,
+	},
 }

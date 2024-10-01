4997e95c5cd7fc2c4d98c2f3cc0aa16765c8b693
Calvin Zachman <calvin.zachman@protonmail.com>
2024-10-01 08:35:01 -0500

    switchrpc: add new SendOnion rpc proto
    Support all fields from UpdateAddHtlc. This allows
    users of the SendOnion RPC to include all fields that
    we support using with the UpdateAddHtlc type.

--- lnrpc/switchrpc/switch_grpc.pb.go
+++ lnrpc/switchrpc/switch_grpc.pb.go
@@ -3,7 +3,10 @@
 package switchrpc
 
 import (
+	context "context"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -15,6 +18,10 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type SwitchClient interface {
+	// SendOnion attempts to make a payment via the specified onion. This
+	// method differs from SendPayment in that the instance need not be aware of
+	// the full details of the payment route.
+	SendOnion(ctx context.Context, in *SendOnionRequest, opts ...grpc.CallOption) (*SendOnionResponse, error)
 }
 
 type switchClient struct {
@@ -25,10 +32,23 @@ func NewSwitchClient(cc grpc.ClientConnInterface) SwitchClient {
 	return &switchClient{cc}
 }
 
+func (c *switchClient) SendOnion(ctx context.Context, in *SendOnionRequest, opts ...grpc.CallOption) (*SendOnionResponse, error) {
+	out := new(SendOnionResponse)
+	err := c.cc.Invoke(ctx, "/switchrpc.Switch/SendOnion", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // SwitchServer is the server API for Switch service.
 // All implementations must embed UnimplementedSwitchServer
 // for forward compatibility
 type SwitchServer interface {
+	// SendOnion attempts to make a payment via the specified onion. This
+	// method differs from SendPayment in that the instance need not be aware of
+	// the full details of the payment route.
+	SendOnion(context.Context, *SendOnionRequest) (*SendOnionResponse, error)
 	mustEmbedUnimplementedSwitchServer()
 }
 
@@ -36,6 +56,9 @@ type SwitchServer interface {
 type UnimplementedSwitchServer struct {
 }
 
+func (UnimplementedSwitchServer) SendOnion(context.Context, *SendOnionRequest) (*SendOnionResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SendOnion not implemented")
+}
 func (UnimplementedSwitchServer) mustEmbedUnimplementedSwitchServer() {}
 
 // UnsafeSwitchServer may be embedded to opt out of forward compatibility for this service.
@@ -49,13 +72,36 @@ func RegisterSwitchServer(s grpc.ServiceRegistrar, srv SwitchServer) {
 	s.RegisterService(&Switch_ServiceDesc, srv)
 }
 
+func _Switch_SendOnion_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SendOnionRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SwitchServer).SendOnion(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/switchrpc.Switch/SendOnion",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SwitchServer).SendOnion(ctx, req.(*SendOnionRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Switch_ServiceDesc is the grpc.ServiceDesc for Switch service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
 var Switch_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "switchrpc.Switch",
 	HandlerType: (*SwitchServer)(nil),
-	Methods:     []grpc.MethodDesc{},
-	Streams:     []grpc.StreamDesc{},
-	Metadata:    "switchrpc/switch.proto",
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "SendOnion",
+			Handler:    _Switch_SendOnion_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "switchrpc/switch.proto",
 }

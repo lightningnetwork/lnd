83751ee20853b48108741e0b8f1ade468be10be1
Elle Mouton <elle.mouton@gmail.com>
2024-10-23 10:15:06 +0200

    sqldb: update btclog dep

--- sqldb/log.go
+++ sqldb/log.go
@@ -1,8 +1,6 @@
 package sqldb
 
-import (
-	"github.com/btcsuite/btclog"
-)
+import "github.com/btcsuite/btclog/v2"
 
 // Subsystem defines the logging code for this subsystem.
 const Subsystem = "SQLD"

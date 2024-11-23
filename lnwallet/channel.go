d1b2bff2c8cfd0939775fbd6af324dab4a320da9
Olaoluwa Osuntokun <laolu32@gmail.com>
2024-11-22 19:05:18 -0800

    lnwallet: update CoopCloseBalance to allow a paying party
    This preps us for an upcoming change to the rbf coop state machine where
    either party can pay for the channel fees. We also add a new test to
    make sure the new function adheres to some key properties.

--- lnwallet/channel.go
+++ lnwallet/channel.go
@@ -8289,6 +8289,7 @@ func (lc *LightningChannel) CreateCloseProposal(proposedFee btcutil.Amount,
 		lc.channelState.LocalCommitment.LocalBalance.ToSatoshis(),
 		lc.channelState.LocalCommitment.RemoteBalance.ToSatoshis(),
 		lc.channelState.LocalCommitment.CommitFee,
+		fn.None[lntypes.ChannelParty](),
 	)
 	if err != nil {
 		return nil, nil, 0, err
@@ -8402,6 +8403,7 @@ func (lc *LightningChannel) CompleteCooperativeClose(
 		lc.channelState.LocalCommitment.LocalBalance.ToSatoshis(),
 		lc.channelState.LocalCommitment.RemoteBalance.ToSatoshis(),
 		lc.channelState.LocalCommitment.CommitFee,
+		fn.None[lntypes.ChannelParty](),
 	)
 	if err != nil {
 		return nil, 0, err

4b346bae81dcbeaea7adb969d5ad70e34aa91a68
MPins <mpinsdorf@gmail.com>
2024-12-05 11:24:34 -0300

    multi: use all valid routes during blinded path construction
    MaxNumPaths restriction moved from FindBlindedPaths to BuildBlindedPaymentPaths
    this way we can interact through all possibly routes when creating a new
    blinded path. Modify the TestFindBlindedPathsWithMC to not use MaxNumPaths and
    modify TestBuildBlindedPath to use MaxNumPaths.

--- routing/router_test.go
+++ routing/router_test.go
@@ -3153,12 +3153,11 @@ func TestFindBlindedPathsWithMC(t *testing.T) {
 	}
 
 	// All the probabilities are set to 1. So if we restrict the path length
-	// to 2 and allow a max of 3 routes, then we expect three paths here.
+	// to 2, then we expect three paths here.
 	routes, err := ctx.router.FindBlindedPaths(
 		dave, 1000, probabilitySrc, &BlindedPathRestrictions{
 			MinDistanceFromIntroNode: 2,
 			NumHops:                  2,
-			MaxNumPaths:              3,
 		},
 	)
 	require.NoError(t, err)
@@ -3192,8 +3191,7 @@ func TestFindBlindedPathsWithMC(t *testing.T) {
 	}
 
 	// Now, let's lower the MC probability of the B-D to 0.5 and F-D link to
-	// 0.25. We will leave the MaxNumPaths as 3 and so all paths should
-	// still be returned but the order should be:
+	// 0.25:
 	// 1) A -> C -> D
 	// 2) A -> B -> D
 	// 3) A -> F -> D
@@ -3203,7 +3201,6 @@ func TestFindBlindedPathsWithMC(t *testing.T) {
 		dave, 1000, probabilitySrc, &BlindedPathRestrictions{
 			MinDistanceFromIntroNode: 2,
 			NumHops:                  2,
-			MaxNumPaths:              3,
 		},
 	)
 	require.NoError(t, err)
@@ -3220,7 +3217,6 @@ func TestFindBlindedPathsWithMC(t *testing.T) {
 		dave, 1000, probabilitySrc, &BlindedPathRestrictions{
 			MinDistanceFromIntroNode: 2,
 			NumHops:                  2,
-			MaxNumPaths:              3,
 		},
 	)
 	require.NoError(t, err)
@@ -3230,27 +3226,12 @@ func TestFindBlindedPathsWithMC(t *testing.T) {
 		"alice,charlie,dave",
 	})
 
-	// Change the MaxNumPaths to 1 to assert that only the best route is
-	// returned.
-	routes, err = ctx.router.FindBlindedPaths(
-		dave, 1000, probabilitySrc, &BlindedPathRestrictions{
-			MinDistanceFromIntroNode: 2,
-			NumHops:                  2,
-			MaxNumPaths:              1,
-		},
-	)
-	require.NoError(t, err)
-	assertPaths(routes, []string{
-		"alice,bob,dave",
-	})
-
 	// Test the edge case where Dave, the recipient, is also the
 	// introduction node.
 	routes, err = ctx.router.FindBlindedPaths(
 		dave, 1000, probabilitySrc, &BlindedPathRestrictions{
 			MinDistanceFromIntroNode: 0,
 			NumHops:                  0,
-			MaxNumPaths:              1,
 		},
 	)
 	require.NoError(t, err)
@@ -3265,7 +3246,6 @@ func TestFindBlindedPathsWithMC(t *testing.T) {
 		dave, 1000, probabilitySrc, &BlindedPathRestrictions{
 			MinDistanceFromIntroNode: 2,
 			NumHops:                  2,
-			MaxNumPaths:              3,
 		},
 	)
 	require.NoError(t, err)
@@ -3280,7 +3260,6 @@ func TestFindBlindedPathsWithMC(t *testing.T) {
 		dave, 1000, probabilitySrc, &BlindedPathRestrictions{
 			MinDistanceFromIntroNode: 2,
 			NumHops:                  2,
-			MaxNumPaths:              3,
 			NodeOmissionSet:          fn.NewSet(frank),
 		},
 	)

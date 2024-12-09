75f500e0ae5bb13e3d0717fdcfeab5a807583a8d
Viktor Tigerström <vtigerstrom@gmail.com>
2024-12-09 21:38:39 +0100

    lnd+lncfg: Refactor `RemoteSigner` Config
    Upcoming commits will introduce and enable a new alternative remote
    signer implementation. This new implementation establishes the
    connection between the remote signer and the watch-only node in the
    reverse direction compared to the existing implementation. In this
    setup, the remote signer initiates an outbound connection to the
    watch-only node, whereas the previous version allowed an inbound
    connection from the watch-only node. As a result, this type is referred
    to as an "outbound remote signer."
    
    This commit refactors the `RemoteSigner` configuration struct in
    preparation for the new implementation. The new implementation will
    require configuration changes on both the watch-only node side and the
    remote signer side. This refactor simplifies the process of integrating
    that functionality.

--- lncfg/remotesigner.go
+++ lncfg/remotesigner.go
@@ -11,34 +11,91 @@ const (
 	DefaultRemoteSignerRPCTimeout = 5 * time.Second
 )
 
-// RemoteSigner holds the configuration options for a remote RPC signer.
+// RemoteSigner holds the configuration options for how to connect to a remote
+// signer. Only a watch-only node specifies this config.
 //
 //nolint:ll
 type RemoteSigner struct {
-	Enable           bool          `long:"enable" description:"Use a remote signer for signing any on-chain related transactions or messages. Only recommended if local wallet is initialized as watch-only. Remote signer must use the same seed/root key as the local watch-only wallet but must have private keys."`
-	RPCHost          string        `long:"rpchost" description:"The remote signer's RPC host:port"`
-	MacaroonPath     string        `long:"macaroonpath" description:"The macaroon to use for authenticating with the remote signer"`
-	TLSCertPath      string        `long:"tlscertpath" description:"The TLS certificate to use for establishing the remote signer's identity"`
-	Timeout          time.Duration `long:"timeout" description:"The timeout for connecting to and signing requests with the remote signer. Valid time units are {s, m, h}."`
-	MigrateWatchOnly bool          `long:"migrate-wallet-to-watch-only" description:"If a wallet with private key material already exists, migrate it into a watch-only wallet on first startup. WARNING: This cannot be undone! Make sure you have backed up your seed before you use this flag! All private keys will be purged from the wallet after first unlock with this flag!"`
+	// Enable signals if this node is a watch-only node in a remote signer
+	// setup.
+	Enable bool `long:"enable" description:"Use a remote signer for signing any on-chain related transactions or messages. Only recommended if local wallet is initialized as watch-only. Remote signer must use the same seed/root key as the local watch-only wallet but must have private keys."`
+
+	// MigrateWatchOnly migrates the wallet to a watch-only wallet by
+	// purging all private keys from the wallet after first unlock with this
+	// flag.
+	MigrateWatchOnly bool `long:"migrate-wallet-to-watch-only" description:"If a wallet with private key material already exists, migrate it into a watch-only wallet on first startup. WARNING: This cannot be undone! Make sure you have backed up your seed before you use this flag! All private keys will be purged from the wallet after first unlock with this flag!"`
+
+	// ConnectionCfg holds the connection configuration options that the
+	// watch-only node will use when setting up the connection to the remote
+	// signer.
+	ConnectionCfg
+}
+
+// DefaultRemoteSignerCfg returns the default RemoteSigner config.
+func DefaultRemoteSignerCfg() *RemoteSigner {
+	return &RemoteSigner{
+		Enable:        false,
+		ConnectionCfg: defaultConnectionCfg(),
+	}
 }
 
 // Validate checks the values configured for our remote RPC signer.
 func (r *RemoteSigner) Validate() error {
-	if !r.Enable {
-		return nil
-	}
-
-	if r.Timeout < time.Millisecond {
-		return fmt.Errorf("remote signer: timeout of %v is invalid, "+
-			"cannot be smaller than %v", r.Timeout,
-			time.Millisecond)
-	}
-
 	if r.MigrateWatchOnly && !r.Enable {
 		return fmt.Errorf("remote signer: cannot turn on wallet " +
 			"migration to watch-only if remote signing is not " +
 			"enabled")
+	}
+
+	if !r.Enable {
+		return nil
+	}
+
+	// Else, we are in outbound mode, so we verify the connection config.
+	err := r.ConnectionCfg.Validate()
+	if err != nil {
+		return fmt.Errorf("remotesigner.%w", err)
+	}
+
+	return nil
+}
+
+// ConnectionCfg holds the configuration options required when setting up a
+// connection to either a remote signer or watch-only node, depending on which
+// side makes the outbound connection.
+//
+//nolint:ll
+type ConnectionCfg struct {
+	RPCHost      string        `long:"rpchost" description:"The RPC host:port of the remote signer. For watch-only nodes, this should be set to the remote signer's RPC host:port."`
+	MacaroonPath string        `long:"macaroonpath" description:"The macaroon to use for authenticating with the remote signer. For watch-only nodes, this should be set to the remote signer's macaroon."`
+	TLSCertPath  string        `long:"tlscertpath" description:"The TLS certificate to use for establishing the remote signer's identity. For watch-only nodes, this should be set to the remote signer's TLS certificate."`
+	Timeout      time.Duration `long:"timeout" description:"The timeout for making the connection to the remote signer. Valid time units are {s, m, h}."`
+}
+
+// defaultConnectionCfg returns the default ConnectionCfg config.
+func defaultConnectionCfg() ConnectionCfg {
+	return ConnectionCfg{
+		Timeout: DefaultRemoteSignerRPCTimeout,
+	}
+}
+
+// Validate checks the values set in the ConnectionCfg config are valid.
+func (c *ConnectionCfg) Validate() error {
+	if c.Timeout < time.Millisecond {
+		return fmt.Errorf("timeout of %v is invalid, cannot be "+
+			"smaller than %v", c.Timeout, time.Millisecond)
+	}
+
+	if c.RPCHost == "" {
+		return fmt.Errorf("rpchost must be set")
+	}
+
+	if c.MacaroonPath == "" {
+		return fmt.Errorf("macaroonpath must be set")
+	}
+
+	if c.TLSCertPath == "" {
+		return fmt.Errorf("tlscertpath must be set")
 	}
 
 	return nil

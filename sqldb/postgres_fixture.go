97c025f28930fa97b1c15007fa6651ee0bde3620
Andras Banki-Horvath <bhandras@gmail.com>
2025-01-23 08:48:32 +0100

    invoices: raise the number of allowed clients for the Postgres fixture

--- sqldb/postgres_fixture.go
+++ sqldb/postgres_fixture.go
@@ -59,7 +59,7 @@ func NewTestPgFixture(t *testing.T, expiry time.Duration) *TestPgFixture {
 			"postgres",
 			"-c", "log_statement=all",
 			"-c", "log_destination=stderr",
-			"-c", "max_connections=1000",
+			"-c", "max_connections=5000",
 		},
 	}, func(config *docker.HostConfig) {
 		// Set AutoRemove to true so that stopped container goes away

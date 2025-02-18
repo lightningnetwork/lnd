bb3839e422a43dde3f5736fb63593b5877dc296e
Elle Mouton <elle.mouton@gmail.com>
2025-02-18 15:06:33 -0300

    graph/db: completely remove cache from KVStore

--- graph/db/kv_store.go
+++ graph/db/kv_store.go
@@ -184,13 +184,12 @@ const (
 type KVStore struct {
 	db kvdb.Backend
 
-	// cacheMu guards all caches (rejectCache, chanCache, graphCache). If
+	// cacheMu guards all caches (rejectCache and chanCache). If
 	// this mutex will be acquired at the same time as the DB mutex then
 	// the cacheMu MUST be acquired first to prevent deadlock.
 	cacheMu     sync.RWMutex
 	rejectCache *rejectCache
 	chanCache   *channelCache
-	graphCache  *GraphCache
 
 	chanScheduler batch.Scheduler
 	nodeScheduler batch.Scheduler
@@ -225,15 +224,6 @@ func NewKVStore(db kvdb.Backend, options ...KVStoreOptionModifier) (*KVStore,
 	)
 
 	return g, nil
-}
-
-// setGraphCache sets the KVStore's graphCache.
-//
-// NOTE: this is temporary and will only be called from the ChannelGraph's
-// constructor before the KVStore methods are available to be called. This will
-// be removed once the graph cache is fully owned by the ChannelGraph.
-func (c *KVStore) setGraphCache(cache *GraphCache) {
-	c.graphCache = cache
 }
 
 // channelMapKey is the key structure used for storing channel edge policies.

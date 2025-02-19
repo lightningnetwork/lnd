84c3fbee062c4d3dff136cc73c8312277c59a523
Elle Mouton <elle.mouton@gmail.com>
2025-02-19 07:51:54 -0300

    multi: add Start and Stop methods for ChannelGraph
    We do this in preparation for moving channel cache population logic out
    of the constructor and into the Start method. We also will later on
    (when topology subscription is moved to the ChannelGraph), have a
    goroutine that will need to be kicked off and stopped.

--- server.go
+++ server.go
@@ -2287,6 +2287,12 @@ func (s *server) Start() error {
 			return
 		}
 
+		cleanup = cleanup.add(s.graphDB.Stop)
+		if err := s.graphDB.Start(); err != nil {
+			startErr = err
+			return
+		}
+
 		cleanup = cleanup.add(s.graphBuilder.Stop)
 		if err := s.graphBuilder.Start(); err != nil {
 			startErr = err
@@ -2587,6 +2593,9 @@ func (s *server) Stop() error {
 		}
 		if err := s.graphBuilder.Stop(); err != nil {
 			srvrLog.Warnf("failed to stop graphBuilder %v", err)
+		}
+		if err := s.graphDB.Stop(); err != nil {
+			srvrLog.Warnf("failed to stop graphDB %v", err)
 		}
 		if err := s.chainArb.Stop(); err != nil {
 			srvrLog.Warnf("failed to stop chainArb: %v", err)

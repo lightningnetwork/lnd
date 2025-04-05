34b1d9b9b3264a50ac0e7ed80fd3293dab0da85b
Elle Mouton <elle.mouton@gmail.com>
2025-04-05 19:24:48 +0200

    graph/db: add a framework for testing against SQL backends incrementally
    In this commit, we implement the postgres and sqlite versions of the
    NewTestDB function. We add the various build flags so that only one of
    the three versions of this function can be active at a time.
    
    We also introduce the SQLStore struct which is the SQL implementation of
    the V1Store interface.
    
    NOTE: it currently temporarily embeds the KVStore struct so that we can
    implement the V1Store interface incrementally. For any method not
    implemented, things will fall back to the KVStore. This is ONLY the
    case for the time being while this struct is purely used in unit tests
    only. Once all the methods have been implemented, the KVStore field will
    be removed from the SQLStore struct.

--- graph/db/test_sqlite.go
+++ graph/db/test_sqlite.go
@@ -1,16 +1,18 @@
-//go:build !test_db_sqlite && !test_db_postgres
+//go:build !test_db_posgres && test_db_sqlite
 
 package graphdb
 
 import (
+	"database/sql"
 	"testing"
 
 	"github.com/lightningnetwork/lnd/kvdb"
+	"github.com/lightningnetwork/lnd/sqldb"
 	"github.com/stretchr/testify/require"
 )
 
 // NewTestDB is a helper function that creates an BBolt database for testing.
-func NewTestDB(t testing.TB) *KVStore {
+func NewTestDB(t testing.TB) *SQLStore {
 	backend, backendCleanup, err := kvdb.GetTestBackend(t.TempDir(), "cgr")
 	require.NoError(t, err)
 
@@ -19,5 +21,13 @@ func NewTestDB(t testing.TB) *KVStore {
 	graphStore, err := NewKVStore(backend)
 	require.NoError(t, err)
 
-	return graphStore
+	db := sqldb.NewTestSqliteDB(t).BaseDB
+
+	executor := sqldb.NewTransactionExecutor(
+		db, func(tx *sql.Tx) SQLQueries {
+			return db.WithTx(tx)
+		},
+	)
+
+	return NewSQLStore(executor, graphStore)
 }

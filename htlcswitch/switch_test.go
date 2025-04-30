3271f17f8fa34737785900d59a14e873a93dbc63
Calvin Zachman <calvin.zachman@protonmail.com>
2025-04-30 11:59:23 -0500

    htlcswitch: add AttemptStore interface
    Preperatory refactor to allow for future alteration
    of the store backing the Switch.

--- htlcswitch/switch_test.go
+++ htlcswitch/switch_test.go
@@ -3106,7 +3106,7 @@ func TestSwitchGetAttemptResult(t *testing.T) {
 		isResolution: true,
 	}
 
-	err = s.networkResults.storeResult(paymentID, n)
+	err = s.attemptStore.StoreResult(paymentID, n)
 	require.NoError(t, err, "unable to store result")
 
 	// The result should be available.

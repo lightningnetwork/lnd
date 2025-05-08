25e652669bd4534f7f82789ad3d7715b89c8e9c6
Elle Mouton <elle.mouton@gmail.com>
2025-05-08 12:19:50 +0200

    plug into itests behind feature flag

--- sqldb/migrations_dev.go
+++ sqldb/migrations_dev.go
@@ -1,4 +1,4 @@
-//go:build test_db_postgres || test_db_sqlite
+//go:build test_db_postgres || test_db_sqlite || test_native_sql
 
 package sqldb
 
@@ -7,5 +7,14 @@ var migrationAdditions = []MigrationConfig{
 		Name:          "000007_graph",
 		Version:       8,
 		SchemaVersion: 7,
+	},
+	{
+		Name:          "kv_graph_migration",
+		Version:       9,
+		SchemaVersion: 7,
+		// A migration function may be attached to this
+		// migration to migrate KV graph to the native SQL
+		// schema. This is optional and can be disabled by the
+		// user if necessary.
 	},
 }

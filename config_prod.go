eefe1c73d179e0824e609486ae1d69d48f79a4fd
Elle Mouton <elle.mouton@gmail.com>
2025-07-02 12:27:37 +0200

    plug in graph migration into test_native_sql build

--- config_prod.go
+++ config_prod.go
@@ -3,9 +3,13 @@
 package lnd
 
 import (
+	"context"
+
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	graphdb "github.com/lightningnetwork/lnd/graph/db"
 	"github.com/lightningnetwork/lnd/kvdb"
 	"github.com/lightningnetwork/lnd/sqldb"
+	"github.com/lightningnetwork/lnd/sqldb/sqlc"
 )
 
 // getGraphStore returns a graphdb.V1Store backed by a graphdb.KVStore
@@ -15,4 +19,11 @@ func (d *DefaultDatabaseBuilder) getGraphStore(_ *sqldb.BaseDB,
 	opts ...graphdb.StoreOptionModifier) (graphdb.V1Store, error) {
 
 	return graphdb.NewKVStore(kvBackend, opts...)
+}
+
+func getSQLMigration(ctx context.Context, version int,
+	kvBackend kvdb.Backend,
+	chain chainhash.Hash) (func(tx *sqlc.Queries) error, bool) {
+
+	return nil, false
 }

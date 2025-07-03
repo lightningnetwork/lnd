39369e36a01827837f5425de6abe2c2ffcae5720
yyforyongyu <yong2452@gmail.com>
2025-07-04 03:39:03 +0800

    peer: skip Disconnect in read and write handlers
    There's no need to disconnect if the peer has already been disconnected.

--- peer/brontide.go
+++ peer/brontide.go
@@ -2069,6 +2069,9 @@ func (p *Brontide) readHandler() {
 	// gossiper?
 	p.initGossipSync()
 
+	// exitErr is the error to be used when disconnect the peer.
+	var exitErr error
+
 	discStream := newDiscMsgStream(p)
 	discStream.Start()
 	defer discStream.Stop()
@@ -2121,6 +2124,7 @@ out:
 			// didn't recognize, then we'll stop all processing as
 			// this is a fatal error.
 			default:
+				exitErr = err
 				break out
 			}
 		}
@@ -2264,9 +2268,13 @@ out:
 		idleTimer.Reset(idleTimeout)
 	}
 
-	p.Disconnect(errors.New("read handler closed"))
+	// Disconnect the peer on exitErr, but only if the peer hasn't been
+	// disconnected before.
+	if atomic.LoadInt32(&p.disconnect) == 0 {
+		p.Disconnect(exitErr)
+	}
 
-	p.log.Trace("readHandler for peer done")
+	p.log.Debugf("peer quit, exit readHandler")
 }
 
 // handleCustomMessage handles the given custom message if a handler is
@@ -2751,7 +2759,7 @@ out:
 			}
 
 		case <-p.cg.Done():
-			exitErr = lnpeer.ErrPeerExiting
+			p.log.Debug("peer quit, exit writeHandler")
 			break out
 		}
 	}
@@ -2760,7 +2768,9 @@ out:
 	// disconnect.
 	p.cg.WgDone()
 
-	p.Disconnect(exitErr)
+	if exitErr != nil {
+		p.Disconnect(exitErr)
+	}
 
 	p.log.Trace("writeHandler for peer done")
 }

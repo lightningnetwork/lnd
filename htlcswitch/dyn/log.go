c29556b92a00008df6504abf2213ebcced241b92
yyforyongyu <yong2452@gmail.com>
2025-07-15 14:47:52 +0800

    dyn: add package skeleton
    In this commit we introduce a new package, `htlcswitch/dyn`, which is
    used to handle the dynamic commitment. This is made of its own package
    to stop growing the `htlcswitch` package, and debugging will be easier
    this way as we can now just turn on debug logs for `DYNC`.

---
+++ htlcswitch/dyn/log.go
@@ -0,0 +1,32 @@
+package dyn
+
+import (
+	"github.com/btcsuite/btclog/v2"
+	"github.com/lightningnetwork/lnd/build"
+)
+
+// log is a logger that is initialized with no output filters. This means the
+// package will not perform any logging by default until the caller requests it.
+var log btclog.Logger
+
+// Subsystem defines the logging code for this subsystem.
+const Subsystem = "DYNC"
+
+// The default amount of logging is none.
+func init() {
+	logger := build.NewSubLogger(Subsystem, nil)
+
+	UseLogger(logger)
+}
+
+// DisableLog disables all library log output. Logging output is disabled by
+// default until UseLogger is called.
+func DisableLog() {
+	UseLogger(btclog.Disabled)
+}
+
+// UseLogger uses a specified Logger to output package logging info. This should
+// be used in preference to SetLogWriter if the caller is also using btclog.
+func UseLogger(logger btclog.Logger) {
+	log = logger
+}

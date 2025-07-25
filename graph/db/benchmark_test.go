335be2e0c4d31b03514abb7af06f718b07b301d6
Elle Mouton <elle.mouton@gmail.com>
2025-07-25 12:37:44 +0200

    graph/db: add test DB population helper
    This commit adds a TestPopulateDBs helper test (which is skipped by
    default) which can be used to point to a source graph DB which is then
    used to populate various target graph DBs. This is for the sake of
    generating various test graph DBs that can then be used to run benchmark
    tests against. It's also a useful test to have around so that real graph
    data can be used to simulate fast/batch writes to the backing DB.
    
    graph/db: add important TODO to investigate

--- graph/db/benchmark_test.go
+++ graph/db/benchmark_test.go
@@ -3,17 +3,22 @@ package graphdb
 import (
 	"context"
 	"database/sql"
-	"github.com/lightningnetwork/lnd/kvdb/postgres"
-	"github.com/lightningnetwork/lnd/kvdb/sqlite"
+	"errors"
 	"path"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/lightningnetwork/lnd/batch"
+	"github.com/lightningnetwork/lnd/graph/db/models"
 	"github.com/lightningnetwork/lnd/kvdb"
+	"github.com/lightningnetwork/lnd/kvdb/postgres"
 	"github.com/lightningnetwork/lnd/kvdb/sqlbase"
+	"github.com/lightningnetwork/lnd/kvdb/sqlite"
 	"github.com/lightningnetwork/lnd/sqldb"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/time/rate"
 )
 
 // Here we define various database paths, connection strings and file names that
@@ -249,4 +254,241 @@ func newSQLStore(t testing.TB, db sqldb.DB) V1Store {
 	require.NoError(t, err)
 
 	return store
+}
+
+// TestPopulateDBs is a helper test that can be used to populate various local
+// graph DBs from some source graph DB. This can then be used to run the
+// various benchmark tests against the same graph data.
+//
+// TODO(elle): this test reveals that the batching logic we use might be
+// problematic for postgres backends. This needs some investigation & it might
+// make sense to only use LazyAdd for sqlite backends & it may make sense to
+// also add a maximum batch size to avoid grouping too many updates at once.
+// Observations:
+//   - the LazyAdd options need to be turned off for channel/policy update calls
+//     for both the native SQL postgres & kvdb postgres backend for this test to
+//     succeed.
+//   - The LazyAdd option must be added for the sqlite backends, else it takes
+//     very long to sync the graph.
+func TestPopulateDBs(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	// NOTE: uncomment the line below to run this test locally.
+	t.Skipf("Skipping local helper test")
+
+	// Set your desired source database.
+	sourceDB := kvdbSqliteConn
+
+	// Populate this list with the desired destination databases.
+	destinations := []dbConnection{
+		kvdbBBoltConn,
+		nativeSQLSqliteConn,
+		kvdbPostgresConn,
+		nativeSQLPostgresConn,
+	}
+
+	// Open and start the source graph.
+	src, err := NewChannelGraph(sourceDB.open(t))
+	require.NoError(t, err)
+	require.NoError(t, src.Start())
+	t.Cleanup(func() {
+		require.NoError(t, src.Stop())
+	})
+
+	// countNodes is a helper function to count the number of nodes in the
+	// graph.
+	countNodes := func(graph *ChannelGraph) int {
+		numNodes := 0
+		err := graph.ForEachNode(ctx, func(tx NodeRTx) error {
+			numNodes++
+			return nil
+		}, func() {})
+		require.NoError(t, err)
+
+		return numNodes
+	}
+
+	// countChannels is a helper function to count the number of channels
+	// in the graph.
+	countChannels := func(graph *ChannelGraph) (int, int) {
+		var (
+			numChans    = 0
+			numPolicies = 0
+		)
+		err := graph.ForEachChannel(
+			ctx, func(info *models.ChannelEdgeInfo,
+				policy,
+				policy2 *models.ChannelEdgePolicy) error {
+
+				numChans++
+				if policy != nil {
+					numPolicies++
+				}
+				if policy2 != nil {
+					numPolicies++
+				}
+
+				return nil
+			}, func() {})
+		require.NoError(t, err)
+
+		return numChans, numPolicies
+	}
+
+	t.Logf("Number of nodes in source graph (%s): %d", sourceDB.name,
+		countNodes(src))
+	numChan, numPol := countChannels(src)
+	t.Logf("Number of channels & policies in source graph (%s): %d "+
+		"channels, %d policies", sourceDB.name, numChan, numPol)
+
+	for _, destDB := range destinations {
+		t.Run(destDB.name, func(t *testing.T) {
+			t.Parallel()
+
+			// Open and start the destination graph.
+			dest, err := NewChannelGraph(destDB.open(t))
+			require.NoError(t, err)
+			require.NoError(t, dest.Start())
+			t.Cleanup(func() {
+				require.NoError(t, dest.Stop())
+			})
+
+			t.Logf("Number of nodes in %s graph: %d", destDB.name,
+				countNodes(dest))
+			numChan, numPol := countChannels(dest)
+			t.Logf("Number of channels in %s graph: %d, %d",
+				destDB.name, numChan, numPol)
+
+			// Sync the source graph to the destination graph.
+			syncGraph(t, src, dest)
+
+			t.Logf("Number of nodes in %s graph after sync: %d",
+				destDB.name, countNodes(dest))
+			numChan, numPol = countChannels(dest)
+			t.Logf("Number of channels in %s graph after sync: "+
+				"%d, %d", destDB.name, numChan, numPol)
+		})
+	}
+}
+
+// syncGraph synchronizes the source graph with the destination graph by
+// copying all nodes and channels from the source to the destination.
+func syncGraph(t *testing.T, src, dest *ChannelGraph) {
+	ctx := context.Background()
+
+	var (
+		s = rate.Sometimes{
+			Interval: 10 * time.Second,
+		}
+		t0 = time.Now()
+
+		chunk = 0
+		total = 0
+		mu    sync.Mutex
+	)
+
+	reportNodeStats := func() {
+		elapsed := time.Since(t0).Seconds()
+		ratePerSec := float64(chunk) / elapsed
+		t.Logf("Synced %d nodes (last chunk: %d) "+
+			"(%.2f nodes/second)",
+			total, chunk, ratePerSec)
+
+		t0 = time.Now()
+	}
+
+	var wgNodes sync.WaitGroup
+	err := src.ForEachNode(ctx, func(tx NodeRTx) error {
+		wgNodes.Add(1)
+		go func() {
+			defer wgNodes.Done()
+
+			err := dest.AddLightningNode(
+				ctx, tx.Node(), batch.LazyAdd(),
+			)
+			require.NoError(t, err)
+
+			mu.Lock()
+			total++
+			chunk++
+			s.Do(func() {
+				reportNodeStats()
+				chunk = 0
+			})
+			mu.Unlock()
+		}()
+
+		return nil
+	}, func() {})
+	require.NoError(t, err)
+
+	wgNodes.Wait()
+	reportNodeStats()
+	t.Logf("Done syncing %d nodes", total)
+
+	total = 0
+	chunk = 0
+	t0 = time.Now()
+
+	reportChanStats := func() {
+		elapsed := time.Since(t0).Seconds()
+		ratePerSec := float64(chunk) / elapsed
+		t.Logf("Synced %d channels (and its "+
+			"policies) (last chunk: %d) "+
+			"(%.2f channels/second)",
+			total, chunk, ratePerSec)
+
+		t0 = time.Now()
+	}
+
+	var wgChans sync.WaitGroup
+	err = src.ForEachChannel(ctx, func(info *models.ChannelEdgeInfo,
+		policy1, policy2 *models.ChannelEdgePolicy) error {
+
+		// Add each channel & policy. We do this in a goroutine to
+		// take advantage of batch processing.
+		wgChans.Add(1)
+		go func() {
+			defer wgChans.Done()
+
+			err := dest.AddChannelEdge(
+				ctx, info, batch.LazyAdd(),
+			)
+			if !errors.Is(err, ErrEdgeAlreadyExist) {
+				require.NoError(t, err)
+			}
+
+			if policy1 != nil {
+				err = dest.UpdateEdgePolicy(
+					ctx, policy1, batch.LazyAdd(),
+				)
+				require.NoError(t, err)
+			}
+
+			if policy2 != nil {
+				err = dest.UpdateEdgePolicy(
+					ctx, policy2, batch.LazyAdd(),
+				)
+				require.NoError(t, err)
+			}
+
+			mu.Lock()
+			total++
+			chunk++
+			s.Do(func() {
+				reportChanStats()
+				chunk = 0
+			})
+			mu.Unlock()
+		}()
+
+		return nil
+	}, func() {})
+	require.NoError(t, err)
+
+	wgChans.Wait()
+	reportChanStats()
+
+	t.Logf("Done syncing %d channels", total)
 }

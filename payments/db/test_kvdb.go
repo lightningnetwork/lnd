9f62108cb88d8e574db3cb7a1414f2c11d415f13
ziggie <ziggie1984@protonmail.com>
2025-08-12 18:14:57 +0200

    paymentsdb: move db interface dependant tests to different file
    This commit starts reusing test cases which are not dependant on
    the kv db backend. So they can be later used with the native db
    implementation as well.

--- payments/db/test_kvdb.go
+++ payments/db/test_kvdb.go
@@ -8,7 +8,24 @@ import (
 )
 
 // NewTestDB is a helper function that creates an BBolt database for testing.
-func NewTestDB(t *testing.T, opts ...OptionModifier) *KVPaymentsDB {
+func NewTestDB(t *testing.T, opts ...OptionModifier) PaymentDB {
+	backend, backendCleanup, err := kvdb.GetTestBackend(
+		t.TempDir(), "kvPaymentDB",
+	)
+	require.NoError(t, err)
+
+	t.Cleanup(backendCleanup)
+
+	paymentDB, err := NewKVPaymentsDB(backend, opts...)
+	require.NoError(t, err)
+
+	return paymentDB
+}
+
+// NewKVTestDB is a helper function that creates an BBolt database for testing
+// and there is no need to convert the interface to the KVPaymentsDB because for
+// some unit tests we still need access to the kvdb interface.
+func NewKVTestDB(t *testing.T, opts ...OptionModifier) *KVPaymentsDB {
 	backend, backendCleanup, err := kvdb.GetTestBackend(
 		t.TempDir(), "kvPaymentDB",
 	)

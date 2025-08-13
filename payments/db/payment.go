8245e356e5e941839229137244ea4513bf03aeb8
ziggie <ziggie1984@protonmail.com>
2025-08-13 15:06:44 +0200

    paymentsdb: move serialization methods to kv_store file

--- payments/db/payment.go
+++ payments/db/payment.go
@@ -4,12 +4,9 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
-	"io"
-	"math"
 	"time"
 
 	"github.com/btcsuite/btcd/btcec/v2"
-	"github.com/btcsuite/btcd/wire"
 	"github.com/davecgh/go-spew/spew"
 	sphinx "github.com/lightningnetwork/lightning-onion"
 	"github.com/lightningnetwork/lnd/lntypes"
@@ -648,94 +645,6 @@ func (m *MPPayment) AllowMoreAttempts() (bool, error) {
 
 	// Now we know we can register new HTLCs.
 	return true, nil
-}
-
-// serializeHTLCSettleInfo serializes the details of a settled htlc.
-func serializeHTLCSettleInfo(w io.Writer, s *HTLCSettleInfo) error {
-	if _, err := w.Write(s.Preimage[:]); err != nil {
-		return err
-	}
-
-	if err := serializeTime(w, s.SettleTime); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// deserializeHTLCSettleInfo deserializes the details of a settled htlc.
-func deserializeHTLCSettleInfo(r io.Reader) (*HTLCSettleInfo, error) {
-	s := &HTLCSettleInfo{}
-	if _, err := io.ReadFull(r, s.Preimage[:]); err != nil {
-		return nil, err
-	}
-
-	var err error
-	s.SettleTime, err = deserializeTime(r)
-	if err != nil {
-		return nil, err
-	}
-
-	return s, nil
-}
-
-// serializeHTLCFailInfo serializes the details of a failed htlc including the
-// wire failure.
-func serializeHTLCFailInfo(w io.Writer, f *HTLCFailInfo) error {
-	if err := serializeTime(w, f.FailTime); err != nil {
-		return err
-	}
-
-	// Write failure. If there is no failure message, write an empty
-	// byte slice.
-	var messageBytes bytes.Buffer
-	if f.Message != nil {
-		err := lnwire.EncodeFailureMessage(&messageBytes, f.Message, 0)
-		if err != nil {
-			return err
-		}
-	}
-	if err := wire.WriteVarBytes(w, 0, messageBytes.Bytes()); err != nil {
-		return err
-	}
-
-	return WriteElements(w, byte(f.Reason), f.FailureSourceIndex)
-}
-
-// deserializeHTLCFailInfo deserializes the details of a failed htlc including
-// the wire failure.
-func deserializeHTLCFailInfo(r io.Reader) (*HTLCFailInfo, error) {
-	f := &HTLCFailInfo{}
-	var err error
-	f.FailTime, err = deserializeTime(r)
-	if err != nil {
-		return nil, err
-	}
-
-	// Read failure.
-	failureBytes, err := wire.ReadVarBytes(
-		r, 0, math.MaxUint16, "failure",
-	)
-	if err != nil {
-		return nil, err
-	}
-	if len(failureBytes) > 0 {
-		f.Message, err = lnwire.DecodeFailureMessage(
-			bytes.NewReader(failureBytes), 0,
-		)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	var reason byte
-	err = ReadElements(r, &reason, &f.FailureSourceIndex)
-	if err != nil {
-		return nil, err
-	}
-	f.Reason = HTLCFailReason(reason)
-
-	return f, nil
 }
 
 // generateSphinxPacket generates then encodes a sphinx packet which encodes

96dca1efdce4e30cd18625e91797bd5df0e41fcf
ziggie <ziggie1984@protonmail.com>
2025-08-25 16:41:42 +0200

    lnd+paymentsdb:  introduce harness for the payment sql backend
    We prepare the code for the sql payment backend. However no
    payment db interface method for the sql backend is implemented
    yet. This will be done in the following commits. Currently they
    are just dummies to allow the commit to compile properly.

--- config_test_native_sql.go
+++ config_test_native_sql.go
@@ -10,6 +10,7 @@ import (
 	graphdb "github.com/lightningnetwork/lnd/graph/db"
 	"github.com/lightningnetwork/lnd/kvdb"
 	"github.com/lightningnetwork/lnd/lncfg"
+	paymentsdb "github.com/lightningnetwork/lnd/payments/db"
 	"github.com/lightningnetwork/lnd/sqldb"
 	"github.com/lightningnetwork/lnd/sqldb/sqlc"
 )
@@ -79,4 +80,29 @@ func (d *DefaultDatabaseBuilder) getSQLMigration(ctx context.Context,
 	// No version was matched, so we return false to indicate that no
 	// migration is known for the given version.
 	return nil, false
+}
+
+// getPaymentsStore returns a paymentsdb.DB backed by a paymentsdb.SQLStore
+// implementation.
+func (d *DefaultDatabaseBuilder) getPaymentsStore(baseDB *sqldb.BaseDB,
+	kvBackend kvdb.Backend,
+	opts ...paymentsdb.StoreOptionModifier) (paymentsdb.DB, error) {
+
+	paymentsExecutor := sqldb.NewTransactionExecutor(
+		baseDB, func(tx *sql.Tx) paymentsdb.SQLQueries {
+			return baseDB.WithTx(tx)
+		},
+	)
+
+	queryConfig := d.cfg.DB.Sqlite.QueryConfig
+	if d.cfg.DB.Backend == lncfg.PostgresBackend {
+		queryConfig = d.cfg.DB.Postgres.QueryConfig
+	}
+
+	return paymentsdb.NewSQLStore(
+		&paymentsdb.SQLStoreConfig{
+			QueryCfg: &queryConfig,
+		},
+		paymentsExecutor, opts...,
+	)
 }

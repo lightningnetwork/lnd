c590675a8da0a9db692739ccb8d2d54c86453344
Elle Mouton <elle.mouton@gmail.com>
2025-09-04 13:21:49 +0200

    graph/db: simplify auth proof and edge info
    Remove various unused fields and methods.

--- graph/db/models/channel_edge_info.go
+++ graph/db/models/channel_edge_info.go
@@ -26,9 +26,6 @@ type ChannelEdgeInfo struct {
 
 	// ChainHash is the hash that uniquely identifies the chain that this
 	// channel was opened within.
-	//
-	// TODO(roasbeef): need to modify db keying for multi-chain
-	//  * must add chain hash to prefix as well
 	ChainHash chainhash.Hash
 
 	// NodeKey1Bytes is the raw public key of the first node.
@@ -41,11 +38,9 @@ type ChannelEdgeInfo struct {
 
 	// BitcoinKey1Bytes is the raw public key of the first node.
 	BitcoinKey1Bytes [33]byte
-	bitcoinKey1      *btcec.PublicKey
 
 	// BitcoinKey2Bytes is the raw public key of the first node.
 	BitcoinKey2Bytes [33]byte
-	bitcoinKey2      *btcec.PublicKey
 
 	// Features is the list of protocol features supported by this channel
 	// edge.
@@ -77,24 +72,6 @@ type ChannelEdgeInfo struct {
 	// and ensure we're able to make upgrades to the network in a forwards
 	// compatible manner.
 	ExtraOpaqueData []byte
-}
-
-// AddNodeKeys is a setter-like method that can be used to replace the set of
-// keys for the target ChannelEdgeInfo.
-func (c *ChannelEdgeInfo) AddNodeKeys(nodeKey1, nodeKey2, bitcoinKey1,
-	bitcoinKey2 *btcec.PublicKey) {
-
-	c.nodeKey1 = nodeKey1
-	copy(c.NodeKey1Bytes[:], c.nodeKey1.SerializeCompressed())
-
-	c.nodeKey2 = nodeKey2
-	copy(c.NodeKey2Bytes[:], nodeKey2.SerializeCompressed())
-
-	c.bitcoinKey1 = bitcoinKey1
-	copy(c.BitcoinKey1Bytes[:], c.bitcoinKey1.SerializeCompressed())
-
-	c.bitcoinKey2 = bitcoinKey2
-	copy(c.BitcoinKey2Bytes[:], bitcoinKey2.SerializeCompressed())
 }
 
 // NodeKey1 is the identity public key of the "first" node that was involved in
@@ -135,46 +112,6 @@ func (c *ChannelEdgeInfo) NodeKey2() (*btcec.PublicKey, error) {
 		return nil, err
 	}
 	c.nodeKey2 = key
-
-	return key, nil
-}
-
-// BitcoinKey1 is the Bitcoin multi-sig key belonging to the first node, that
-// was involved in the funding transaction that originally created the channel
-// that this struct represents.
-//
-// NOTE: By having this method to access an attribute, we ensure we only need
-// to fully deserialize the pubkey if absolutely necessary.
-func (c *ChannelEdgeInfo) BitcoinKey1() (*btcec.PublicKey, error) {
-	if c.bitcoinKey1 != nil {
-		return c.bitcoinKey1, nil
-	}
-
-	key, err := btcec.ParsePubKey(c.BitcoinKey1Bytes[:])
-	if err != nil {
-		return nil, err
-	}
-	c.bitcoinKey1 = key
-
-	return key, nil
-}
-
-// BitcoinKey2 is the Bitcoin multi-sig key belonging to the second node, that
-// was involved in the funding transaction that originally created the channel
-// that this struct represents.
-//
-// NOTE: By having this method to access an attribute, we ensure we only need
-// to fully deserialize the pubkey if absolutely necessary.
-func (c *ChannelEdgeInfo) BitcoinKey2() (*btcec.PublicKey, error) {
-	if c.bitcoinKey2 != nil {
-		return c.bitcoinKey2, nil
-	}
-
-	key, err := btcec.ParsePubKey(c.BitcoinKey2Bytes[:])
-	if err != nil {
-		return nil, err
-	}
-	c.bitcoinKey2 = key
 
 	return key, nil
 }

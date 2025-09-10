09df35569550ea379c78f5fdb8450afe25e34a2b
Calvin Zachman <calvin.zachman@protonmail.com>
2025-09-10 13:00:41 -0400

    itest: add send_onion test

--- itest/list_on_test.go
+++ itest/list_on_test.go
@@ -759,6 +759,10 @@ var allTestCases = []*lntest.TestCase{
 		Name:     "estimate fee",
 		TestFunc: testEstimateFee,
 	},
+	{
+		Name:     "send onion",
+		TestFunc: testSendOnion,
+	},
 }
 
 // appendPrefixed is used to add a prefix to each test name in the subtests

c568bd6f16bacd87bdcbd265c74fdfd9b9fc5c02
3nprob <3nprob@3nprob>
2025-09-23 03:54:13 +0000

    tor: improve logging

--- lnd.go
+++ lnd.go
@@ -510,16 +510,17 @@ func Main(cfg *Config, lisCfg ListenerCfg, implCfg *ImplementationCfg,
 	}
 
 	if cfg.Tor.StreamIsolation && cfg.Tor.SkipProxyForClearNetTargets {
-		srvrLog.Warn("!!! Skipping Tor while Stream Isolation is on." +
+		srvrLog.Warn("Danger! Skipping Tor while Stream Isolation is on." +
 			"This has high risk of leaking your IP. Make sure this is " +
 			"what you want.")
 	}
 
 	if cfg.Tor.Active {
-		if cfg.Tor.SkipProxyForClearNetTargets {
-			srvrLog.InfoS(ctx, "Onion services are accessible "+
-				"via Tor! NOTE: Traffic to clearnet services "+
-				"is not routed via Tor.")
+		if cfg.Tor.NoProxyTargets != defaultNetNoProxyTargets {
+			srvrLog.InfoS(ctx, fmt.Printf("Proxying all network traffic "+
+				"via Tor, EXCEPT [%s]! NOTE: Ensure the backend node is "+
+				"proxying over Tor as well"),
+				"stream_isolation", cfg.Tor.StreamIsolation)
 		} else {
 			srvrLog.InfoS(ctx, "Proxying all network traffic "+
 				"via Tor! NOTE: Ensure the backend node is "+

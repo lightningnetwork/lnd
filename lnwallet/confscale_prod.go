93cb115d78dac73ad0953ed9d19916cd87ffe6e0
Olaoluwa Osuntokun <laolu32@gmail.com>
2025-10-01 16:42:45 -0700

    fixup! lnwallet: define helper func to coop close conf scaling

--- lnwallet/confscale_prod.go
+++ lnwallet/confscale_prod.go
@@ -7,10 +7,19 @@ import "github.com/btcsuite/btcd/btcutil"
 
 // CloseConfsForCapacity returns the number of confirmations to wait
 // before signaling a cooperative close, scaled by channel capacity.
-// This uses the ScaleNumConfs function for consistent scaling with
-// funding confirmations, but without considering push amounts.
+// We enforce a minimum of 3 confirmations to provide better reorg protection,
+// even for small channels.
 func CloseConfsForCapacity(cap btcutil.Amount) uint32 { //nolint:revive
 	// For cooperative closes, we don't have a push amount to consider,
 	// so we pass 0 for the pushAmt parameter.
-	return uint32(ScaleNumConfs(cap, 0))
+	scaledConfs := uint32(ScaleNumConfs(cap, 0))
+	
+	// Enforce a minimum of 3 confirmations for reorg safety.
+	// This protects against shallow reorgs which are more common.
+	const minCoopCloseConfs = 3
+	if scaledConfs < minCoopCloseConfs {
+		return minCoopCloseConfs
+	}
+	
+	return scaledConfs
 }

e765b3a7b107f094b57eb9fd1b0b0cf5c67a2303
ziggie <ziggie1984@protonmail.com>
2025-10-21 11:46:35 +0200

    routing: add context to reloadPayment method

--- routing/payment_lifecycle.go
+++ routing/payment_lifecycle.go
@@ -239,7 +239,7 @@ lifecycle:
 		}
 
 		// We update the payment state on every iteration.
-		currentPayment, ps, err := p.reloadPayment()
+		currentPayment, ps, err := p.reloadPayment(ctx)
 		if err != nil {
 			return exitWithErr(err)
 		}
@@ -1143,10 +1143,9 @@ func (p *paymentLifecycle) reloadInflightAttempts(
 }
 
 // reloadPayment returns the latest payment found in the db (control tower).
-func (p *paymentLifecycle) reloadPayment() (paymentsdb.DBMPPayment,
+func (p *paymentLifecycle) reloadPayment(
+	ctx context.Context) (paymentsdb.DBMPPayment,
 	*paymentsdb.MPPaymentState, error) {
-
-	ctx := context.TODO()
 
 	// Read the db to get the latest state of the payment.
 	payment, err := p.router.cfg.Control.FetchPayment(ctx, p.identifier)

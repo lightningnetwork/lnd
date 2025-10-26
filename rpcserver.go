d2db80c10cc8f1bd49d4c41470b28799a7688d8e
Elle Mouton <elle.mouton@gmail.com>
2025-10-26 17:32:01 +0200

    graph/db: introduce VersionedGraph
    For now, all instances will use V1 only so as not to change behaviour
    yet.

--- rpcserver.go
+++ rpcserver.go
@@ -745,7 +745,7 @@ func (r *rpcServer) addDeps(ctx context.Context, s *server,
 			return info.NodeKey1Bytes, info.NodeKey2Bytes, nil
 		},
 		HasNode: func(nodePub route.Vertex) (bool, error) {
-			exists, err := graph.HasNode(ctx, nodePub)
+			exists, err := s.v1Graph.HasNode(ctx, nodePub)
 
 			return exists, err
 		},
@@ -1820,7 +1820,7 @@ func (r *rpcServer) VerifyMessage(ctx context.Context,
 	// channels signed the message.
 	//
 	// TODO(phlip9): Require valid nodes to have capital in active channels.
-	graph := r.server.graphDB
+	graph := r.server.v1Graph
 	active, err := graph.HasNode(ctx, pub)
 	if err != nil {
 		return nil, fmt.Errorf("failed to query graph: %w", err)
@@ -7128,7 +7128,7 @@ func (r *rpcServer) GetNodeInfo(ctx context.Context,
 			"include_channels")
 	}
 
-	graph := r.server.graphDB
+	graph := r.server.v1Graph
 
 	// First, parse the hex-encoded public key into a full in-memory public
 	// key object we can work with for querying.
@@ -8283,7 +8283,7 @@ func (r *rpcServer) ForwardingHistory(ctx context.Context,
 			return "", err
 		}
 
-		peer, err := r.server.graphDB.FetchNode(ctx, vertex)
+		peer, err := r.server.v1Graph.FetchNode(ctx, vertex)
 		if err != nil {
 			return "", err
 		}

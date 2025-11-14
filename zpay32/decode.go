d31fe5213dee08e4331c0c2f6d9c2c3d4d826bdd
MPins <pinsdorf@proton.me>
2025-11-14 12:00:03 -0300

    zpay32: enforce low-S signature when `n` is defined
    Enforce low-S canonical signatures when n is defined and include
    test vectors to validate the new behavior.

--- zpay32/decode.go
+++ zpay32/decode.go
@@ -186,6 +186,10 @@ func Decode(invoice string, net *chaincfg.Params, opts ...DecodeOption) (
 			return nil, fmt.Errorf("unable to deserialize "+
 				"signature: %v", err)
 		}
+		// Ensure the signature is in canonical low-S form.
+		if err = ecdsa.VerifyLowS(sig.ToSignatureBytes()); err != nil {		
+			return nil, err
+		}
 		if !signature.Verify(hash, decodedInvoice.Destination) {
 			return nil, fmt.Errorf("invalid invoice signature")
 		}

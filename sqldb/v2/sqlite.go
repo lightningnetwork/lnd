10d0851cb280a63818caad9c9e092308490dd811
Viktor Torstensson <viktor.t.git@gmail.com>
2025-12-18 18:39:39 +0100

    sqldb/v2: move Sqlite test helpers to separate file

--- sqldb/v2/sqlite.go
+++ sqldb/v2/sqlite.go
@@ -6,14 +6,11 @@ import (
 	"database/sql"
 	"fmt"
 	"net/url"
-	"path/filepath"
-	"testing"
 	"time"
 
 	"github.com/golang-migrate/migrate/v4"
 	sqlite_migrate "github.com/golang-migrate/migrate/v4/database/sqlite"
 	"github.com/lightningnetwork/lnd/fn/v2"
-	"github.com/stretchr/testify/require"
 	_ "modernc.org/sqlite" // Register relevant drivers.
 )
 
@@ -305,79 +302,4 @@ func (s *SqliteStore) SetSchemaVersion(version int, dirty bool) error {
 	}
 
 	return driver.SetVersion(version, dirty)
-}
-
-// NewTestSqliteDB is a helper function that creates an SQLite database for
-// testing.
-func NewTestSqliteDB(t testing.TB, streams []MigrationStream) *SqliteStore {
-	t.Helper()
-
-	t.Logf("Creating new SQLite DB for testing")
-
-	// TODO(roasbeef): if we pass :memory: for the file name, then we get
-	// an in mem version to speed up tests
-	dbFileName := filepath.Join(t.TempDir(), "tmp.db")
-	sqlDB, err := NewSqliteStore(&SqliteConfig{
-		SkipMigrations: false,
-	}, dbFileName)
-	require.NoError(t, err)
-
-	require.NoError(t, ApplyAllMigrations(sqlDB, streams))
-
-	t.Cleanup(func() {
-		require.NoError(t, sqlDB.DB.Close())
-	})
-
-	return sqlDB
-}
-
-// NewTestSqliteDBFromPath is a helper function that creates a SQLite database
-// for testing from a given database file path.
-func NewTestSqliteDBFromPath(t *testing.T, dbPath string,
-	streams []MigrationStream) *SqliteStore {
-
-	t.Helper()
-
-	t.Logf("Creating new SQLite DB for testing, using DB path %s", dbPath)
-
-	sqlDB, err := NewSqliteStore(&SqliteConfig{
-		SkipMigrations: false,
-	}, dbPath)
-	require.NoError(t, err)
-
-	require.NoError(t, ApplyAllMigrations(sqlDB, streams))
-
-	t.Cleanup(func() {
-		require.NoError(t, sqlDB.DB.Close())
-	})
-
-	return sqlDB
-}
-
-// NewTestSqliteDBWithVersion is a helper function that creates an SQLite
-// database for testing and migrates it to the given version.
-func NewTestSqliteDBWithVersion(t *testing.T, stream MigrationStream,
-	version uint) *SqliteStore {
-
-	t.Helper()
-
-	t.Logf("Creating new SQLite DB for testing, migrating to version %d",
-		version)
-
-	// TODO(roasbeef): if we pass :memory: for the file name, then we get
-	// an in mem version to speed up tests
-	dbFileName := filepath.Join(t.TempDir(), "tmp.db")
-	sqlDB, err := NewSqliteStore(&SqliteConfig{
-		SkipMigrations: true,
-	}, dbFileName)
-	require.NoError(t, err)
-
-	err = sqlDB.ExecuteMigrations(TargetVersion(version), stream)
-	require.NoError(t, err)
-
-	t.Cleanup(func() {
-		require.NoError(t, sqlDB.DB.Close())
-	})
-
-	return sqlDB
 }

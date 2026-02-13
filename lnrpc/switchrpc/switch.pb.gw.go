dc4dacb713f29a8c147cda8062196c5311472108
Calvin Zachman <calvin.zachman@protonmail.com>
2026-02-13 17:38:34 -0500

    switchrpc: add DeleteAttempts RPC
    Expose the store-layer DeleteAttempts as a gRPC endpoint
    on the Switch sub-server, allowing remote callers to
    garbage-collect terminal attempt records by explicitly
    naming the finished IDs.
    
    A unit test with a stateful mock demonstrates that hard
    delete removes the idempotency protection, allowing
    re-dispatch with the same ID.

--- lnrpc/switchrpc/switch.pb.gw.go
+++ lnrpc/switchrpc/switch.pb.gw.go
@@ -133,6 +133,40 @@ func local_request_Switch_BuildOnion_0(ctx context.Context, marshaler runtime.Ma
 
 }
 
+func request_Switch_DeleteAttempts_0(ctx context.Context, marshaler runtime.Marshaler, client SwitchClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq DeleteAttemptsRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.DeleteAttempts(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Switch_DeleteAttempts_0(ctx context.Context, marshaler runtime.Marshaler, server SwitchServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq DeleteAttemptsRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.DeleteAttempts(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterSwitchHandlerServer registers the http handlers for service Switch to "mux".
 // UnaryRPC     :call SwitchServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -211,6 +245,31 @@ func RegisterSwitchHandlerServer(ctx context.Context, mux *runtime.ServeMux, ser
 		}
 
 		forward_Switch_BuildOnion_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_Switch_DeleteAttempts_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/switchrpc.Switch/DeleteAttempts", runtime.WithHTTPPathPattern("/v2/switch/deleteattempts"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Switch_DeleteAttempts_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Switch_DeleteAttempts_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -321,6 +380,28 @@ func RegisterSwitchHandlerClient(ctx context.Context, mux *runtime.ServeMux, cli
 
 	})
 
+	mux.Handle("POST", pattern_Switch_DeleteAttempts_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/switchrpc.Switch/DeleteAttempts", runtime.WithHTTPPathPattern("/v2/switch/deleteattempts"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Switch_DeleteAttempts_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Switch_DeleteAttempts_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -330,6 +411,8 @@ var (
 	pattern_Switch_TrackOnion_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v2", "switch", "track"}, ""))
 
 	pattern_Switch_BuildOnion_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v2", "switch", "build"}, ""))
+
+	pattern_Switch_DeleteAttempts_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v2", "switch", "deleteattempts"}, ""))
 )
 
 var (
@@ -338,4 +421,6 @@ var (
 	forward_Switch_TrackOnion_0 = runtime.ForwardResponseMessage
 
 	forward_Switch_BuildOnion_0 = runtime.ForwardResponseMessage
+
+	forward_Switch_DeleteAttempts_0 = runtime.ForwardResponseMessage
 )

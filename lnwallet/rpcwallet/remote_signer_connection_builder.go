f198c6ed4dcb1d0da811d82c542304f48cb60411
Viktor Torstensson <viktor.t.git@gmail.com>
2026-02-27 11:11:12 +0100

    f - rpcwallet: add `RemoteSignerConnectionBuilder`

--- lnwallet/rpcwallet/remote_signer_connection_builder.go
+++ lnwallet/rpcwallet/remote_signer_connection_builder.go
@@ -7,35 +7,21 @@ import (
 	"github.com/lightningnetwork/lnd/lncfg"
 )
 
-// RemoteSignerConnectionBuilder creates instances of the RemoteSignerConnection
-// interface, based on the provided configuration.
-type RemoteSignerConnectionBuilder struct {
-	cfg *lncfg.RemoteSigner
-}
-
-// NewRemoteSignerConnectionBuilder creates a new instance of the
-// RemoteSignerBuilder.
-func NewRemoteSignerConnectionBuilder(
-	cfg *lncfg.RemoteSigner) *RemoteSignerConnectionBuilder {
-
-	return &RemoteSignerConnectionBuilder{cfg}
-}
-
-// Build creates a new RemoteSignerConnection instance. If the configuration
-// specifies that an inbound remote signer should be used, a new
-// OutboundConnection is created. If the configuration specifies that an
+// BuildRemoteSignerConnection creates a new RemoteSignerConnection instance.
+// If the configuration specifies that an inbound remote signer should be used,
+// a new OutboundConnection is created. If the configuration specifies that an
 // outbound remote signer should be used, a new InboundConnection is created.
 // The function returns the created RemoteSignerConnection instance, and a
 // cleanup function that should be called when the RemoteSignerConnection is no
 // longer needed.
-func (b *RemoteSignerConnectionBuilder) Build(
-	ctx context.Context) (RemoteSignerConnection, error) {
+func BuildRemoteSignerConnection(ctx context.Context,
+	cfg *lncfg.RemoteSigner) (RemoteSignerConnection, error) {
 
-	if !b.cfg.Enable {
+	if !cfg.Enable {
 		// This should be unreachable, but this is an extra sanity check
 		return nil, errors.New("remote signer not enabled in " +
 			"config")
 	}
 
-	return NewOutboundConnection(ctx, b.cfg.ConnectionCfg)
+	return NewOutboundConnection(ctx, cfg.ConnectionCfg)
 }

0d58bc992b157ae410c9b91a97a92e8adbeb190e
BlueMoon <a.ireyram@hotmail.com>
2026-03-04 20:07:56 -0600

    Increase default recovery window to 31000
    Increase the default recovery windows for OGs Lightning runners.
    
    I tested 2500, 10000, 20000, 30000 and 31000 and with the last value I recover all my funds finally.
    
    Or maybe add a value in docs between 2500 to XXXXXX

--- cmd/commands/commands.go
+++ cmd/commands/commands.go
@@ -38,7 +38,7 @@ import (
 
 // TODO(roasbeef): expose all fee conf targets
 
-const defaultRecoveryWindow int32 = 2500
+const defaultRecoveryWindow int32 = 31000
 
 const (
 	defaultUtxoMinConf = 1

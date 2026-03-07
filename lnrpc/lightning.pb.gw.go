f0e228733d2667aef8ac4a0ad6454ba534f63e55
saubyk <39208279+saubyk@users.noreply.github.com>
2026-03-07 12:50:41 -0800

    lnrpc: add include_log field to GetDebugInfoRequest
    Add an `include_log` bool field to GetDebugInfoRequest proto message.
    When set to true, the server will include the log file content in the
    response in addition to the configuration map.

--- lnrpc/lightning.pb.gw.go
+++ lnrpc/lightning.pb.gw.go
@@ -550,9 +550,20 @@ func local_request_Lightning_GetInfo_0(ctx context.Context, marshaler runtime.Ma
 
 }
 
+var (
+	filter_Lightning_GetDebugInfo_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_Lightning_GetDebugInfo_0(ctx context.Context, marshaler runtime.Marshaler, client LightningClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetDebugInfoRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Lightning_GetDebugInfo_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := client.GetDebugInfo(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -562,6 +573,13 @@ func request_Lightning_GetDebugInfo_0(ctx context.Context, marshaler runtime.Mar
 func local_request_Lightning_GetDebugInfo_0(ctx context.Context, marshaler runtime.Marshaler, server LightningServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetDebugInfoRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Lightning_GetDebugInfo_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := server.GetDebugInfo(ctx, &protoReq)
 	return msg, metadata, err

534070858d3db74f5d08c97cd1a27fa3eb47ff07
Calvin Zachman <calvin.zachman@protonmail.com>
2026-03-24 12:30:19 -0400

    htlcswitch/hop: add default logger initialization
    Without this, running hop tests in isolation panics because the
    package's log variable is nil until the parent htlcswitch package
    calls UseLogger during init.

--- htlcswitch/hop/log.go
+++ htlcswitch/hop/log.go
@@ -2,12 +2,27 @@ package hop
 
 import (
 	"github.com/btcsuite/btclog/v2"
+	"github.com/lightningnetwork/lnd/build"
 )
+
+// Subsystem defines the logging sub system name of this package.
+const Subsystem = "HOPS"
 
 // log is a logger that is initialized with no output filters.  This
 // means the package will not perform any logging by default until the caller
 // requests it.
 var log btclog.Logger
+
+// The default amount of logging is none.
+func init() {
+	UseLogger(build.NewSubLogger(Subsystem, nil))
+}
+
+// DisableLog disables all library log output. Logging output is disabled
+// by default until UseLogger is called.
+func DisableLog() {
+	UseLogger(btclog.Disabled)
+}
 
 // UseLogger uses a specified Logger to output package logging info. This
 // function is called from the parent package htlcswitch logger initialization.

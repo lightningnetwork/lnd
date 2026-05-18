0c0fe0f8f371fd8b682abe7103c8c6a540623e2e
ziggie <ziggie1984@protonmail.com>
2026-05-17 22:25:18 -0300

    chanstate: move shutdown kv helpers
    Move the shutdown-info key, serialization, and channel-bucket
    
    put/fetch helpers into chanstate.
    
    Leave ChannelStateDB responsible for opening the channel bucket so this
    
    commit only moves the backend-specific record handling for shutdown
    
    state.

--- channeldb/channel.go
+++ channeldb/channel.go
@@ -122,12 +122,6 @@ var (
 	// broadcasted when moving the channel to state CoopBroadcasted.
 	coopCloseTxKey = []byte("coop-closing-tx-key")
 
-	// shutdownInfoKey points to the serialised shutdown info that has been
-	// persisted for a channel. The existence of this info means that we
-	// have sent the Shutdown message before and so should re-initiate the
-	// shutdown on re-establish.
-	shutdownInfoKey = []byte("shutdown-info-key")
-
 	// commitDiffKey stores the current pending commitment state we've
 	// extended to the remote party (if any). Each time we propose a new
 	// state, we store the information necessary to reconstruct this state
@@ -1121,12 +1115,6 @@ var (
 func (c *ChannelStateDB) StoreChannelShutdownInfo(channel *OpenChannel,
 	info *ShutdownInfo) error {
 
-	var b bytes.Buffer
-	err := encodeShutdownInfo(info, &b)
-	if err != nil {
-		return err
-	}
-
 	return kvdb.Update(c.backend, func(tx kvdb.RwTx) error {
 		chanBucket, err := fetchChanBucketRw(
 			tx, channel.IdentityPub, &channel.FundingOutpoint,
@@ -1136,7 +1124,7 @@ func (c *ChannelStateDB) StoreChannelShutdownInfo(channel *OpenChannel,
 			return err
 		}
 
-		return chanBucket.Put(shutdownInfoKey, b.Bytes())
+		return cstate.PutChannelShutdownInfo(chanBucket, info)
 	}, func() {})
 }
 
@@ -1162,12 +1150,7 @@ func (c *ChannelStateDB) FetchChannelShutdownInfo(
 			return err
 		}
 
-		shutdownInfoBytes := chanBucket.Get(shutdownInfoKey)
-		if shutdownInfoBytes == nil {
-			return ErrNoShutdownInfo
-		}
-
-		shutdownInfo, err = decodeShutdownInfo(shutdownInfoBytes)
+		shutdownInfo, err = cstate.FetchChannelShutdownInfo(chanBucket)
 
 		return err
 	}, func() {
@@ -3553,35 +3536,4 @@ func NewShutdownInfo(deliveryScript lnwire.DeliveryAddress,
 	locallyInitiated bool) *ShutdownInfo {
 
 	return cstate.NewShutdownInfo(deliveryScript, locallyInitiated)
-}
-
-// encodeShutdownInfo serialises the ShutdownInfo to the given io.Writer.
-func encodeShutdownInfo(s *ShutdownInfo, w io.Writer) error {
-	records := []tlv.Record{
-		s.DeliveryScript.Record(),
-		s.LocalInitiator.Record(),
-	}
-
-	stream, err := tlv.NewStream(records...)
-	if err != nil {
-		return err
-	}
-
-	return stream.Encode(w)
-}
-
-// decodeShutdownInfo constructs a ShutdownInfo struct by decoding the given
-// byte slice.
-func decodeShutdownInfo(b []byte) (*ShutdownInfo, error) {
-	tlvStream := lnwire.ExtraOpaqueData(b)
-
-	var info ShutdownInfo
-	records := []tlv.RecordProducer{
-		&info.DeliveryScript,
-		&info.LocalInitiator,
-	}
-
-	_, err := tlvStream.ExtractRecords(records...)
-
-	return &info, err
 }

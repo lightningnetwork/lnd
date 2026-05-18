054cd09e2da81a7c29964bc6ff7c1c98eea518a4
ziggie <ziggie1984@protonmail.com>
2026-05-17 22:53:22 -0300

    chanstate: move channel kv keys
    Move the live channel-state bucket and record key definitions into
    
    chanstate while keeping channeldb aliases for the current callers.
    
    This keeps the on-disk layout documentation next to the channel-state
    
    package before the larger open-channel serialization code is moved.

--- channeldb/channel.go
+++ channeldb/channel.go
@@ -38,91 +38,20 @@ const (
 )
 
 var (
-	// closedChannelBucket stores summarization information concerning
-	// previously open, but now closed channels.
-	closedChannelBucket = []byte("closed-chan-bucket")
-
-	// openChannelBucket stores all the currently open channels. This bucket
-	// has a second, nested bucket which is keyed by a node's ID. Within
-	// that node ID bucket, all attributes required to track, update, and
-	// close a channel are stored.
-	//
-	// openChan -> nodeID -> chanPoint
-	//
-	// TODO(roasbeef): flesh out comment
-	openChannelBucket = []byte("open-chan-bucket")
-
-	// outpointBucket stores all of our channel outpoints and a tlv
-	// stream containing channel data.
-	//
-	// outpoint -> tlv stream.
-	//
-	outpointBucket = []byte("outpoint-bucket")
-
-	// chanIDBucket stores all of the 32-byte channel ID's we know about.
-	// These could be derived from outpointBucket, but it is more
-	// convenient to have these in their own bucket.
-	//
-	// chanID -> tlv stream.
-	//
-	chanIDBucket = []byte("chan-id-bucket")
-
-	// historicalChannelBucket stores all channels that have seen their
-	// commitment tx confirm. All information from their previous open state
-	// is retained.
-	historicalChannelBucket = []byte("historical-chan-bucket")
-
-	// chanInfoKey can be accessed within the bucket for a channel
-	// (identified by its chanPoint). This key stores all the static
-	// information for a channel which is decided at the end of  the
-	// funding flow.
-	chanInfoKey = []byte("chan-info-key")
-
-	// localUpfrontShutdownKey can be accessed within the bucket for a channel
-	// (identified by its chanPoint). This key stores an optional upfront
-	// shutdown script for the local peer.
-	localUpfrontShutdownKey = []byte("local-upfront-shutdown-key")
-
-	// remoteUpfrontShutdownKey can be accessed within the bucket for a channel
-	// (identified by its chanPoint). This key stores an optional upfront
-	// shutdown script for the remote peer.
-	remoteUpfrontShutdownKey = []byte("remote-upfront-shutdown-key")
-
-	// chanCommitmentKey can be accessed within the sub-bucket for a
-	// particular channel. This key stores the up to date commitment state
-	// for a particular channel party. Appending a 0 to the end of this key
-	// indicates it's the commitment for the local party, and appending a 1
-	// to the end of this key indicates it's the commitment for the remote
-	// party.
-	chanCommitmentKey = []byte("chan-commitment-key")
-
-	// unsignedAckedUpdatesKey is an entry in the channel bucket that
-	// contains the remote updates that we have acked, but not yet signed
-	// for in one of our remote commits.
-	unsignedAckedUpdatesKey = []byte("unsigned-acked-updates-key")
-
-	// remoteUnsignedLocalUpdatesKey is an entry in the channel bucket that
-	// contains the local updates that the remote party has acked, but
-	// has not yet signed for in one of their local commits.
-	remoteUnsignedLocalUpdatesKey = []byte("remote-unsigned-local-updates-key")
-
-	// revocationStateKey stores their current revocation hash, our
-	// preimage producer and their preimage store.
-	revocationStateKey = []byte("revocation-state-key")
-
-	// commitDiffKey stores the current pending commitment state we've
-	// extended to the remote party (if any). Each time we propose a new
-	// state, we store the information necessary to reconstruct this state
-	// from the prior commitment. This allows us to resync the remote party
-	// to their expected state in the case of message loss.
-	//
-	// TODO(roasbeef): rename to commit chain?
-	commitDiffKey = []byte("commit-diff-key")
-
-	// lastWasRevokeKey is a key that stores true when the last update we
-	// sent was a revocation and false when it was a commitment signature.
-	// This is nil in the case of new channels with no updates exchanged.
-	lastWasRevokeKey = []byte("last-was-revoke")
+	closedChannelBucket           = cstate.ClosedChannelBucketKey()
+	openChannelBucket             = cstate.OpenChannelBucketKey()
+	outpointBucket                = cstate.OutpointBucketKey()
+	chanIDBucket                  = cstate.ChanIDBucketKey()
+	historicalChannelBucket       = cstate.HistoricalChannelBucketKey()
+	chanInfoKey                   = cstate.ChanInfoKey()
+	localUpfrontShutdownKey       = cstate.LocalUpfrontShutdownKey()
+	remoteUpfrontShutdownKey      = cstate.RemoteUpfrontShutdownKey()
+	chanCommitmentKey             = cstate.ChanCommitmentKey()
+	unsignedAckedUpdatesKey       = cstate.UnsignedAckedUpdatesKey()
+	remoteUnsignedLocalUpdatesKey = cstate.RemoteUnsignedLocalUpdatesKey()
+	revocationStateKey            = cstate.RevocationStateKey()
+	commitDiffKey                 = cstate.CommitDiffKey()
+	lastWasRevokeKey              = cstate.LastWasRevokeKey()
 )
 
 var (

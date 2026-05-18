a544352e21decccb92cee16d75c66a0b54d9fe0a
ziggie <ziggie1984@protonmail.com>
2026-05-18 00:41:09 -0300

    chanstate: move close summary kv storage
    Move close summary serialization and closed-channel bucket writes into a
    dedicated channel state KV store file. Keep the existing channeldb
    helper names as thin wrappers so current call sites continue to work
    while the storage implementation moves into chanstate.

--- channeldb/channel.go
+++ channeldb/channel.go
@@ -2064,161 +2064,17 @@ func (c *ChannelStateDB) RemoteRevocationStore(channel *OpenChannel) (
 func putChannelCloseSummary(tx kvdb.RwTx, chanID []byte,
 	summary *ChannelCloseSummary, lastChanState *OpenChannel) error {
 
-	closedChanBucket, err := tx.CreateTopLevelBucket(closedChannelBucket)
-	if err != nil {
-		return err
-	}
-
-	summary.RemoteCurrentRevocation = lastChanState.RemoteCurrentRevocation
-	summary.RemoteNextRevocation = lastChanState.RemoteNextRevocation
-	summary.LocalChanConfig = lastChanState.LocalChanCfg
-
-	var b bytes.Buffer
-	if err := serializeChannelCloseSummary(&b, summary); err != nil {
-		return err
-	}
-
-	return closedChanBucket.Put(chanID, b.Bytes())
+	return cstate.PutChannelCloseSummary(
+		tx, chanID, summary, lastChanState,
+	)
 }
 
 func serializeChannelCloseSummary(w io.Writer, cs *ChannelCloseSummary) error {
-	err := WriteElements(w,
-		cs.ChanPoint, cs.ShortChanID, cs.ChainHash, cs.ClosingTXID,
-		cs.CloseHeight, cs.RemotePub, cs.Capacity, cs.SettledBalance,
-		cs.TimeLockedBalance, cs.CloseType, cs.IsPending,
-	)
-	if err != nil {
-		return err
-	}
-
-	// If this is a close channel summary created before the addition of
-	// the new fields, then we can exit here.
-	if cs.RemoteCurrentRevocation == nil {
-		return WriteElements(w, false)
-	}
-
-	// If fields are present, write boolean to indicate this, and continue.
-	if err := WriteElements(w, true); err != nil {
-		return err
-	}
-
-	if err := WriteElements(w, cs.RemoteCurrentRevocation); err != nil {
-		return err
-	}
-
-	if err := writeChanConfig(w, &cs.LocalChanConfig); err != nil {
-		return err
-	}
-
-	// The RemoteNextRevocation field is optional, as it's possible for a
-	// channel to be closed before we learn of the next unrevoked
-	// revocation point for the remote party. Write a boolean indicating
-	// whether this field is present or not.
-	if err := WriteElements(w, cs.RemoteNextRevocation != nil); err != nil {
-		return err
-	}
-
-	// Write the field, if present.
-	if cs.RemoteNextRevocation != nil {
-		if err = WriteElements(w, cs.RemoteNextRevocation); err != nil {
-			return err
-		}
-	}
-
-	// Write whether the channel sync message is present.
-	if err := WriteElements(w, cs.LastChanSyncMsg != nil); err != nil {
-		return err
-	}
-
-	// Write the channel sync message, if present.
-	if cs.LastChanSyncMsg != nil {
-		if err := WriteElements(w, cs.LastChanSyncMsg); err != nil {
-			return err
-		}
-	}
-
-	return nil
+	return cstate.SerializeChannelCloseSummary(w, cs)
 }
 
 func deserializeCloseChannelSummary(r io.Reader) (*ChannelCloseSummary, error) {
-	c := &ChannelCloseSummary{}
-
-	err := ReadElements(r,
-		&c.ChanPoint, &c.ShortChanID, &c.ChainHash, &c.ClosingTXID,
-		&c.CloseHeight, &c.RemotePub, &c.Capacity, &c.SettledBalance,
-		&c.TimeLockedBalance, &c.CloseType, &c.IsPending,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	// We'll now check to see if the channel close summary was encoded with
-	// any of the additional optional fields.
-	var hasNewFields bool
-	err = ReadElements(r, &hasNewFields)
-	if err != nil {
-		return nil, err
-	}
-
-	// If fields are not present, we can return.
-	if !hasNewFields {
-		return c, nil
-	}
-
-	// Otherwise read the new fields.
-	if err := ReadElements(r, &c.RemoteCurrentRevocation); err != nil {
-		return nil, err
-	}
-
-	if err := readChanConfig(r, &c.LocalChanConfig); err != nil {
-		return nil, err
-	}
-
-	// Finally, we'll attempt to read the next unrevoked commitment point
-	// for the remote party. If we closed the channel before receiving a
-	// channel_ready message then this might not be present. A boolean
-	// indicating whether the field is present will come first.
-	var hasRemoteNextRevocation bool
-	err = ReadElements(r, &hasRemoteNextRevocation)
-	if err != nil {
-		return nil, err
-	}
-
-	// If this field was written, read it.
-	if hasRemoteNextRevocation {
-		err = ReadElements(r, &c.RemoteNextRevocation)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	// Check if we have a channel sync message to read.
-	var hasChanSyncMsg bool
-	err = ReadElements(r, &hasChanSyncMsg)
-	if err == io.EOF {
-		return c, nil
-	} else if err != nil {
-		return nil, err
-	}
-
-	// If a chan sync message is present, read it.
-	if hasChanSyncMsg {
-		// We must pass in reference to a lnwire.Message for the codec
-		// to support it.
-		var msg lnwire.Message
-		if err := ReadElements(r, &msg); err != nil {
-			return nil, err
-		}
-
-		chanSync, ok := msg.(*lnwire.ChannelReestablish)
-		if !ok {
-			return nil, errors.New("unable cast db Message to " +
-				"ChannelReestablish")
-		}
-		c.LastChanSyncMsg = chanSync
-	}
-
-	return c, nil
+	return cstate.DeserializeCloseChannelSummary(r)
 }
 
 func writeChanConfig(b io.Writer, c *ChannelConfig) error {

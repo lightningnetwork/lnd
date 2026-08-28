21818763a09670793b3724d9507f1eb6ae61aa68
ziggie <ziggie1984@protonmail.com>
2026-08-28 18:12:18 -0300

    build: bump version to v0.20.4-beta.rc1

--- build/version.go
+++ build/version.go
@@ -47,11 +47,11 @@ const (
 	AppMinor uint = 20
 
 	// AppPatch defines the application patch for this binary.
-	AppPatch uint = 03
+	AppPatch uint = 04
 
 	// AppPreRelease MUST only contain characters from semanticAlphabet per
 	// the semantic versioning spec.
-	AppPreRelease = "beta"
+	AppPreRelease = "beta.rc1"
 )
 
 func init() {
